980be9be216ec29f41318198de809c6c7bc20efd
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-05 20:11:37 +0200

    Add Rust Leap exercise solution
    IntelliJ IDEA Rust plugin gives error about "Use of moved value" but the project compiles and tests run fine. Not sure if it is supposed to be a warning about suboptimal code or what...

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,8 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+    // on every year that is evenly divisible by 4
+    return year % 4 == 0
+        // except every year that is evenly divisible by 100
+        && (year % 100 != 0
+            // unless the year is also evenly divisible by 400
+            || (year % 100 == 0 && year % 400 == 0));
 }
