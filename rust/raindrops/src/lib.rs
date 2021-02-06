a738e3520a0b0d222d8f4b38a835ebcbeea09b0b
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-06 19:20:04 +0200

    Add Rust Raindrops exercise solution

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,35 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+    let mut raindrop_sounds = String::with_capacity(3);
+
+    if should_pling(n) {
+        raindrop_sounds.push_str("Pling");
+    }
+    if should_plang(n) {
+        raindrop_sounds.push_str("Plang");
+    }
+    if should_plong(n) {
+        raindrop_sounds.push_str("Plong");
+    }
+
+    if no_sound(&raindrop_sounds) {
+        return n.to_string();
+    }
+
+    raindrop_sounds
+}
+
+fn should_pling(n: u32) -> bool {
+    n % 3 == 0
+}
+
+fn should_plang(n: u32) -> bool {
+    n % 5 == 0
+}
+
+fn should_plong(n: u32) -> bool {
+    n % 7 == 0
+}
+
+fn no_sound(raindrop : &String) -> bool {
+    raindrop.is_empty()
 }
