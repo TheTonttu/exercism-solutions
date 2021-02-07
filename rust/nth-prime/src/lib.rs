ad93dfbe72956cc2be8ec059f4f518149b84c1f8
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-07 20:47:18 +0200

    Move if clauses into functions

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -7,13 +7,21 @@ pub fn nth(n: u32) -> u32 {
     let mut prime_candidate = *primes.last().expect("There should be at least one prime value in the beginning");
     while primes.len() <= (n as usize) {
         prime_candidate += 1;
-        if prime_candidate % 2 == 0 {
+        if is_even(&prime_candidate) {
             continue;
         }
-        if primes.iter().any(|prime|prime_candidate % prime == 0) {
+        if is_dividable_with_any(&prime_candidate, &primes) {
             continue;
         }
         primes.push(prime_candidate);
     }
     primes[n as usize]
+}
+
+fn is_even(number:&u32) -> bool {
+    number % 2 == 0
+}
+
+fn is_dividable_with_any(number:&u32, dividends:&[u32]) -> bool {
+    dividends.iter().any(|dividend|number % dividend == 0)
 }
