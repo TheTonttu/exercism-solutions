ae440bcf4ad27e53141ba4d20db9a6961f6a6601
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-07 20:19:47 +0200

    Improvements based on cargo clippy feedback + constant for first prime

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,16 +1,19 @@
+const FIRST_PRIME:u32 = 2;
+
 pub fn nth(n: u32) -> u32 {
     let mut primes:Vec<u32> = Vec::new();
-    primes.push(2);
+    primes.push(FIRST_PRIME);
 
-    let mut prime_candidate = primes.last().expect("There should be at least one prime value in the beginning").clone();
+    let mut prime_candidate = *primes.last().expect("There should be at least one prime value in the beginning");
     while primes.len() <= (n as usize) {
         prime_candidate += 1;
         if prime_candidate % 2 == 0 {
             continue;
-        } else if primes.iter().any(|prime|prime_candidate % prime == 0) {
+        }
+        if primes.iter().any(|prime|prime_candidate % prime == 0) {
             continue;
         }
         primes.push(prime_candidate);
     }
-    return primes[n as usize];
+    primes[n as usize]
 }
