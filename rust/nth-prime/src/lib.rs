8d76c4874fe564e5362f8498436c7ec30bd40a47
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-07 20:00:59 +0200

    Add Rust Nth Prime exercise solution

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+    let mut primes:Vec<u32> = Vec::new();
+    primes.push(2);
+
+    let mut prime_candidate = primes.last().expect("There should be at least one prime value in the beginning").clone();
+    while primes.len() <= (n as usize) {
+        prime_candidate += 1;
+        if prime_candidate % 2 == 0 {
+            continue;
+        } else if primes.iter().any(|prime|prime_candidate % prime == 0) {
+            continue;
+        }
+        primes.push(prime_candidate);
+    }
+    return primes[n as usize];
 }
