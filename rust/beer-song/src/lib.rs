dc029fa5bde78c37bd3ef9b70d56c292c6af8320
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-08 19:53:56 +0200

    Pass beer-song verse tests

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,5 +1,17 @@
+const MAX_BEER_COUNT:u32 = 99;
+
 pub fn verse(n: u32) -> String {
-    unimplemented!("emit verse {}", n)
+    match n {
+        // multiple beers
+        n if n >= 3 => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottles of beer on the wall.\n", n, n, n-1),
+        // two beers, transition from plural to singular
+        n if n == 2 => format!("{} bottles of beer on the wall, {} bottles of beer.\nTake one down and pass it around, {} bottle of beer on the wall.\n", n, n, n-1),
+        // one beer
+        n if n == 1 => format!("{} bottle of beer on the wall, {} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n", n, n),
+        // zero beers
+        n if n == 0 => format!("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, {} bottles of beer on the wall.\n", MAX_BEER_COUNT),
+        _ => panic!("Deal with it.")
+    }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
