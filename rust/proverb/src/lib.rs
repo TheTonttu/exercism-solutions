a6f278dd9dd0a700df9768a66fb84f8632ca3f74
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-09 21:48:23 +0200

    Move item pair parts gathering to a separate function

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -6,16 +6,11 @@ pub fn build_proverb(list: &[&str]) -> String {
 
     let mut proverb = String::new();
     if has_at_least_one_pair(list) {
-        let overlapping_pairs = list.windows(2);
-        for item_pair in overlapping_pairs {
-            match item_pair {
-                [first, second] => proverb.push_str(& *format!("For want of a {} the {} was lost.\n", first, second)),
-                _ => panic!("Oh no! :o")
-            }
-        }
+        let item_pairs = gather_item_pairs(list);
+        proverb.push_str(&item_pairs.concat());
     }
 
-    let first_item = list.first().unwrap();
+    let first_item = list.first().unwrap_or(&"nail");
     proverb.push_str(&*format!("And all for the want of a {}.", first_item));
 
     proverb
@@ -23,4 +18,19 @@ pub fn build_proverb(list: &[&str]) -> String {
 
 fn has_at_least_one_pair(list: &[&str]) -> bool {
     list.len() >= 2
+}
+
+fn gather_item_pairs(list: &[&str]) -> Vec<String> {
+    let mut pair_parts = Vec::with_capacity(list.len());
+
+    let overlapping_pairs = list.windows(2);
+    for item_pair in overlapping_pairs {
+        let part = match item_pair {
+            [first, second] => format!("For want of a {} the {} was lost.\n", first, second),
+            _ => "".to_string()
+        };
+        pair_parts.push(part);
+    }
+
+    pair_parts
 }
