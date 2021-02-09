088ae300a1bfaad849b4db28eb941955435bc64e
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-09 22:00:45 +0200

    Replace vector push with functional style chain collection

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,5 +1,4 @@
 pub fn build_proverb(list: &[&str]) -> String {
-
     if list.is_empty() {
         return String::new();
     }
@@ -21,16 +20,10 @@ fn has_at_least_one_pair(list: &[&str]) -> bool {
 }
 
 fn gather_item_pairs(list: &[&str]) -> Vec<String> {
-    let mut pair_parts = Vec::with_capacity(list.len());
-
-    let overlapping_pairs = list.windows(2);
-    for item_pair in overlapping_pairs {
-        let part = match item_pair {
+    list.windows(2)
+        .map(|item_pair| match item_pair {
             [first, second] => format!("For want of a {} the {} was lost.\n", first, second),
-            _ => "".to_string()
-        };
-        pair_parts.push(part);
-    }
-
-    pair_parts
+            _ => "".to_string(),
+        })
+        .collect()
 }
