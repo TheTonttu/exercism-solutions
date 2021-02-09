a426c2edd26b1e6a8897e28b6372795fbecd10d4
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-09 21:22:05 +0200

    Add Rust Proverb exercise solution

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,3 +1,26 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+
+    if list.is_empty() {
+        return String::new();
+    }
+
+    let mut proverb = String::new();
+    if has_at_least_one_pair(list) {
+        let overlapping_pairs = list.windows(2);
+        for item_pair in overlapping_pairs {
+            match item_pair {
+                [first, second] => proverb.push_str(& *format!("For want of a {} the {} was lost.\n", first, second)),
+                _ => panic!("Oh no! :o")
+            }
+        }
+    }
+
+    let first_item = list.first().unwrap();
+    proverb.push_str(&*format!("And all for the want of a {}.", first_item));
+
+    proverb
+}
+
+fn has_at_least_one_pair(list: &[&str]) -> bool {
+    list.len() >= 2
 }
