98677f0019c42b8ef00aecc7788d9d19f791cde5
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-10 19:40:20 +0200

    Replace brute force sum of squares with a condensed formula

--- rust/difference-of-squares/src/lib.rs
+++ rust/difference-of-squares/src/lib.rs
@@ -5,9 +5,12 @@ pub fn square_of_sum(n: u32) -> u32 {
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1..=n)
-        .map(|i| i.pow(2))
-        .sum::<u32>()
+    // Gauss ❤
+    (n*(n+1)*(2*n+1))/6
+
+    // (1..=n)
+    //     .map(|i| i.pow(2))
+    //     .sum::<u32>()
 }
 
 pub fn difference(n: u32) -> u32 {
