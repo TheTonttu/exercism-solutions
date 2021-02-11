91e0ddb19cda2cd75bad6b0ac047924bd615c48f
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-11 18:56:31 +0200

    Add Rust Sum of Multiples exercise solution

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,7 +1,20 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+    let mut unique_multiples = HashSet::new();
+    let mut sum = 0;
+    for factor in factors {
+        for multiplier in 1.. {
+            let result = factor * multiplier;
+            if result == 0 || result >= limit {
+                break;
+            }
+            if !unique_multiples.contains(&result) {
+                unique_multiples.insert(result);
+                sum += result;
+            }
+        }
+    }
+
+    sum
 }
