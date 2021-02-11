7b41e59cf30790df6f087d3350d339ec0082792a
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-11 19:11:11 +0200

    Add checks before trying to use factor and rename variable result to multiple

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -4,17 +4,25 @@ pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     let mut unique_multiples = HashSet::new();
     let mut sum = 0;
     for factor in factors {
+        if is_factor_unusable(factor, limit) {
+            continue;
+        }
+
         for multiplier in 1.. {
-            let result = factor * multiplier;
-            if result == 0 || result >= limit {
+            let multiple = multiplier * factor;
+            if multiple >= limit {
                 break;
             }
-            if !unique_multiples.contains(&result) {
-                unique_multiples.insert(result);
-                sum += result;
+            if !unique_multiples.contains(&multiple) {
+                unique_multiples.insert(multiple);
+                sum += multiple;
             }
         }
     }
 
     sum
+}
+
+fn is_factor_unusable(factor: &u32, limit: u32) -> bool {
+    *factor == 0 || *factor >= limit
 }
