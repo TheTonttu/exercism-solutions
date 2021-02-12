f5ac45df05ecb5164bde9ba5b81d6ed33d689c27
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-12 20:13:55 +0200

    Derp, should have looked at the pattern more closely. It is binary...

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -4,8 +4,7 @@ const MAX_SQUARES: u32 = 64;
 pub fn square(s: u32) -> u64 {
     assert!(MIN_SQUARES <= s && s <= MAX_SQUARES, format!("Square must be between {min} and {max}", min = MIN_SQUARES, max = MAX_SQUARES));
 
-    (2..=s as u64)
-        .fold(1, |acc, _| acc * 2)
+    2u64.pow(s - 1)
 }
 
 pub fn total() -> u64 {
