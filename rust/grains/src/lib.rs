f83a27bb2c9c2ded4989bdf29f864b5d87e612ee
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-12 19:53:11 +0200

    Add Rust Grains exercise solution

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,7 +1,14 @@
+const TOTAL_SQUARES: u32 = 64;
+
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    assert!(1 <= s && s <= 64, "Square must be between 1 and 64");
+
+    (2..=s as u64)
+        .fold(1, |acc, _| acc * 2)
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    (1..=TOTAL_SQUARES)
+        .map(|i| square(i))
+        .sum()
 }
