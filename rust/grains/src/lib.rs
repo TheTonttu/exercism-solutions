7d7ec7d99f9116a6af98e18fa3ad75bf11d0256c
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-12 19:59:47 +0200

    More constants, all the constants.

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -1,14 +1,15 @@
-const TOTAL_SQUARES: u32 = 64;
+const MIN_SQUARES: u32 = 1;
+const MAX_SQUARES: u32 = 64;
 
 pub fn square(s: u32) -> u64 {
-    assert!(1 <= s && s <= 64, "Square must be between 1 and 64");
+    assert!(MIN_SQUARES <= s && s <= MAX_SQUARES, format!("Square must be between {min} and {max}", min = MIN_SQUARES, max = MAX_SQUARES));
 
     (2..=s as u64)
         .fold(1, |acc, _| acc * 2)
 }
 
 pub fn total() -> u64 {
-    (1..=TOTAL_SQUARES)
+    (MIN_SQUARES..=MAX_SQUARES)
         .map(|i| square(i))
         .sum()
 }
