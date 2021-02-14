c1d39076b720419b96c81a22226214258e9e175f
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-14 12:06:11 +0200

    Add alternative solution using vector to hold digits

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,4 +1,22 @@
+// u32::MAX digit count
+const MAX_CAPACITY_DIGITS_U32: usize = 10;
+
 pub fn is_armstrong_number(num: u32) -> bool {
+    let mut digits = Vec::with_capacity(MAX_CAPACITY_DIGITS_U32);
+    let mut remainder = num;
+
+    while remainder > 0 {
+        let digit = remainder % 10;
+        digits.push(digit);
+        remainder /= 10;
+    }
+
+    let digit_count = digits.len() as u32;
+    num == digits.iter().map(|d| d.pow(digit_count)).sum()
+}
+
+// alternative implementation without vector
+pub fn is_armstrong_number_without_vector(num: u32) -> bool {
     let digit_count = count_digits(num);
     let mut remainder = num;
     let mut sum = 0;
