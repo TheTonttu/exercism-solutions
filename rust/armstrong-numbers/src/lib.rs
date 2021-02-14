20350b334929215cf7d050a4556bdee0d1230d2d
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-14 11:38:50 +0200

    Add Rust Armstrong Numbers exercise solution

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,22 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+    let digit_count = count_digits(num);
+    let mut remainder = num;
+    let mut sum = 0;
+
+    while remainder > 0 {
+        let digit = remainder % 10;
+        sum += digit.pow(digit_count);
+        remainder /= 10;
+    }
+
+    num == sum
+}
+
+fn count_digits(mut num: u32) -> u32 {
+    let mut count = 0;
+    while num > 0 {
+        num /= 10;
+        count += 1;
+    }
+    count
 }
