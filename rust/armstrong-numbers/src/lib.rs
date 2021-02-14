bfe87e5f3e15b8a32c25840e12ad20d045df8eae
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-14 12:08:31 +0200

    Add constant for number base

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,14 +1,16 @@
 // u32::MAX digit count
 const MAX_CAPACITY_DIGITS_U32: usize = 10;
+// number base
+const BASE: u32 = 10;
 
 pub fn is_armstrong_number(num: u32) -> bool {
     let mut digits = Vec::with_capacity(MAX_CAPACITY_DIGITS_U32);
     let mut remainder = num;
 
     while remainder > 0 {
-        let digit = remainder % 10;
+        let digit = remainder % BASE;
         digits.push(digit);
-        remainder /= 10;
+        remainder /= BASE;
     }
 
     let digit_count = digits.len() as u32;
@@ -22,9 +24,9 @@ pub fn is_armstrong_number_without_vector(num: u32) -> bool {
     let mut sum = 0;
 
     while remainder > 0 {
-        let digit = remainder % 10;
+        let digit = remainder % BASE;
         sum += digit.pow(digit_count);
-        remainder /= 10;
+        remainder /= BASE;
     }
 
     num == sum
@@ -33,7 +35,7 @@ pub fn is_armstrong_number_without_vector(num: u32) -> bool {
 fn count_digits(mut num: u32) -> u32 {
     let mut count = 0;
     while num > 0 {
-        num /= 10;
+        num /= BASE;
         count += 1;
     }
     count
