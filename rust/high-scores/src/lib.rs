5ccd6cdac052be7282bef78911c9443be3ce18f3
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-16 21:35:04 +0200

    Add Rust High Scores exercise solution

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -1,27 +1,39 @@
-#[derive(Debug)]
-pub struct HighScores;
+use std::cmp::Ordering;
 
-impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        unimplemented!(
-            "Construct a HighScores struct, given the scores: {:?}",
+#[derive(Debug)]
+pub struct HighScores<'a> {
+    scores: &'a[u32]
+}
+
+impl <'a> HighScores<'a> {
+    pub fn new(scores: &'a[u32]) -> Self {
+        HighScores {
             scores
-        )
+        }
     }
 
     pub fn scores(&self) -> &[u32] {
-        unimplemented!("Return all the scores as a slice")
+        self.scores
     }
 
     pub fn latest(&self) -> Option<u32> {
-        unimplemented!("Return the latest (last) score")
+        self.scores.last().copied()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        unimplemented!("Return the highest score")
+        self.scores.iter().max().copied()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        unimplemented!("Return 3 highest scores")
+        // Not very efficient...
+        let mut top3_scores = self.scores.to_vec();
+        top3_scores.sort_by(highest_to_lowest);
+        top3_scores.truncate(3);
+
+        top3_scores
     }
+}
+
+fn highest_to_lowest(a: &u32, b: &u32) -> Ordering {
+    a.cmp(b).reverse()
 }
