cb2206dc76de551d123af86ace54c8de86e09782
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-19 21:31:20 +0200

    Replace main if-else with match pattern and reverse 'leftover open brackets' to 'expected closed brackets' to reduce nested matches. Also get rid of the extension traits.

--- rust/matching-brackets/src/lib.rs
+++ rust/matching-brackets/src/lib.rs
@@ -10,24 +10,13 @@ const SQUARE_BRACKETS: BracketPair = ('[', ']');
 const BRACKET_PAIRS: [BracketPair; 3] = [PARENTHESES, BRACES, SQUARE_BRACKETS];
 
 pub fn brackets_are_balanced(string: &str) -> bool {
-    let mut leftover_open_brackets = Vec::new();
+    let mut expected_closed_brackets = Vec::new();
 
     for char in string.chars() {
         match char {
-            c if is_open_bracket(&c) => leftover_open_brackets.push(c),
+            c if is_open_bracket(&c) => expected_closed_brackets.push(get_closed_pair(&c)),
             c if is_closed_bracket(&c) => {
-                if leftover_open_brackets.is_empty() {
-                    return false;
-                }
-
-                let last_open_bracket = leftover_open_brackets.pop().unwrap();
-                let matching_close_bracket = BRACKET_PAIRS
-                    .iter()
-                    .find_by_open_bracket(&last_open_bracket)
-                    .unwrap()
-                    .closed_bracket();
-
-                if c != matching_close_bracket {
+                if expected_closed_brackets.pop() != Some(c) {
                     return false;
                 }
             }
@@ -35,40 +24,20 @@ pub fn brackets_are_balanced(string: &str) -> bool {
         }
     }
 
-    leftover_open_brackets.is_empty()
+    expected_closed_brackets.is_empty()
 }
 
-fn is_open_bracket(c: &char) -> bool {
-    OPEN_BRACKETS.contains(c)
+fn is_open_bracket(char: &char) -> bool {
+    OPEN_BRACKETS.contains(char)
 }
 
-fn is_closed_bracket(c: &char) -> bool {
-    CLOSED_BRACKETS.contains(c)
+fn is_closed_bracket(char: &char) -> bool {
+    CLOSED_BRACKETS.contains(char)
 }
 
-trait FindBracketPair {
-    /// Find bracket pair based on open bracket.
-    ///
-    /// Returns the matching `BracketPair`, or `None` if none found.
-    fn find_by_open_bracket(&mut self, open_bracket: &char) -> Option<&BracketPair>;
-}
-
-impl FindBracketPair for std::slice::Iter<'_, BracketPair> {
-    fn find_by_open_bracket(&mut self, open_bracket: &char) -> Option<&BracketPair> {
-        self.find(|(open, _closed)| open == open_bracket)
-    }
-}
-
-trait BracketPairAccess {
-    /// Get the closed bracket from `BracketPair`.
-    ///
-    /// Returns `BracketPair` closed bracket.
-    fn closed_bracket(&self) -> char;
-}
-
-impl BracketPairAccess for BracketPair {
-    fn closed_bracket(&self) -> char {
-        let (_open, closed) = self;
-        *closed
+fn get_closed_pair(open_bracket: &char) -> char {
+    match BRACKET_PAIRS.iter().find(|(open, ..)| open == open_bracket) {
+        Some((.., closed)) => *closed,
+        None => panic!(format!("Unexpected '{}' open bracket", open_bracket)),
     }
 }
