f842e1e5b8f4261d9511230c2df30cf55beeca8b
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-20 21:23:34 +0200

    Add Rust Clock exercise solution

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,15 +1,54 @@
-pub struct Clock;
+use core::fmt;
+
+const HOURS_IN_DAY: i32 = 24;
+const MINUTES_IN_HOUR: i32 = 60;
+
+#[derive(Debug)]
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+        let (extracted_hours, extracted_minutes) = extract_hours_from_minutes(minutes);
+
+        let positive_hours = match hours + extracted_hours {
+            h if h >= 0 => h % HOURS_IN_DAY,
+            h if h < 0 => (HOURS_IN_DAY - (h.abs() % HOURS_IN_DAY)) % HOURS_IN_DAY,
+            _ => 0,
+        };
+
+        Clock {
+            hours: positive_hours,
+            minutes: extracted_minutes,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+        Clock::new(self.hours, self.minutes + minutes)
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+    }
+}
+
+impl PartialEq for Clock {
+    fn eq(&self, other: &Self) -> bool {
+        self.hours == other.hours && self.minutes == other.minutes
+    }
+}
+
+fn extract_hours_from_minutes(minutes: i32) -> (i32, i32) {
+    match minutes {
+        min if min > 0 => (minutes / MINUTES_IN_HOUR, minutes % MINUTES_IN_HOUR),
+        min if min < 0 => {
+            let hours = (min.abs() as f32 / MINUTES_IN_HOUR as f32).ceil() as i32;
+            (-(hours % HOURS_IN_DAY),(MINUTES_IN_HOUR + (min % MINUTES_IN_HOUR)) % MINUTES_IN_HOUR)
+        },
+        _ => (0, 0),
     }
 }
