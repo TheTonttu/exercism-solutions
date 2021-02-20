dddda5261849349f3212dc845e1e099fc605a8dc
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-20 22:24:42 +0200

    Simplify calculations by doing all the calculations in minutes and then extracting hours from the result

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -2,6 +2,7 @@ use core::fmt;
 
 const HOURS_IN_DAY: i32 = 24;
 const MINUTES_IN_HOUR: i32 = 60;
+const MINUTES_IN_DAY: i32 = HOURS_IN_DAY * MINUTES_IN_HOUR;
 
 #[derive(Debug)]
 pub struct Clock {
@@ -11,17 +12,16 @@ pub struct Clock {
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let (extracted_hours, extracted_minutes) = extract_hours_from_minutes(minutes);
-
-        let normalized_hours = match hours + extracted_hours {
-            h if h > 0 => h % HOURS_IN_DAY,
-            h if h < 0 => (HOURS_IN_DAY - (h.abs() % HOURS_IN_DAY)) % HOURS_IN_DAY,
-            _ => 0,
-        };
+        let collective_minutes = hours * MINUTES_IN_HOUR + minutes;
+        let normalized_minutes = ((collective_minutes % MINUTES_IN_DAY)
+            // Make sure value is positive
+            + MINUTES_IN_DAY)
+            // Keep value within 24 h
+            % MINUTES_IN_DAY;
 
         Clock {
-            hours: normalized_hours,
-            minutes: extracted_minutes,
+            hours: normalized_minutes / MINUTES_IN_HOUR,
+            minutes: normalized_minutes % MINUTES_IN_HOUR,
         }
     }
 
@@ -39,16 +39,5 @@ impl fmt::Display for Clock {
 impl PartialEq for Clock {
     fn eq(&self, other: &Self) -> bool {
         self.hours == other.hours && self.minutes == other.minutes
-    }
-}
-
-fn extract_hours_from_minutes(minutes: i32) -> (i32, i32) {
-    match minutes {
-        min if min > 0 => (minutes / MINUTES_IN_HOUR, minutes % MINUTES_IN_HOUR),
-        min if min < 0 => {
-            let hours = -(min.abs() as f32 / MINUTES_IN_HOUR as f32).ceil() as i32;
-            (hours, (MINUTES_IN_HOUR + (min % MINUTES_IN_HOUR)) % MINUTES_IN_HOUR)
-        },
-        _ => (0, 0),
     }
 }
