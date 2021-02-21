0dcc746854489718c0e33bfc662cc2e62fb3c666
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-21 21:05:54 +0200

    Pass first unit test

--- rust/dot-dsl/src/lib.rs
+++ rust/dot-dsl/src/lib.rs
@@ -1,9 +1,74 @@
 pub mod graph {
-    pub struct Graph;
+    use std::collections::HashMap;
+    use crate::graph::graph_items::node::Node;
+    use crate::graph::graph_items::edge::Edge;
+
+    pub struct Graph {
+        pub nodes: Vec<Node>,
+        pub edges: Vec<Edge>,
+        pub attrs: HashMap<String, String>,
+    }
 
     impl Graph {
         pub fn new() -> Self {
-            unimplemented!("Construct a new Graph struct.");
+            Graph {
+                nodes: vec![],
+                edges: vec![],
+                attrs: HashMap::new(),
+            }
+        }
+
+        pub fn with_nodes(&self, nodes: &[Node]) -> Self {
+            unimplemented!("with_nodes")
+        }
+
+        pub fn with_edges(&self, edges: &[Edge]) -> Self {
+            unimplemented!("with_edges")
+        }
+
+        pub fn with_attrs(&self, attributes: &[(&str, &str)]) -> Self {
+            unimplemented!("with_attrs")
+        }
+
+        pub fn get_node(&self, node_name: &str) -> Option<Node> {
+            unimplemented!("get_node")
+        }
+    }
+
+    pub mod graph_items {
+
+        pub mod node {
+            #[derive(Debug, PartialEq)]
+            pub struct Node {}
+
+            impl Node {
+                pub fn new(node_name: &str) -> Self {
+                    unimplemented!("new node");
+                }
+
+                pub fn with_attrs(&self, attributes: &[(&str, &str)]) -> Self {
+                    unimplemented!("with_attrs")
+                }
+
+                pub fn get_attr(&self, attribute_name: &str) -> Option<&str> {
+                    unimplemented!("with_attrs")
+                }
+            }
+        }
+
+        pub mod edge {
+            #[derive(Debug, PartialEq)]
+            pub struct Edge {}
+
+            impl Edge {
+                pub fn new(node_from_name: &str, node_to_name: &str) -> Self {
+                    unimplemented!("new node");
+                }
+
+                pub fn with_attrs(&self, attributes: &[(&str, &str)]) -> Self {
+                    unimplemented!("with_attrs")
+                }
+            }
         }
     }
 }
