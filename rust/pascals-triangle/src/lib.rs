13230c377c01b00793fd30b96a6ed7b4ed9115dd
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-27 00:42:30 +0200

    Rearrange functions based on call order + remove unnecessary negative matches

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -27,6 +27,14 @@ fn create_pascals_triangle(row_count: u32) -> Vec<Vec<u32>> {
     pascals_triangle
 }
 
+fn get_previous_row(pascals_triangle: &[Vec<u32>], curr_row_index: u32) -> Option<&Vec<u32>> {
+    let prev_row_index = (curr_row_index as i32) - 1;
+    match prev_row_index {
+        i if i >= 0 => pascals_triangle.get(i as usize),
+        _ => None,
+    }
+}
+
 fn create_row(row_index: u32, maybe_prev_row: Option<&Vec<u32>>) -> Vec<u32> {
     let row_item_count = 1 + row_index as usize;
     let mut row = Vec::with_capacity(row_item_count);
@@ -55,30 +63,10 @@ fn extract_value_from_entries(entries: (Option<u32>, Option<u32>)) -> u32 {
 fn get_above_entries(prev_row: &[u32], curr_item_index: usize) -> (Option<u32>, Option<u32>) {
     let left_entry_index = (curr_item_index as i32) - 1;
     let left_entry = match left_entry_index {
-        i if i.is_positive() || i.is_zero() => prev_row.get(i as usize).copied(),
-        i if i.is_negative() => None,
+        i if i >= 0 => prev_row.get(i as usize).copied(),
         _ => None,
     };
 
     let right_entry = prev_row.get(curr_item_index).copied();
     (left_entry, right_entry)
-}
-
-fn get_previous_row(pascals_triangle: &[Vec<u32>], curr_row_index: u32) -> Option<&Vec<u32>> {
-    let prev_row_index = (curr_row_index as i32) - 1;
-    match prev_row_index {
-        i if i.is_positive() || i == 0 => pascals_triangle.get(i as usize),
-        i if i.is_negative() => None,
-        _ => None,
-    }
-}
-
-trait Zero {
-    fn is_zero(&self) -> bool;
-}
-
-impl Zero for i32 {
-    fn is_zero(&self) -> bool {
-        *self == 0
-    }
 }
