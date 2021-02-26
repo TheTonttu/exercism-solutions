62d9792e19314895dc297083a16e1d003d6b2d6e
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-26 22:26:01 +0200

    Pass first 2 tests

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -1,11 +1,34 @@
-pub struct PascalsTriangle;
+use std::borrow::BorrowMut;
+
+pub struct PascalsTriangle {
+    rows: Vec<Vec<u32>>,
+}
 
 impl PascalsTriangle {
     pub fn new(row_count: u32) -> Self {
-        unimplemented!("create Pascal's triangle with {} rows", row_count);
+        Self {
+            rows: create_pascals_triangle(row_count),
+        }
     }
 
     pub fn rows(&self) -> Vec<Vec<u32>> {
-        unimplemented!();
+        self.rows.clone()
     }
+}
+
+fn create_pascals_triangle(row_count: u32) -> Vec<Vec<u32>> {
+    let mut pascals_triangle: Vec<Vec<u32>> = Vec::with_capacity(row_count as usize);
+    let mut row_item_count = 1;
+    for i in 0..row_count {
+        let mut row = Vec::with_capacity(row_item_count);
+        for _row_index in 0..row_item_count {
+            row.push(1u32);
+        }
+
+        pascals_triangle.push(row);
+
+        row_item_count += 1;
+    }
+
+    pascals_triangle
 }
