e2ba297b6500d443af4245b9083f8b66de20cfbc
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-26 23:22:24 +0200

    Pass 4th test

--- rust/pascals-triangle/src/lib.rs
+++ rust/pascals-triangle/src/lib.rs
@@ -1,6 +1,3 @@
-use std::borrow::BorrowMut;
-use std::num::Wrapping;
-
 pub struct PascalsTriangle {
     rows: Vec<Vec<u32>>,
 }
@@ -29,7 +26,9 @@ fn create_pascals_triangle(row_count: u32) -> Vec<Vec<u32>> {
         match maybe_prev_row {
             Some(prev_row) => {
                 for item_index in 0..row_item_count {
-                    row.push(1u32);
+                    let entries = get_above_entries(prev_row, item_index);
+                    let value = extract_value_from_entries(entries);
+                    row.push(value);
                 }
             },
             None => row.push(1u32)
@@ -40,6 +39,27 @@ fn create_pascals_triangle(row_count: u32) -> Vec<Vec<u32>> {
     }
 
     pascals_triangle
+}
+
+fn extract_value_from_entries(entries: (Option<u32>, Option<u32>)) -> u32 {
+    match entries {
+        (Some(left), Some(right)) => left + right,
+        (Some(left), None) => left,
+        (None, Some(right)) => right,
+        _ => 0,
+    }
+}
+
+fn get_above_entries(prev_row: &Vec<u32>, curr_item_index: usize) -> (Option<u32>, Option<u32>) {
+    let left_entry_index = (curr_item_index as i32) - 1;
+    let left_entry = match left_entry_index {
+        i if i.is_positive() || i == 0 => prev_row.get(i as usize).copied(),
+        i if i.is_negative() => None,
+        _ => None
+    };
+
+    let right_entry = prev_row.get(curr_item_index).copied();
+    (left_entry, right_entry)
 }
 
 fn get_previous_row(pascals_triangle: &Vec<Vec<u32>>, curr_row_index: u32) -> Option<&Vec<u32>> {
