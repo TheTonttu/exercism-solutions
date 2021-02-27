778fc221575e8e56ffdf0d21efdc3009747360af
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-27 22:17:33 +0200

    Pass all tests

--- rust/nucleotide-count/src/lib.rs
+++ rust/nucleotide-count/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 const VALID_NUCLEOTIDES: [char; 4] = ['A', 'C', 'G', 'T'];
 
@@ -16,8 +16,31 @@ pub fn count(nucleotide: char, dna: &str) -> Result<usize, char> {
 }
 
 pub fn nucleotide_counts(dna: &str) -> Result<HashMap<char, usize>, char> {
-    unimplemented!(
-        "How much of every nucleotide type is contained inside DNA string '{}'?",
-        dna
-    );
+    // TODO: Not very efficient as we do multiple iterations on dna + use count function to do another iteration
+
+    let mut uniques = HashSet::new();
+
+    let mut nucleotide_counts = HashMap::with_capacity(VALID_NUCLEOTIDES.len());
+    dna.chars()
+        .collect::<Vec<char>>()
+        .retain(|n| uniques.insert(*n));
+
+    for nucleotide in uniques {
+        match count(nucleotide, dna) {
+            Ok(count) => {
+                nucleotide_counts.insert(nucleotide, count);
+            }
+            Err(invalid) => {
+                return Err(invalid);
+            }
+        }
+    }
+
+    for nucleotide in &VALID_NUCLEOTIDES {
+        if !nucleotide_counts.contains_key(nucleotide) {
+            nucleotide_counts.insert(*nucleotide, 0);
+        }
+    }
+
+    Ok(nucleotide_counts)
 }
