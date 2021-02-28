18f39e1e6d7b7eecd8058fd8e835aad4eef71fbd
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-28 16:09:16 +0200

    Replace for loop with iterator flat_map

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,10 +1,7 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    let mut transformed_data = BTreeMap::new();
-    for (points, chars) in h.iter() {
-        transformed_data.extend(chars.iter().map(|c| (c.to_ascii_lowercase(), *points)))
-    }
-
-    transformed_data
+    h.iter()
+        .flat_map(|(points, chars)| chars.iter().map(move |c| (c.to_ascii_lowercase(), *points)))
+        .collect()
 }
