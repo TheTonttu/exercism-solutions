66b3ac74e8ad79b0ba29752fbe5cf951e0c41cb1
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-02-28 15:16:29 +0200

    Pass tests

--- rust/etl/src/lib.rs
+++ rust/etl/src/lib.rs
@@ -1,5 +1,10 @@
 use std::collections::BTreeMap;
 
 pub fn transform(h: &BTreeMap<i32, Vec<char>>) -> BTreeMap<char, i32> {
-    unimplemented!("How will you transform the tree {:?}?", h)
+    let mut transformed_data = BTreeMap::new();
+    for (points, chars) in h.iter() {
+        transformed_data.extend(chars.iter().map(|c| (c.to_ascii_lowercase(), *points)))
+    }
+
+    transformed_data
 }
