3a56d412d50a1b31e1897354e683df78869fbedf
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-01 20:15:28 +0200

    Abandon regex and pass all but one tests

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,14 +1,13 @@
-#[macro_use]
-extern crate lazy_static;
-use regex::Regex;
-
-lazy_static! {
-    // Initialize regex only once.
-    static ref RE: Regex = Regex::new(r"\b[_]*(?P<first>[A-Za-z0-9])[\w\d]*").unwrap();
-}
 
 pub fn abbreviate(phrase: &str) -> String {
-    RE.captures_iter(phrase)
-        .map(|caps| caps["first"].to_uppercase())
-        .collect::<String>()
+    phrase
+        .split(&[' ', '-'][..])
+        .map(|s| {
+            s.trim_matches('_')
+                .chars()
+                .next()
+                .map(|c| c.to_uppercase().collect::<String>())
+        })
+        .flatten()
+        .collect()
 }
