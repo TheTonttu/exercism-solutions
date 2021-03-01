20a980419344750a82a8b738ef919e19bad7de60
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-01 21:37:05 +0200

    Abandon all hope but pass the tests :)

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,13 +1,25 @@
+use std::iter::once;
 
 pub fn abbreviate(phrase: &str) -> String {
     phrase
         .split(&[' ', '-'][..])
-        .map(|s| {
-            s.trim_matches('_')
-                .chars()
-                .next()
-                .map(|c| c.to_uppercase().collect::<String>())
-        })
-        .flatten()
+        // Yikes forever...
+        .flat_map(|s|
+            // Get first character + uppercase characters from all lowercase-uppercase patterns.
+            // Prepend window with None to check separately start of split word.
+            once(None)
+                .chain(s.trim_matches('_').chars().map(Some))
+                .collect::<Vec<_>>()
+                .windows(2)
+                .filter(|window| match window {
+                    [None, Some(_first)] => true,
+                    [Some(c1), Some(c2)] => c1.is_lowercase() && c2.is_uppercase(),
+                    &_ => false,
+                })
+                .map(|window| match window {
+                    [_, Some(c2)] => c2.to_uppercase().to_string(),
+                    &_ => "".to_string()
+                })
+                .collect::<Vec<_>>())
         .collect()
 }
