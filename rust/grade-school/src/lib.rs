e0537889871b23cb556a2f39c74854dc7492f5a3
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-06 18:08:45 +0200

    Add Rust Grade School exercise solution

--- rust/grade-school/src/lib.rs
+++ rust/grade-school/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 // This annotation prevents Clippy from warning us that `School` has a
 // `fn new()` with no arguments, but doesn't implement the `Default` trait.
 //
@@ -5,19 +7,33 @@
 // case, we want to keep things relatively simple. The `Default` trait is not the point
 // of this exercise.
 #[allow(clippy::new_without_default)]
-pub struct School {}
+pub struct School {
+    roster: HashMap<String, u32>,
+}
 
 impl School {
-    pub fn new() -> School {
-        unimplemented!()
+    pub fn new() -> Self {
+        School {
+            roster: HashMap::new(),
+        }
     }
 
     pub fn add(&mut self, grade: u32, student: &str) {
-        unimplemented!("Add {} to the roster for {}", student, grade)
+        let student_name = student.to_string();
+        self.roster.insert(student_name, grade);
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        unimplemented!()
+        let unique_grades = self.roster.values().collect::<HashSet<_>>();
+        let mut grades = unique_grades
+            .iter()
+            // kek
+            .copied()
+            .copied()
+            .collect::<Vec<_>>();
+
+        grades.sort_unstable();
+        grades
     }
 
     // If `grade` returned a reference, `School` would be forced to keep a `Vec<String>`
@@ -25,6 +41,14 @@ impl School {
     // the internal structure can be completely arbitrary. The tradeoff is that some data
     // must be copied each time `grade` is called.
     pub fn grade(&self, grade: u32) -> Vec<String> {
-        unimplemented!("Return the list of students in {}", grade)
+        let mut students = self
+            .roster
+            .iter()
+            .filter(|(_k, v)| **v == grade)
+            .map(|(k, _v)| k.clone())
+            .collect::<Vec<_>>();
+
+        students.sort();
+        students
     }
 }
