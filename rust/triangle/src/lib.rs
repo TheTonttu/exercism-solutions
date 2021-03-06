776a4c72a567991773d8755e35bd0664c999c497
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-06 13:41:06 +0200

    Remove need for explicit lifetimes and iter Sum trait by splitting the sides array to separate variables then summing those together.

--- rust/triangle/src/lib.rs
+++ rust/triangle/src/lib.rs
@@ -1,22 +1,27 @@
 use num_traits::Num;
-use std::iter::Sum;
 
 pub struct Triangle<T> {
-    sides: [T; 3],
+    a: T,
+    b: T,
+    c: T,
 }
 
-impl<'a, T: 'a + Num + PartialOrd + Copy + Sum> Triangle<T> {
+impl<T: Num + PartialOrd + Copy> Triangle<T> {
     pub fn build(sides: [T; 3]) -> Option<Triangle<T>> {
         match is_each_side_valid(sides) {
-            true => Some(Triangle { sides }),
+            true => {
+                let (a, b, c) = (sides[0], sides[1], sides[2]);
+                Some(Triangle { a, b, c })
+            }
             false => None,
         }
     }
 
     pub fn is_equilateral(&self) -> bool {
-        let (a, b, c) = (self.sides[0], self.sides[1], self.sides[2]);
+        let (a, b, c) = (self.a, self.b, self.c);
         a == b && b == c
 
+        // Old with sides arrays:
         // self.sides.windows(2).all(|window| match window {
         //     [s1, s2] => s1 == s2,
         //     _ => false,
@@ -28,18 +33,22 @@ impl<'a, T: 'a + Num + PartialOrd + Copy + Sum> Triangle<T> {
     }
 
     pub fn is_isosceles(&self) -> bool {
-        let (a, b, c) = (self.sides[0], self.sides[1], self.sides[2]);
+        let (a, b, c) = (self.a, self.b, self.c);
         (a == b) || (b == c) || (a == c)
     }
 }
 
-fn is_each_side_valid<'a, T: 'a + Num + PartialOrd + Copy + Sum>(sides: [T; 3]) -> bool {
+fn is_each_side_valid<T: Num + PartialOrd + Copy>(sides: [T; 3]) -> bool {
     // a == 0 || b == 0 || c == 0
     if sides.iter().any(|s| (*s).is_zero()) {
         return false;
     }
+    let (a, b, c) = (sides[0], sides[1], sides[2]);
 
-    let perimeter = sides.iter().copied().sum::<T>();
+    // No need for iter Sum trait
+    let perimeter = a + b + c;
+    // let perimeter = sides.iter().copied().sum::<T>();
+
     // a+b > c == (a+b+c)-c > c
     sides.iter().all(|s| perimeter - *s >= *s)
 }
