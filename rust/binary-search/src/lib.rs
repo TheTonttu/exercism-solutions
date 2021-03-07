efcbba4afe8e8442c29b35693a24fe70307af64f
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-07 17:50:04 +0200

    Implement u32 key binary search

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,12 +1,23 @@
 use std::cmp::Ordering;
 
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    let mid = (array.len() - 1) / 2;
-    if let Some(mid_value) = array.get(mid) {
+    let mid_index = match array.len() {
+        0 | 1 => 0,
+        2..=usize::MAX => (array.len() - 1) / 2,
+        _ => panic!("wat"),
+    };
+
+    if let Some(mid_value) = array.get(mid_index) {
         match mid_value.cmp(&key) {
-            Ordering::Less => None, /* TODO: Search right */
-            Ordering::Equal => Some(mid),
-            Ordering::Greater => None, /* TODO: Search left */
+            Ordering::Less => match find(&array[mid_index + 1..], key) {
+                Some(sub_index) => Some((mid_index + 1) + sub_index),
+                None => None,
+            },
+            Ordering::Equal => Some(mid_index),
+            Ordering::Greater => match find(&array[0..mid_index], key) {
+                Some(sub_index) => Some(sub_index),
+                None => None,
+            },
         }
     } else {
         None
