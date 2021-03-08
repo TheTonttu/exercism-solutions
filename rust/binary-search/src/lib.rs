50f09aa78b5f0f0f97da72087b7f2cd2f330d7f1
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-08 13:49:03 +0200

    Simplify middle index calculation...

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -3,10 +3,7 @@ use std::cmp::Ordering;
 pub fn find<K: Ord, A: AsRef<[K]>>(array: A, key: K) -> Option<usize> {
     let values = array.as_ref();
 
-    let mid_index = match values.len() {
-        len if len >= 3 => (len - 1) / 2,
-        _ => 0,
-    };
+    let mid_index = values.len() / 2;
 
     match key.cmp(values.get(mid_index)?) {
         Ordering::Less => find(&values[0..mid_index], key),
