20c2d0b1d914f8785174f16adf042a9d43d033a2
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-08 14:02:41 +0200

    Final formatting?

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,13 +1,12 @@
-use std::cmp::Ordering;
+use std::cmp::Ordering::{Equal, Greater, Less};
 
-pub fn find<K: Ord, A: AsRef<[K]>>(array: A, key: K) -> Option<usize> {
+pub fn find<T: Ord, U: AsRef<[T]>>(array: U, key: T) -> Option<usize> {
     let values = array.as_ref();
-
     let mid_index = values.len() / 2;
 
     match key.cmp(values.get(mid_index)?) {
-        Ordering::Less => find(&values[..mid_index], key),
-        Ordering::Equal => Some(mid_index),
-        Ordering::Greater => find(&values[mid_index + 1..], key).map(|i| i + mid_index + 1),
+        Less => find(&values[..mid_index], key),
+        Equal => Some(mid_index),
+        Greater => find(&values[mid_index + 1..], key).map(|i| i + mid_index + 1),
     }
 }
