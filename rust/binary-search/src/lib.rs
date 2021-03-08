7588b4ce287a8e3ec82524b8978fcf53bbafd1c7
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-08 13:50:43 +0200

    Remove unnecessary start index from slice range

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -6,7 +6,7 @@ pub fn find<K: Ord, A: AsRef<[K]>>(array: A, key: K) -> Option<usize> {
     let mid_index = values.len() / 2;
 
     match key.cmp(values.get(mid_index)?) {
-        Ordering::Less => find(&values[0..mid_index], key),
+        Ordering::Less => find(&values[..mid_index], key),
         Ordering::Equal => Some(mid_index),
         Ordering::Greater => find(&values[mid_index + 1..], key).map(|i| i + mid_index + 1),
     }
