b3b9da396a8c37617c35c00faacc3854439c7360
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-08 18:58:08 +0200

    Add Rust Queen Attack exercise solution

--- rust/queen-attack/src/lib.rs
+++ rust/queen-attack/src/lib.rs
@@ -1,31 +1,42 @@
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition {
+    file: i32,
+    rank: i32,
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen {
+    position: ChessPosition,
+}
 
 impl ChessPosition {
+    const MAX_FILE: i32 = 7;
+    const MAX_RANK: i32 = 7;
+
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({}, {}). If the position is invalid return None.",
-            rank,
-            file
-        );
+        match ChessPosition::is_valid_position(&rank, &file) {
+            true => Some(Self { file, rank }),
+            false => None,
+        }
+    }
+
+    fn is_valid_position(rank: &i32, file: &i32) -> bool {
+        (0..=ChessPosition::MAX_FILE).contains(file) && (0..=ChessPosition::MAX_RANK).contains(rank)
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!(
-            "Given the chess position {:?}, construct a Queen struct.",
-            position
-        );
+        Self { position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!(
-            "Determine if this Queen can attack the other Queen {:?}",
-            other
-        );
+        let (f1, r1) = (self.position.file, self.position.rank);
+        let (f2, r2) = (other.position.file, other.position.rank);
+
+        f1 == f2 || r1 == r2
+            // Positions are diagonal if difference in coordinates is equal
+            // |x1 - x2| == |y1 - y2|
+            || (f1 - f2).abs() == (r1 - r2).abs()
     }
 }
