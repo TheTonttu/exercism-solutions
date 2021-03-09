ca1c987106f1f27b50ea94ff8ecc6654416161f2
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-09 13:15:55 +0200

    Keep track of frame count

--- rust/bowling/src/lib.rs
+++ rust/bowling/src/lib.rs
@@ -1,4 +1,5 @@
 const MAX_PINS_PER_ROLL: u16 = 10;
+const FRAMES_PER_GAME: u16 = 10;
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
@@ -8,21 +9,26 @@ pub enum Error {
 
 pub struct BowlingGame {
     roll_count: u16,
+    frame_count: u16,
     score: u16,
 }
 
 impl BowlingGame {
     pub fn new() -> Self {
         Self {
+            frame_count: 0,
             roll_count: 0,
             score: 0,
         }
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        match pins {
-            0..=MAX_PINS_PER_ROLL => {
+        match (self.frame_count, pins) {
+            (frame_count, _) if frame_count >= FRAMES_PER_GAME => Err(Error::GameComplete),
+            (_, pins) if (0..=MAX_PINS_PER_ROLL).contains(&pins) => {
                 self.roll_count += 1;
+                // TODO: Proper open frame management
+                self.frame_count = self.roll_count / 2;
                 self.score += pins;
                 Ok(())
             }
@@ -31,8 +37,8 @@ impl BowlingGame {
     }
 
     pub fn score(&self) -> Option<u16> {
-        match self.roll_count {
-            1..=u16::MAX => Some(self.roll_count),
+        match self.frame_count {
+            FRAMES_PER_GAME => Some(self.roll_count),
             _ => None,
         }
     }
