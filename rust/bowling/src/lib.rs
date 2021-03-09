20d2f3638eaf4669247aa0e2d0304553b102025d
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-09 12:56:19 +0200

    Implement rudimentary roll functionality for BowlingGame

--- rust/bowling/src/lib.rs
+++ rust/bowling/src/lib.rs
@@ -1,18 +1,29 @@
+const MAX_PINS_PER_ROLL: u16 = 10;
+
 #[derive(Debug, PartialEq)]
 pub enum Error {
     NotEnoughPinsLeft,
     GameComplete,
 }
 
-pub struct BowlingGame {}
+pub struct BowlingGame {
+    score: u16,
+}
 
 impl BowlingGame {
     pub fn new() -> Self {
-        unimplemented!();
+        Self { score: 0 }
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
-        unimplemented!("Record that {} pins have been scored", pins);
+        match pins {
+            0..=MAX_PINS_PER_ROLL => {
+                self.score += pins;
+                Ok(())
+            }
+            _ => Err(Error::NotEnoughPinsLeft)
+        }
+
     }
 
     pub fn score(&self) -> Option<u16> {
@@ -25,4 +36,3 @@ impl Default for BowlingGame {
         Self::new()
     }
 }
-
