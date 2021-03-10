b87d48279d022eb4e2ce190fd497360b1fea053e
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-10 20:44:15 +0200

    Make code more readable(?) + add general check for roll pins to get rid of them from the messy try_create_frame match pattern

--- rust/bowling/src/lib.rs
+++ rust/bowling/src/lib.rs
@@ -2,7 +2,8 @@ use std::iter::once;
 
 const MAX_PINS_PER_ROLL: u16 = 10;
 const FRAMES_PER_GAME: usize = 10;
-const STRIKE_ROLL: u16 = 10;
+const STRIKE_THROW: u16 = 10;
+const SPARE_THROW: u16 = 10;
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
@@ -27,11 +28,12 @@ struct Frame {
 impl Frame {
     fn new(rolls: &[u16]) -> Self {
         let frame_type = match rolls {
-            [r1] if *r1 == 10 => FrameType::Strike,
-            [r1, r2] if *r1 + *r2 == 10 => FrameType::Spare,
-            [r1, r2] if *r1 + *r2 < 10 => FrameType::Open,
+            [r1] if *r1 == STRIKE_THROW => FrameType::Strike,
+            [r1, r2] if *r1 + *r2 == SPARE_THROW => FrameType::Spare,
+            [r1, r2] if *r1 + *r2 < SPARE_THROW => FrameType::Open,
             _ => FrameType::LastFrame,
         };
+
         Self {
             rolls: Vec::from(rolls),
             frame_type,
@@ -47,8 +49,8 @@ pub struct BowlingGame {
 impl BowlingGame {
     pub fn new() -> Self {
         Self {
-            pending_rolls: vec![],
-            frames: Vec::with_capacity(FRAMES_PER_GAME as usize),
+            pending_rolls: Vec::new(),
+            frames: Vec::with_capacity(FRAMES_PER_GAME),
         }
     }
 
@@ -79,21 +81,23 @@ impl BowlingGame {
                 let mut score = 0;
 
                 for (index, frame) in self.frames.iter().enumerate() {
-                    let roll_points: u16 = frame.rolls.iter().sum();
-
                     let bonus_roll_count = match frame.frame_type {
                         FrameType::Open | FrameType::LastFrame => 0,
                         FrameType::Spare => 1,
                         FrameType::Strike => 2,
                     };
-                    let bonus = self
+                    // Sum rolls from following frames that count toward the frame bonus.
+                    let bonus_points = self
                         .frames
                         .iter()
                         .skip(index + 1)
                         .flat_map(|f| f.rolls.iter())
                         .take(bonus_roll_count)
                         .sum::<u16>();
-                    score += roll_points + bonus;
+
+                    let roll_points: u16 = frame.rolls.iter().sum();
+
+                    score += roll_points + bonus_points;
                 }
 
                 Some(score)
@@ -115,41 +119,36 @@ fn try_create_frame<T: AsRef<[u16]>>(
 ) -> Result<Option<Frame>, Error> {
     let rolls = rolls.as_ref();
 
+    if rolls.iter().any(|r| *r > MAX_PINS_PER_ROLL) {
+        return Err(Error::NotEnoughPinsLeft);
+    }
+
     match (frame_number, rolls) {
         (frame_number, _) if frame_number > FRAMES_PER_GAME => Err(Error::GameComplete),
         // Special last frame
         (frame_number, last_rolls) if frame_number == FRAMES_PER_GAME => {
             match last_rolls {
+                // Strike followed by knocking too many pins with fill ball
                 [r1, r2, r3]
-                    // Strike followed by knocking too many pins with fill ball
-                    if *r1 == MAX_PINS_PER_ROLL
-                        && *r2 < MAX_PINS_PER_ROLL
-                        && (*r2 + *r3) > MAX_PINS_PER_ROLL =>
+                    if *r1 == STRIKE_THROW && *r2 < STRIKE_THROW && (*r2 + *r3) > SPARE_THROW =>
                 {
                     Err(Error::NotEnoughPinsLeft)
                 }
-                [r1, r2, r3]
-                    // Any of the rolls knock down too many pins
-                    if *r1 > MAX_PINS_PER_ROLL
-                        || *r2 > MAX_PINS_PER_ROLL
-                        || *r3 > MAX_PINS_PER_ROLL =>
-                {
-                    Err(Error::NotEnoughPinsLeft)
-                }
-                [_, r2] if *r2 > MAX_PINS_PER_ROLL => Err(Error::NotEnoughPinsLeft),
                 // Fill ball
-                [r1, r2, _] if *r1 == MAX_PINS_PER_ROLL || *r1 + *r2 == MAX_PINS_PER_ROLL => {
+                [r1, r2, _] if *r1 == STRIKE_THROW || *r1 + *r2 == SPARE_THROW => {
                     Ok(Some(Frame::new(rolls)))
                 }
-                [r1, r2] if *r1 + *r2 < MAX_PINS_PER_ROLL => Ok(Some(Frame::new(rolls))),
-                // Continue collecting
+                // Last frame is open
+                [r1, r2] if *r1 + *r2 < SPARE_THROW => Ok(Some(Frame::new(rolls))),
+                // Else continue collecting
                 _ => Ok(None),
             }
         }
-        (_, [r1, r2]) if *r1 + *r2 > MAX_PINS_PER_ROLL => Err(Error::NotEnoughPinsLeft),
-        (_, [r1]) if *r1 > MAX_PINS_PER_ROLL => Err(Error::NotEnoughPinsLeft),
+        // Spare has extra pins
+        (_, [r1, r2]) if *r1 + *r2 > SPARE_THROW => Err(Error::NotEnoughPinsLeft),
         // Continue collecting
-        (_, [r1]) if *r1 < STRIKE_ROLL => Ok(None),
+        (_, [r1]) if *r1 < STRIKE_THROW => Ok(None),
+        // Else end frame
         _ => Ok(Some(Frame::new(rolls))),
     }
 }
