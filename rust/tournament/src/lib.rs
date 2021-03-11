4c2606d93098179be3c728b20b8c94d482b4148d
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-11 17:34:20 +0200

    More on point variable names

--- rust/tournament/src/lib.rs
+++ rust/tournament/src/lib.rs
@@ -21,20 +21,20 @@ enum MatchResult<'a> {
 #[derive(Copy, Clone)]
 struct TeamStatistics<'a> {
     team_name: &'a str,
-    matches_won: u32,
-    matches_drawn: u32,
-    matches_lost: u32,
-    score: u32,
+    wins: u32,
+    draws: u32,
+    losses: u32,
+    points: u32,
 }
 
 impl<'a> TeamStatistics<'a> {
     fn new(team_name: &'a str) -> Self {
         Self {
             team_name,
-            matches_won: 0,
-            matches_drawn: 0,
-            matches_lost: 0,
-            score: 0,
+            wins: 0,
+            draws: 0,
+            losses: 0,
+            points: 0,
         }
     }
 }
@@ -84,13 +84,13 @@ fn calculate_team_stats(match_results: Vec<MatchResult>) -> Vec<TeamStatistics> 
             let ht = team_map
                 .entry(home_team)
                 .or_insert_with(|| TeamStatistics::new(home_team));
-            ht.matches_won += 1;
-            ht.score += WIN_POINTS;
+            ht.wins += 1;
+            ht.points += WIN_POINTS;
 
             let vt = team_map
                 .entry(visiting_team)
                 .or_insert_with(|| TeamStatistics::new(visiting_team));
-            vt.matches_lost += 1;
+            vt.losses += 1;
         }
         MatchResult::Draw {
             home_team,
@@ -99,14 +99,14 @@ fn calculate_team_stats(match_results: Vec<MatchResult>) -> Vec<TeamStatistics> 
             let ht = team_map
                 .entry(home_team)
                 .or_insert_with(|| TeamStatistics::new(home_team));
-            ht.matches_drawn += 1;
-            ht.score += DRAW_POINTS;
+            ht.draws += 1;
+            ht.points += DRAW_POINTS;
 
             let vt = team_map
                 .entry(visiting_team)
                 .or_insert_with(|| TeamStatistics::new(visiting_team));
-            vt.matches_drawn += 1;
-            vt.score += DRAW_POINTS;
+            vt.draws += 1;
+            vt.points += DRAW_POINTS;
         }
         MatchResult::Loss {
             home_team,
@@ -115,20 +115,20 @@ fn calculate_team_stats(match_results: Vec<MatchResult>) -> Vec<TeamStatistics> 
             let ht = team_map
                 .entry(home_team)
                 .or_insert_with(|| TeamStatistics::new(home_team));
-            ht.matches_lost += 1;
+            ht.losses += 1;
 
             let vt = team_map
                 .entry(visiting_team)
                 .or_insert_with(|| TeamStatistics::new(visiting_team));
-            vt.matches_won += 1;
-            vt.score += WIN_POINTS;
+            vt.wins += 1;
+            vt.points += WIN_POINTS;
         }
     });
 
     let mut stats: Vec<TeamStatistics> = team_map.values().copied().collect();
     stats.sort_by(|ts1, ts2| {
-        ts2.score
-            .cmp(&ts1.score)
+        ts2.points
+            .cmp(&ts1.points)
             .then(ts1.team_name.cmp(&ts2.team_name))
     });
     stats
@@ -151,11 +151,11 @@ fn format_team_stats(team_stats: Vec<TeamStatistics>) -> String {
             format!(
                 "{:<30} | {:>2} | {:>2} | {:>2} | {:>2} | {:>2}",
                 ts.team_name,
-                ts.matches_won + ts.matches_drawn + ts.matches_lost,
-                ts.matches_won,
-                ts.matches_drawn,
-                ts.matches_lost,
-                ts.score
+                ts.wins + ts.draws + ts.losses,
+                ts.wins,
+                ts.draws,
+                ts.losses,
+                ts.points
             )
         })
         .collect::<Vec<_>>()
