389892432b47c00e5f21032cb8c031f4f2bcbf61
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-11 13:14:18 +0200

    Parse match results

--- rust/tournament/src/lib.rs
+++ rust/tournament/src/lib.rs
@@ -1,6 +1,60 @@
+struct TeamStatistics {
+    matches_played: u32,
+    matches_won: u32,
+    matches_drawn: u32,
+    matches_lost: u32,
+}
+
+enum MatchResult {
+    Win { home_team: String, visiting_team: String },
+    Draw { home_team: String, visiting_team: String },
+    Loss { home_team: String, visiting_team: String },
+}
+
 pub fn tally(match_results: &str) -> String {
-    unimplemented!(
-        "Given the result of the played matches '{}' return a properly formatted tally table string.",
-        match_results
-    );
+    let results = parse_match_results(match_results);
+    let team_stats = calculate_team_stats(results);
+    format_table(team_stats)
+}
+
+fn parse_match_results(match_results: &str) -> Vec<MatchResult> {
+    let lines : Vec<&str> = match_results.split('\n').collect();
+
+    lines.iter().map(|l| {
+        let elements: Vec<&str> = l.split(';').collect();
+        match elements.as_slice() {
+            [home_team, visiting_team, outcome] => {
+                match *outcome {
+                    "win" => Some(MatchResult::Win { home_team: home_team.to_string(), visiting_team: visiting_team.to_string() }),
+                    "draw" => Some(MatchResult::Draw { home_team: home_team.to_string(), visiting_team: visiting_team.to_string() }),
+                    "loss" => Some(MatchResult::Loss { home_team: home_team.to_string(), visiting_team: visiting_team.to_string() }),
+                    _ => None
+                }
+            },
+            _ => None
+        }
+    }).filter_map(|r| r)
+    .collect()
+}
+
+fn calculate_team_stats(match_results: Vec<MatchResult>) -> Vec<TeamStatistics> {
+    Vec::new()
+}
+
+fn format_table(team_stats: Vec<TeamStatistics>) -> String {
+    let header = format_table_header();
+    let content = format_team_stats(team_stats);
+
+    match content.is_empty() {
+        true => header,
+        false => [header, content].join("\n")
+    }
+}
+
+fn format_team_stats(team_stats: Vec<TeamStatistics>) -> String {
+    String::new()
+}
+
+fn format_table_header() -> String {
+    "Team                           | MP |  W |  D |  L |  P".to_string()
 }
