b1571f0e86a6c4c51cf1efcb484978b61623f4dd
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-12 23:15:01 +0200

    Derp start

--- rust/alphametics/src/lib.rs
+++ rust/alphametics/src/lib.rs
@@ -1,5 +1,13 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
+    let unique_chars: HashSet<char> = input.chars().filter(|c| c.is_alphabetic()).collect();
+
+    let char_numbers: HashMap<char, u32> = unique_chars.iter().enumerate().map(|(i, c)| (*c,i as u32)).collect();
+
+    let words = input.split(' ').filter(|element| element.len() > 2);
+
+    let words_as_numbers = words.map(|w| w.chars().filter_map(|c| char_numbers.get(&c));
+
     unimplemented!("Solve the alphametic {:?}", input)
 }
