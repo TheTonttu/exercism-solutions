a4682630564110a92aa7d34cef399f1c37b57d35
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-13 16:24:00 +0200

    Prototyping with one iteration

--- rust/alphametics/src/lib.rs
+++ rust/alphametics/src/lib.rs
@@ -3,11 +3,38 @@ use std::collections::{HashMap, HashSet};
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
     let unique_chars: HashSet<char> = input.chars().filter(|c| c.is_alphabetic()).collect();
 
-    let char_numbers: HashMap<char, u32> = unique_chars.iter().enumerate().map(|(i, c)| (*c,i as u32)).collect();
+    let char_numbers: HashMap<char, u8> = unique_chars
+        .iter()
+        .enumerate()
+        .map(|(i, c)| (*c, i as u8))
+        .collect();
 
-    let words = input.split(' ').filter(|element| element.len() > 2);
+    let words = input
+        .split(' ')
+        .filter(|element| element.chars().any(|c| c.is_alphabetic()));
 
-    let words_as_numbers = words.map(|w| w.chars().filter_map(|c| char_numbers.get(&c));
+    let words_as_numbers: Vec<u32> = words
+        .filter_map(|w| {
+            w.chars()
+                .filter_map(|c| char_numbers.get(&c))
+                .map(|n| n.to_string())
+                .collect::<String>()
+                .parse::<u32>()
+                .ok()
+        })
+        .collect();
 
-    unimplemented!("Solve the alphametic {:?}", input)
+    println!("{:?}", words_as_numbers);
+
+    let sum: u32 = words_as_numbers[..words_as_numbers.len() - 1].iter().sum();
+
+    println!("{:?}", sum);
+
+    let expected_sum: u32 = *words_as_numbers.last().unwrap();
+
+    if sum == expected_sum {
+        Some(char_numbers)
+    } else {
+        None
+    }
 }
