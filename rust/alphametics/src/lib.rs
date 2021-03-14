8b54d687cd9a53c40235783b7b26487c5960de32
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-14 12:36:45 +0200

    Brute force by going through all the number permutations until the sum of words converted to numbers matches the last converted word.

--- rust/alphametics/src/lib.rs
+++ rust/alphametics/src/lib.rs
@@ -1,40 +1,46 @@
+use itertools::Itertools;
 use std::collections::{HashMap, HashSet};
 
 pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
     let unique_chars: HashSet<char> = input.chars().filter(|c| c.is_alphabetic()).collect();
 
-    let char_numbers: HashMap<char, u8> = unique_chars
-        .iter()
-        .enumerate()
-        .map(|(i, c)| (*c, i as u8))
-        .collect();
+    let number_permutations = (0u8..=9).permutations(unique_chars.len());
 
-    let words = input
-        .split(' ')
-        .filter(|element| element.chars().any(|c| c.is_alphabetic()));
+    // Brute force, yay!
+    for numbers in number_permutations {
+        let char_numbers: HashMap<char, u8> = unique_chars
+            .iter()
+            .zip(numbers)
+            .map(|(c, i)| (*c, i))
+            .collect();
 
-    let words_as_numbers: Vec<u32> = words
-        .filter_map(|w| {
-            w.chars()
-                .filter_map(|c| char_numbers.get(&c))
-                .map(|n| n.to_string())
-                .collect::<String>()
-                .parse::<u32>()
-                .ok()
-        })
-        .collect();
+        let words = input
+            .split(' ')
+            .filter(|element| element.chars().any(|c| c.is_alphabetic()));
 
-    println!("{:?}", words_as_numbers);
+        // TODO: Skip the permutation if any of the converted words have leading zeroes.
 
-    let sum: u32 = words_as_numbers[..words_as_numbers.len() - 1].iter().sum();
+        let words_as_numbers: Vec<u32> = words
+            .filter_map(|w| {
+                w.chars()
+                    .filter_map(|c| char_numbers.get(&c))
+                    .map(|n| n.to_string())
+                    .collect::<String>()
+                    .parse::<u32>()
+                    .ok()
+            })
+            .collect();
 
-    println!("{:?}", sum);
+        let sum: u32 = words_as_numbers[..words_as_numbers.len() - 1].iter().sum();
 
-    let expected_sum: u32 = *words_as_numbers.last().unwrap();
+        let expected_sum: u32 = *words_as_numbers.last().unwrap();
 
-    if sum == expected_sum {
-        Some(char_numbers)
-    } else {
-        None
+        if sum == expected_sum {
+            return Some(char_numbers);
+        }
+        println!("wrong numbers: {:?}", words_as_numbers);
+        println!("expected sum: {:?}", sum);
     }
+
+    None
 }
