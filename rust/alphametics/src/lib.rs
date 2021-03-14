8dc8259bff93cb29ab65b22b8160eaa5a81216eb
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-14 14:36:05 +0200

    number -> digit when talking about vector of numbers

--- rust/alphametics/src/lib.rs
+++ rust/alphametics/src/lib.rs
@@ -13,30 +13,30 @@ pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
 
     // Brute force, yay!
     for numbers in number_permutations {
-        let char_number_designations: HashMap<char, u8> = unique_chars
+        let char_digit_designations: HashMap<char, u8> = unique_chars
             .iter()
             .zip(numbers)
             .map(|(c, i)| (*c, i))
             .collect();
 
-        let collected_word_numbers = words
+        let word_digits = words
             .iter()
             .map(|w| {
                 w.chars()
-                    .filter_map(|c| char_number_designations.get(&c))
+                    .filter_map(|c| char_digit_designations.get(&c))
                     .copied()
                     .collect::<Vec<u8>>()
             })
             .collect::<Vec<_>>();
 
-        if has_any_leading_zeroes(&collected_word_numbers) {
+        if has_any_leading_zeroes(&word_digits) {
             continue;
         }
 
-        let words_as_numbers: Vec<u32> = collected_word_numbers
+        let words_as_numbers: Vec<u32> = word_digits
             .iter()
-            .filter_map(|numbers| {
-                numbers
+            .filter_map(|digits| {
+                digits
                     .iter()
                     .filter_map(|n| std::char::from_digit(*n as u32, 10))
                     // TODO: Skip intermediate String collection
@@ -51,7 +51,7 @@ pub fn solve(input: &str) -> Option<HashMap<char, u8>> {
         let expected_sum: u32 = *words_as_numbers.last().unwrap();
 
         if sum == expected_sum {
-            return Some(char_number_designations);
+            return Some(char_digit_designations);
         }
         //println!("wrong numbers: {:?}", words_as_numbers);
         //println!("expected sum: {:?}", sum);
