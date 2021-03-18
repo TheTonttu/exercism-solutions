d6f89b5832cd1365b838c94b99d44652be0a699e
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-18 20:44:04 +0200

    Preliminary brute force method

--- rust/two-bucket/src/lib.rs
+++ rust/two-bucket/src/lib.rs
@@ -4,12 +4,14 @@ pub enum Bucket {
     Two,
 }
 
+#[derive(Debug)]
 enum Move {
     FillBucket(usize),
     EmptyBucket(usize),
     PourFrom(usize),
 }
 
+#[derive(Debug)]
 struct State {
     bucket_contents: [u8; 2],
     bucket_capacities: [u8; 2],
@@ -20,6 +22,56 @@ impl State {
         State {
             bucket_contents,
             bucket_capacities,
+        }
+    }
+
+    fn after_move(&self, selected_move: &Move) -> State {
+        match selected_move {
+            Move::FillBucket(bucket_index) => {
+                let mut new_contents = self.bucket_contents;
+                new_contents[*bucket_index] = self.bucket_capacities[*bucket_index];
+                State {
+                    bucket_contents: new_contents,
+                    bucket_capacities: self.bucket_capacities,
+                }
+            }
+            Move::EmptyBucket(bucket_index) => {
+                let mut new_contents = self.bucket_contents;
+                new_contents[*bucket_index] = 0;
+                State {
+                    bucket_contents: new_contents,
+                    bucket_capacities: self.bucket_capacities,
+                }
+            }
+            Move::PourFrom(bucket_index) => {
+                let source_index = match bucket_index {
+                    0 => 0,
+                    1 => 1,
+                    _ => unimplemented!("unsupported index: {}", bucket_index),
+                };
+
+                let target_index = match bucket_index {
+                    0 => 1,
+                    1 => 0,
+                    _ => unimplemented!("unsupported index: {}", bucket_index),
+                };
+
+                let target_space =
+                    self.bucket_capacities[target_index] - self.bucket_contents[target_index];
+                let source_left =
+                    (self.bucket_contents[source_index] as i8 - target_space as i8).max(0) as u8;
+                let source_remove = self.bucket_contents[source_index] - source_left;
+                let target_content = self.bucket_contents[target_index] + source_remove;
+
+                let mut new_contents = self.bucket_contents;
+                new_contents[source_index] = source_left;
+                new_contents[target_index] = target_content;
+
+                State {
+                    bucket_contents: new_contents,
+                    bucket_capacities: self.bucket_capacities,
+                }
+            }
         }
     }
 }
@@ -48,23 +100,45 @@ pub fn solve(
         Bucket::One => [capacity_1, 0],
         Bucket::Two => [0, capacity_2],
     };
-    // TODO: Should we count the first fill?
+
+    // First fill counts as a move.
     let mut moves: u8 = 1;
 
-    let state = State::new(start_contents, [capacity_1, capacity_2]);
+    let start_state = State::new(start_contents, [capacity_1, capacity_2]);
+
+    // TODO: No need to store the whole history, just one iteration.
+    let mut history: Vec<Vec<State>> = Vec::new();
+    history.push(vec![start_state]);
 
     while moves < u8::MAX {
-        // TODO: State change, etc.
+        println!("{:?}", moves);
 
-        if goal_achieved(&state, goal) {
-            let goal_bucket = Bucket::One;
-            let other_bucket = 0;
+        // Brute force by going through every move permutation until we hit the goal.
+        if let Some(last_permutations) = history.last() {
+            let mut curr_permutations = Vec::new();
+            for state in last_permutations {
+                for a_move in all_legal_moves() {
+                    println!("Before: {:?}", state);
+                    println!("Move: {:?}", a_move);
+                    let new_state = state.after_move(&a_move);
+                    println!("After: {:?}", state);
 
-            return Some(BucketStats {
-                moves,
-                goal_bucket,
-                other_bucket,
-            });
+                    if let Some((goal_bucket, goal_bucket_index)) = goal_bucket(&new_state, goal) {
+                        let other_bucket =
+                            new_state.bucket_contents[get_other_bucket_index(&goal_bucket_index)];
+                        moves += 1;
+
+                        return Some(BucketStats {
+                            moves,
+                            goal_bucket,
+                            other_bucket,
+                        });
+                    }
+
+                    curr_permutations.push(new_state);
+                }
+            }
+            history.push(curr_permutations);
         }
 
         moves += 1;
@@ -73,6 +147,36 @@ pub fn solve(
     None
 }
 
-fn goal_achieved(state: &State, goal: u8) -> bool {
-    state.bucket_contents.iter().any(|c| *c == goal)
+fn goal_bucket(state: &State, goal: u8) -> Option<(Bucket, usize)> {
+    state
+        .bucket_contents
+        .iter()
+        .copied()
+        .enumerate()
+        .find(|(_i, c)| *c == goal)
+        .map(|(i, _c)| match i {
+            0 => (Bucket::One, i),
+            1 => (Bucket::Two, i),
+            _ => unimplemented!("unsupported index: {}", i),
+        })
+}
+
+fn get_other_bucket_index(the_bucket_index: &usize) -> usize {
+    match the_bucket_index {
+        0 => 1,
+        1 => 0,
+        _ => unimplemented!("unsupported index: {}", the_bucket_index),
+    }
+}
+
+fn all_legal_moves() -> Vec<Move> {
+    // TODO: Pass state and check relevant moves.
+    vec![
+        Move::FillBucket(0),
+        Move::FillBucket(1),
+        Move::EmptyBucket(0),
+        Move::EmptyBucket(1),
+        Move::PourFrom(0),
+        Move::PourFrom(1),
+    ]
 }
