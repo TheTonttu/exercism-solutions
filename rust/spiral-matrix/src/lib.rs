c8c67b8809b1eae75059cd56a2eb15eb05a2910a
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-23 20:57:30 +0200

    Utilize Option<T> in new coordinates calculation

--- rust/spiral-matrix/src/lib.rs
+++ rust/spiral-matrix/src/lib.rs
@@ -1,16 +1,20 @@
+use std::ops::Range;
+
 pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
     let size = size as usize;
 
     let mut matrix = vec![vec![0u32; size]; size];
 
     let max_steps = size * size;
+
     let mut number_source = 1..=max_steps as u32;
 
+    let mut coordinates = (0i32, 0i32);
     let mut velocity = (1, 0);
 
-    let mut coordinates = (0i32, 0i32);
-
     let mut step = 0;
+    let boundaries = 0..size;
+
     while step < max_steps {
         step += 1;
 
@@ -19,30 +23,44 @@ pub fn spiral_matrix(size: u32) -> Vec<Vec<u32>> {
             matrix[y as usize][x as usize] = next;
         }
 
-        let mut new_coordinates;
-        let boundaries = 0..size;
+        let mut maybe_new_coordinates: Option<(i32, i32)> = None;
 
-        loop {
-            new_coordinates = calculate_new_coordinates(coordinates, velocity);
-            let (nx, ny) = new_coordinates;
-
-            if boundaries.contains(&(nx as usize))
-                && boundaries.contains(&(ny as usize))
-                && matrix[ny as usize][nx as usize] == 0
-                // On last step numbers are already assigned so turning is unnecessary.
-                || step == max_steps
-            {
-                break;
+        while maybe_new_coordinates.is_none()
+            // On last step numbers are already assigned so turning is unnecessary.
+            && step != max_steps
+        {
+            maybe_new_coordinates =
+                calculate_new_valid_coordinates(&matrix, coordinates, velocity, &boundaries);
+            match maybe_new_coordinates {
+                Some(new_coordinates) => {
+                    coordinates = new_coordinates;
+                }
+                None => {
+                    velocity = change_direction_clockwise(velocity);
+                }
             }
-            velocity = change_direction_clockwise(velocity);
         }
-        coordinates = new_coordinates;
     }
     matrix
 }
 
-fn calculate_new_coordinates((x, y): (i32, i32), (vx, vy): (i32, i32)) -> (i32, i32) {
-    (x + vx, y + vy)
+fn calculate_new_valid_coordinates(
+    matrix: &[Vec<u32>],
+    (x, y): (i32, i32),
+    (vx, vy): (i32, i32),
+    boundaries: &Range<usize>,
+) -> Option<(i32, i32)> {
+    let new_coordinates = (x + vx, y + vy);
+    let (nx, ny) = new_coordinates;
+
+    if boundaries.contains(&(nx as usize))
+        && boundaries.contains(&(ny as usize))
+        && matrix[ny as usize][nx as usize] == 0
+    {
+        Some(new_coordinates)
+    } else {
+        None
+    }
 }
 
 fn change_direction_clockwise(velocity: (i32, i32)) -> (i32, i32) {
