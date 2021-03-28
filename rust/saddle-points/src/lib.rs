be580d625687c0ca2ddf749ac5e3032e309531e3
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-28 19:23:55 +0300

    Replace for-loop with iter-enumerate
    But why?

--- rust/saddle-points/src/lib.rs
+++ rust/saddle-points/src/lib.rs
@@ -3,13 +3,9 @@ use std::cmp::Ordering;
 pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
     let mut saddle_points = Vec::new();
 
-    for row_index in 0..input.len() {
-        if input[row_index].is_empty() {
-            continue;
-        }
-
-        let (row_max, max_col_indexes) = input[row_index].iter().enumerate().skip(1).fold(
-            (input[row_index][0], vec![0]),
+    for (row_index, row) in input.iter().enumerate().filter(|(_, row)| !row.is_empty()) {
+        let (row_max, max_col_indexes) = row.iter().enumerate().skip(1).fold(
+            (row[0], vec![0]),
             |(mut row_max, mut indexes), (col_index, curr_value)| {
                 match row_max.cmp(&curr_value) {
                     Ordering::Less => {
