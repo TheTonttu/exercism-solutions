3800529396b084b8bb8d6f9e910125d63bcaf0e5
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-29 18:07:49 +0300

    Preliminary implementation with char convert to lowercase

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,3 +1,12 @@
+use std::collections::HashSet;
+
 pub fn check(candidate: &str) -> bool {
-    unimplemented!("Is {} an isogram?", candidate);
+    let mut check_set = HashSet::new();
+
+    candidate
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .map(|c| c.to_lowercase())
+        // Insert fails -> same char has already appeared
+        .all(|lc| check_set.insert(lc.fold(0, |unicode_sum, char| (unicode_sum + char as u32))))
 }
