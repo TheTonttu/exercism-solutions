fa123f51969ef0bda6a6b038b7b405fc6efc5d46
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-03-29 18:19:08 +0300

    Alternative implementation with lowercase string

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -7,6 +7,12 @@ pub fn check(candidate: &str) -> bool {
         .chars()
         .filter(|c| c.is_alphanumeric())
         .map(|c| c.to_lowercase())
-        // Insert fails -> same char has already appeared
         .all(|lc| check_set.insert(lc.fold(0, |unicode_sum, char| (unicode_sum + char as u32))))
+
+    // Alternative with lowercase string:
+    // candidate
+    //     .to_lowercase()
+    //     .chars()
+    //     .filter(|c| c.is_alphanumeric())
+    //     .all(|c| check_set.insert(c))
 }
