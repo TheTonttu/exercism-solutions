fd34ec15e0492838c173c19d1f309283ff3a416a
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-04-03 14:41:07 +0300

    Return collection of u8s from number to digits as the max value per digit is very small + move match_teens function closer to where it is used

--- rust/say/src/lib.rs
+++ rust/say/src/lib.rs
@@ -64,7 +64,7 @@ fn number_to_text(number: &u16) -> String {
     }
 }
 
-fn match_ones<'a>(digit: &u16) -> &'a str {
+fn match_ones<'a>(digit: &u8) -> &'a str {
     match digit {
         0 => "zero",
         1 => "one",
@@ -80,22 +80,7 @@ fn match_ones<'a>(digit: &u16) -> &'a str {
     }
 }
 
-fn match_teens<'a>(digit: &u16) -> &'a str {
-    match digit {
-        11 => "eleven",
-        12 => "twelve",
-        13 => "thirteen",
-        14 => "fourteen",
-        15 => "fifteen",
-        16 => "sixteen",
-        17 => "seventeen",
-        18 => "eighteen",
-        19 => "nineteen",
-        _ => panic!("{} not match with 11-19 range", digit),
-    }
-}
-
-fn match_tens<'a>(digit: &u16) -> &'a str {
+fn match_tens<'a>(digit: &u8) -> &'a str {
     match digit {
         1 => "ten",
         2 => "twenty",
@@ -110,18 +95,33 @@ fn match_tens<'a>(digit: &u16) -> &'a str {
     }
 }
 
-fn match_hundreds(digit: &u16) -> String {
+fn match_hundreds(digit: &u8) -> String {
     [match_ones(digit), " hundred"].concat()
 }
 
-fn match_tens_and_ones(tens: &u16, ones: &u16) -> String {
-    const TENS_DIGIT_TEN: u16 = 1;
+fn match_tens_and_ones(tens: &u8, ones: &u8) -> String {
+    const TENS_DIGIT_TEN: u8 = 1;
 
     match (tens, ones) {
         (0, ones) => match_ones(ones).to_string(),
         (tens, 0) => match_tens(tens).to_string(),
         (tens, ones) if *tens == TENS_DIGIT_TEN => match_teens(&(tens * 10 + ones)).to_string(),
         _ => [match_tens(tens), "-", match_ones(ones)].concat(),
+    }
+}
+
+fn match_teens<'a>(digit: &u8) -> &'a str {
+    match digit {
+        11 => "eleven",
+        12 => "twelve",
+        13 => "thirteen",
+        14 => "fourteen",
+        15 => "fifteen",
+        16 => "sixteen",
+        17 => "seventeen",
+        18 => "eighteen",
+        19 => "nineteen",
+        _ => panic!("{} not match with 11-19 range", digit),
     }
 }
 
@@ -132,28 +132,28 @@ pub fn split_number_to_thousands(number: &u64) -> Vec<u16> {
     let mut split = Vec::new();
 
     while remainder >= GROUPING {
-        let part = remainder % GROUPING;
-        split.insert(0, part as u16);
+        let part = (remainder % GROUPING) as u16;
+        split.insert(0, part);
         remainder /= GROUPING;
     }
-    let last_group = remainder;
-    split.insert(0, last_group as u16);
+    let last_group = remainder as u16;
+    split.insert(0, last_group);
 
     split
 }
 
-pub fn number_to_digits(number: &u16) -> Vec<u16> {
+pub fn number_to_digits(number: &u16) -> Vec<u8> {
     const DIGIT_BASE: u16 = 10;
 
     let mut digits = Vec::new();
 
     let mut remainder = *number;
     while remainder > 9 {
-        let digit = remainder % DIGIT_BASE;
+        let digit = (remainder % DIGIT_BASE) as u8;
         digits.insert(0, digit);
         remainder /= DIGIT_BASE;
     }
-    let last_digit = remainder;
+    let last_digit = remainder as u8;
     digits.insert(0, last_digit);
 
     digits
