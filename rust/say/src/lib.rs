8b4747402128a48c7c863bb92ad0e53ca324e76f
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-04-03 14:45:55 +0300

    Reorder all functions closer to where they are used

--- rust/say/src/lib.rs
+++ rust/say/src/lib.rs
@@ -44,6 +44,23 @@ pub fn encode(n: u64) -> String {
         .join(" ")
 }
 
+pub fn split_number_to_thousands(number: &u64) -> Vec<u16> {
+    const GROUPING: u64 = 1000;
+
+    let mut remainder = *number;
+    let mut split = Vec::new();
+
+    while remainder >= GROUPING {
+        let part = (remainder % GROUPING) as u16;
+        split.insert(0, part);
+        remainder /= GROUPING;
+    }
+    let last_group = remainder as u16;
+    split.insert(0, last_group);
+
+    split
+}
+
 fn number_to_text(number: &u16) -> String {
     let digits = number_to_digits(number);
 
@@ -61,6 +78,38 @@ fn number_to_text(number: &u16) -> String {
         [tens, ones] => match_tens_and_ones(tens, ones),
         [ones] => match_ones(ones).to_string(),
         _ => "".to_string(),
+    }
+}
+
+pub fn number_to_digits(number: &u16) -> Vec<u8> {
+    const DIGIT_BASE: u16 = 10;
+
+    let mut digits = Vec::new();
+
+    let mut remainder = *number;
+    while remainder > 9 {
+        let digit = (remainder % DIGIT_BASE) as u8;
+        digits.insert(0, digit);
+        remainder /= DIGIT_BASE;
+    }
+    let last_digit = remainder as u8;
+    digits.insert(0, last_digit);
+
+    digits
+}
+
+fn match_hundreds(digit: &u8) -> String {
+    [match_ones(digit), " hundred"].concat()
+}
+
+fn match_tens_and_ones(tens: &u8, ones: &u8) -> String {
+    const TENS_DIGIT_TEN: u8 = 1;
+
+    match (tens, ones) {
+        (0, ones) => match_ones(ones).to_string(),
+        (tens, 0) => match_tens(tens).to_string(),
+        (tens, ones) if *tens == TENS_DIGIT_TEN => match_teens(&(tens * 10 + ones)).to_string(),
+        _ => [match_tens(tens), "-", match_ones(ones)].concat(),
     }
 }
 
@@ -95,21 +144,6 @@ fn match_tens<'a>(digit: &u8) -> &'a str {
     }
 }
 
-fn match_hundreds(digit: &u8) -> String {
-    [match_ones(digit), " hundred"].concat()
-}
-
-fn match_tens_and_ones(tens: &u8, ones: &u8) -> String {
-    const TENS_DIGIT_TEN: u8 = 1;
-
-    match (tens, ones) {
-        (0, ones) => match_ones(ones).to_string(),
-        (tens, 0) => match_tens(tens).to_string(),
-        (tens, ones) if *tens == TENS_DIGIT_TEN => match_teens(&(tens * 10 + ones)).to_string(),
-        _ => [match_tens(tens), "-", match_ones(ones)].concat(),
-    }
-}
-
 fn match_teens<'a>(digit: &u8) -> &'a str {
     match digit {
         11 => "eleven",
@@ -123,38 +157,4 @@ fn match_teens<'a>(digit: &u8) -> &'a str {
         19 => "nineteen",
         _ => panic!("{} not match with 11-19 range", digit),
     }
-}
-
-pub fn split_number_to_thousands(number: &u64) -> Vec<u16> {
-    const GROUPING: u64 = 1000;
-
-    let mut remainder = *number;
-    let mut split = Vec::new();
-
-    while remainder >= GROUPING {
-        let part = (remainder % GROUPING) as u16;
-        split.insert(0, part);
-        remainder /= GROUPING;
-    }
-    let last_group = remainder as u16;
-    split.insert(0, last_group);
-
-    split
-}
-
-pub fn number_to_digits(number: &u16) -> Vec<u8> {
-    const DIGIT_BASE: u16 = 10;
-
-    let mut digits = Vec::new();
-
-    let mut remainder = *number;
-    while remainder > 9 {
-        let digit = (remainder % DIGIT_BASE) as u8;
-        digits.insert(0, digit);
-        remainder /= DIGIT_BASE;
-    }
-    let last_digit = remainder as u8;
-    digits.insert(0, last_digit);
-
-    digits
 }
