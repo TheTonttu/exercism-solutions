df61116dbb0f247151c9eb4e61c6376139d651a5
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-04-08 20:48:12 +0300

    Encapsulate X position check to separate function

--- rust/isbn-verifier/src/lib.rs
+++ rust/isbn-verifier/src/lib.rs
@@ -3,21 +3,17 @@ const X_NUMBER: u32 = 10;
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
     const VALID_ISBN10_NUMBER_COUNT: usize = 10;
-    const CHECK_DIGIT_INDEX: usize = 9;
+
     const CHECK_MODULUS: u32 = 11;
 
     let isbn_numbers: Vec<u32> = isbn.chars().filter_map(parse_isbn_number).collect();
-
-    println!("{:?}", isbn_numbers);
 
     if isbn_numbers.len() != VALID_ISBN10_NUMBER_COUNT {
         return false;
     }
 
-    if let Some(x_index) = isbn_numbers.iter().position(|v| *v == X_NUMBER) {
-        if x_index != CHECK_DIGIT_INDEX {
-            return false;
-        }
+    if !is_valid_x_position(&isbn_numbers) {
+        return false;
     }
 
     let multipliers = (1..=(isbn_numbers.len() as u32)).rev();
@@ -38,5 +34,16 @@ fn parse_isbn_number(char: char) -> Option<u32> {
         '0'..='9' => char.to_digit(NUMBER_BASE),
         'X' => Some(X_NUMBER),
         _ => None,
+    }
+}
+
+fn is_valid_x_position(isbn_numbers: &[u32]) -> bool {
+    const CHECK_DIGIT_INDEX: usize = 9;
+
+    if let Some(x_index) = isbn_numbers.iter().position(|v| *v == X_NUMBER) {
+        x_index == CHECK_DIGIT_INDEX
+    } else {
+        // Missing X is also valid
+        true
     }
 }
