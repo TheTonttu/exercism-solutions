c9cdca83007b31aac6a235a79de3886a7e4c5266
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-04-08 20:59:15 +0300

    Replace if-else with match

--- rust/isbn-verifier/src/lib.rs
+++ rust/isbn-verifier/src/lib.rs
@@ -3,7 +3,6 @@ const X_NUMBER: u32 = 10;
 /// Determines whether the supplied string is a valid ISBN number
 pub fn is_valid_isbn(isbn: &str) -> bool {
     const VALID_ISBN10_NUMBER_COUNT: usize = 10;
-
     const CHECK_MODULUS: u32 = 11;
 
     let isbn_numbers: Vec<u32> = isbn.chars().filter_map(parse_isbn_number).collect();
@@ -36,10 +35,9 @@ fn parse_isbn_number(char: char) -> Option<u32> {
 fn is_valid_x_position(isbn_numbers: &[u32]) -> bool {
     const CHECK_DIGIT_INDEX: usize = 9;
 
-    if let Some(x_index) = isbn_numbers.iter().position(|v| *v == X_NUMBER) {
-        x_index == CHECK_DIGIT_INDEX
-    } else {
+    match isbn_numbers.iter().position(|v| *v == X_NUMBER) {
+        Some(x_index) => x_index == CHECK_DIGIT_INDEX,
         // Missing X is also valid
-        true
+        None => true,
     }
 }
