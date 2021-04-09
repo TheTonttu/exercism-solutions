5bf0bddbff6dc8f67dd5695efe78411e6815da5d
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-04-09 20:59:21 +0300

    Optimization for aliquot sum
    Calculate sum of all possible divisors for number then subtract number from it to get the sum of proper divisors aka aliquot sum.

--- rust/perfect-numbers/src/lib.rs
+++ rust/perfect-numbers/src/lib.rs
@@ -12,8 +12,7 @@ pub fn classify(num: u64) -> Option<Classification> {
         return None;
     }
 
-    let aliquot_sum: u64 = (1..num).filter(|n| num % n == 0).sum();
-
+    let aliquot_sum = calculate_aliquot_sum(num);
     println!("{:?}", aliquot_sum);
 
     let classification = match aliquot_sum.cmp(&num) {
@@ -23,4 +22,22 @@ pub fn classify(num: u64) -> Option<Classification> {
     };
 
     Some(classification)
+}
+
+fn calculate_aliquot_sum(num: u64) -> u64 {
+    let num_sqrt = (num as f64).sqrt() as u64;
+    // Sum of all the possible divisors
+    let divisors_sum: u64 = (1..=num_sqrt)
+        .filter(|n| num % n == 0)
+        .map(|n| {
+            let quotient = num / n;
+            if quotient == n {
+                n
+            } else {
+                n + quotient
+            }
+        })
+        .sum();
+    // Sum of all the possible divisors - the number =  the sum of proper divisors
+    divisors_sum - num
 }
