6e82a01cce6596f4742efa6738e7c5bd688e4cbc
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-04-10 10:51:44 +0300

    Comment score regions in point list

--- rust/scrabble-score/src/lib.rs
+++ rust/scrabble-score/src/lib.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 const CHAR_POINTS: [(char, u64); 26] = [
+    // 1 point
     ('A', 1),
     ('E', 1),
     ('I', 1),
@@ -11,20 +12,26 @@ const CHAR_POINTS: [(char, u64); 26] = [
     ('R', 1),
     ('S', 1),
     ('T', 1),
+    // 2 points
     ('D', 2),
     ('G', 2),
+    // 3 points
     ('B', 3),
     ('C', 3),
     ('M', 3),
     ('P', 3),
+    // 4 points
     ('F', 4),
     ('H', 4),
     ('V', 4),
     ('W', 4),
     ('Y', 4),
+    // 5 points
     ('K', 5),
+    // 8 points
     ('J', 8),
     ('X', 8),
+    // 10 points
     ('Q', 10),
     ('Z', 10),
 ];
