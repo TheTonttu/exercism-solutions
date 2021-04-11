9f4f6747f3c6f650cb8ae5768b7b047fc3fb84c3
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-04-11 13:20:47 +0300

    More descriptive hash set variable name

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    let chars_present: HashSet<_> = sentence.chars().map(|c| c.to_ascii_lowercase()).collect();
+    let contained_chars: HashSet<_> = sentence.chars().map(|c| c.to_ascii_lowercase()).collect();
 
-    ('a'..='z').all(|c| chars_present.contains(&c))
+    ('a'..='z').all(|c| contained_chars.contains(&c))
 }
