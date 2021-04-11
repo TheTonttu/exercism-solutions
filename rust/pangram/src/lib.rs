6e468476b49690fdd6425a15afe9bb3d171e0edc
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-04-11 13:17:56 +0300

    Add Rust Pangram exercise solution

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,4 +1,8 @@
+use std::collections::HashSet;
+
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {} a pangram?", sentence);
+    let chars_present: HashSet<_> = sentence.chars().map(|c| c.to_ascii_lowercase()).collect();
+
+    ('a'..='z').all(|c| chars_present.contains(&c))
 }
