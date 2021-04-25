7b8cd4ffe7f78f79a9295dff8ac9b406e2313c08
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-04-25 10:15:49 +0300

    Preliminary encoding

--- rust/variable-length-quantity/src/lib.rs
+++ rust/variable-length-quantity/src/lib.rs
@@ -4,9 +4,37 @@ pub enum Error {
     Overflow,
 }
 
+const BITMASK_7: u32 = 0b01111111;
+const SIGN_BIT: u32 = 0b10000000;
+
 /// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
 pub fn to_bytes(values: &[u32]) -> Vec<u8> {
-    unimplemented!("Convert the values {:?} to a list of bytes", values)
+    println!("values hex: {:02X?}", values);
+
+    let mut encoded = Vec::new();
+
+    for number in values {
+        let mut extracted = Vec::new();
+        let mut remainder = *number;
+        while remainder > 0 {
+            let extracted_value = if extracted.is_empty() {
+                (remainder & BITMASK_7) as u8
+            }  else {
+                ((remainder & BITMASK_7) | SIGN_BIT) as u8
+            };
+
+            println!("extract: {:02X?}", extracted_value);
+            extracted.insert(0, extracted_value);
+            remainder >>= 7;
+        }
+        if extracted.is_empty() {
+            extracted.push(0);
+        }
+
+        encoded.extend(extracted);
+    }
+
+    encoded
 }
 
 /// Given a stream of bytes, extract all numbers which are encoded in there.
