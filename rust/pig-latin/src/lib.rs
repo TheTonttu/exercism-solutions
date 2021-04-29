b96041fde1cad345b06e8369931b587cbbd7fad5
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-04-29 20:04:57 +0300

    Some sort of handling for starting consonant cluster

--- rust/pig-latin/src/lib.rs
+++ rust/pig-latin/src/lib.rs
@@ -4,6 +4,16 @@ const VOWEL_SOUNDS: [[char; 2]; 4] = [['x', 'r'], ['y', 't'], ['X', 'R'], ['Y', 
 pub fn translate(input: &str) -> String {
     if starts_with_vowel_sound(input) {
         [input, "ay"].concat()
+    } else if starts_with_consonant_cluster_followed_by_y(input) {
+        String::from("oi!")
+    } else if let Some(consonant_count) = start_consonant_count(input) {
+        let consonant_cluster = input[0..consonant_count].to_string();
+        [
+            input[consonant_count..].to_string(),
+            consonant_cluster,
+            "ay".to_string(),
+        ]
+        .concat()
     } else {
         match input.chars().next() {
             Some(first_char) => [
@@ -30,4 +40,29 @@ fn starts_with_vowel_sound(input: &str) -> bool {
         }
         None => false,
     }
+}
+
+fn starts_with_consonant_cluster_followed_by_y(input: &str) -> bool {
+    let mut consonant_count = 0;
+
+    // TODO: Separate check for two character word (consonant + y) scenario
+    for char in input.chars() {
+        if VOWELS.contains(&char) {
+            return false;
+        } else if (char == 'y' || char == 'Y') && consonant_count >= 2 {
+            return true;
+        } else {
+            consonant_count += 1;
+        }
+    }
+    false
+}
+
+fn start_consonant_count(input: &str) -> Option<usize> {
+    let count = input
+        .chars()
+        .take_while(|c| !(VOWELS.contains(&c) || *c == 'y' || *c == 'Y'))
+        .count();
+
+    (count >= 1).then(|| count)
 }
