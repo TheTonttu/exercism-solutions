fd3c162c37e6bba3bd768f19fa3a31f76bd3695d
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-05-02 20:31:48 +0300

    Add C# Hamming exercise solution

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Linq;
 
 public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (firstStrand?.Length != secondStrand?.Length)
+        {
+            throw new ArgumentException("Strands are not equal length");
+        }
+
+        return firstStrand
+            .Zip(secondStrand)
+            .Count(zip => zip.First != zip.Second);
     }
 }
