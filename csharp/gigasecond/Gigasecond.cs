1b937f41310d799c99b11e0d790edbd3e11602a5
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-05-02 11:08:58 +0300

    Add C# Gigasecond exercise solution

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -2,8 +2,10 @@ using System;
 
 public static class Gigasecond
 {
+    private static readonly TimeSpan GigasecondTimeSpan = TimeSpan.FromSeconds(1_000_000_000);
+
     public static DateTime Add(DateTime moment)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return moment.Add(GigasecondTimeSpan);
     }
 }
