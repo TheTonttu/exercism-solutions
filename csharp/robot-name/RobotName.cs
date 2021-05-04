22a177f2f321c964c6d822cf4bdc3edfe8d914b0
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-05-04 17:58:02 +0300

    Preliminary name generation

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,17 +1,52 @@
 using System;
+using System.Linq;
+using System.Text;
 
 public class Robot
 {
+    private static readonly Random RandomGenerator = new Random();
+
+    private string m_name;
+
     public string Name
     {
         get
         {
-            throw new NotImplementedException("You need to implement this function.");
+            return m_name;
         }
+    }
+
+    public Robot()
+    {
+        m_name = GenerateRobotName();
     }
 
     public void Reset()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        m_name = GenerateRobotName();
+    }
+
+    const string Alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private string GenerateRobotName()
+    {
+        const int LetterCount = 2;
+        const int NumberCount = 3;
+
+        var nameBuilder = new StringBuilder();
+        for (int i = 0; i < LetterCount; i++)
+        {
+            int randomIndex = RandomGenerator.Next(0, Alphabets.Length - 1);
+            char randomChar = Alphabets[randomIndex];
+            nameBuilder.Append(randomChar);
+        }
+
+        for (int i = 0; i < NumberCount; i++)
+        {
+            int randomNumber = RandomGenerator.Next(0, 9);
+            nameBuilder.Append(randomNumber);
+        }
+
+        return nameBuilder.ToString();
     }
 }
