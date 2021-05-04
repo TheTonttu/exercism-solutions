3b3e911675191c1ff8e7d7f1c2a029ae9b432b48
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-05-04 19:03:46 +0300

    Keep track of used robot names

--- csharp/robot-name/RobotName.cs
+++ csharp/robot-name/RobotName.cs
@@ -1,32 +1,41 @@
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using System.Text;
 
-public class Robot
+public class Robot : IDisposable
 {
     private static readonly Random RandomGenerator = new Random();
+    private static readonly HashSet<int> NameHashRegistry = new HashSet<int>();
+    private bool _isDisposed;
 
-    private string m_name;
-
-    public string Name
-    {
-        get
-        {
-            return m_name;
-        }
-    }
+    public string Name { get; private set; }
 
     public Robot()
     {
-        m_name = GenerateRobotName();
+        ReplaceName();
     }
 
     public void Reset()
     {
-        m_name = GenerateRobotName();
+        if (_isDisposed) { throw new ObjectDisposedException("Cannot reset disposed robot."); }
+
+        ReplaceName();
     }
 
     const string Alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    private void ReplaceName()
+    {
+        string newName;
+        do
+        {
+            newName = GenerateRobotName();
+        } while (!NameHashRegistry.Add(newName.GetHashCode()));
+
+        int oldNameHash = Name?.GetHashCode() ?? default;
+        NameHashRegistry.Remove(oldNameHash);
+        Name = newName;
+    }
 
     private string GenerateRobotName()
     {
@@ -49,4 +58,32 @@ public class Robot
 
         return nameBuilder.ToString();
     }
+
+    #region IDispoable Members
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!_isDisposed)
+        {
+            if (disposing)
+            {
+                if (Name != null)
+                {
+                    // Unregister name once the robot is out of scope and collected. Otherwise name registry will just keep collecting unused names.
+                    NameHashRegistry.Remove(Name.GetHashCode());
+                    Name = null;
+                }
+            }
+            _isDisposed = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    #endregion
 }
