ccb1be43c37fc396363a8c6155b0e3394cd670f9
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-05-06 19:22:26 +0300

    First try

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,9 +1,23 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+    let char_counts = counts(word);
+
+    possible_anagrams
+        .iter()
+        .map(|w| (w, counts(*w)))
+        .filter(|(_w, counts)| {
+            char_counts
+                .iter()
+                .all(|(c, count)| counts.get(c).map(|count2| count == count2) == Some(true))
+        })
+        .map(|(w, _counts)| *w)
+        .collect()
+}
+
+fn counts(word: &str) -> HashMap<char, usize> {
+    word.chars().fold(HashMap::new(), |mut hm, c| {
+        *hm.entry(c).or_insert(0) += 1;
+        hm
+    })
 }
