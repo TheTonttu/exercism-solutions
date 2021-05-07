b38d1526c06561a4812d512c38554073a848d9e4
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-05-07 20:18:39 +0300

    Replace comparison of hashmap counts with comparison of sorted lowercase chars

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,34 +1,24 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    let char_counts = counts(word);
+
+    let lowercase_word = word.to_lowercase();
+
+    let mut sorted_chars: Vec<char> = lowercase_word.chars().collect();
+    sorted_chars.sort_unstable();
+
+    println!("{:?}", sorted_chars);
 
     possible_anagrams
         .iter()
-        .map(|w| (w, counts(*w)))
-        .filter(|(_w, counts)| {
-            subtract(&char_counts, counts).is_empty()
+        .filter(|w| w.len() == word.len() && w.to_lowercase() != lowercase_word)
+        .map(|w| {
+            let mut sorted_w: Vec<char> = w.to_lowercase().chars().collect();
+            sorted_w.sort_unstable();
+
+            (w, sorted_w)
         })
-        .map(|(w, _counts)| *w)
+        .filter(|(_w, s_w)| sorted_chars.iter().zip(s_w).all(|(c1, c2)| c1 == c2))
+        .map(|(w, _s_w)| *w)
         .collect()
-}
-
-fn counts(word: &str) -> HashMap<char, usize> {
-    word.chars().fold(HashMap::new(), |mut hm, c| {
-        *hm.entry(c).or_insert(0) += 1;
-        hm
-    })
-}
-
-fn subtract(hm1: &HashMap<char, usize>, hm2: &HashMap<char, usize>) -> HashMap<char, usize> {
-    println!("{:?}", hm1);
-    println!("{:?}", hm2);
-    let mut hm = hm1.clone();
-    for (c, count) in hm2.iter() {
-        let entry = hm.entry(*c).or_insert(0);
-        *entry = entry.wrapping_sub(*count);
-    }
-    println!("{:?}", hm);
-    println!();
-    hm.drain().filter(|(_c, count)| *count != 0).collect()
 }
--- rust/anagram/tests/anagram.rs
+++ rust/anagram/tests/anagram.rs
@@ -31,7 +31,6 @@ fn test_detect_simple_anagram() {
 }
 
 #[test]
-#[ignore]
 fn test_does_not_confuse_different_duplicates() {
     let word = "galea";
 
@@ -43,7 +42,6 @@ fn test_does_not_confuse_different_duplicates() {
 }
 
 #[test]
-#[ignore]
 fn test_eliminate_anagram_subsets() {
     let word = "good";
 
@@ -55,7 +53,6 @@ fn test_eliminate_anagram_subsets() {
 }
 
 #[test]
-#[ignore]
 fn test_detect_anagram() {
     let word = "listen";
 
@@ -67,7 +64,6 @@ fn test_detect_anagram() {
 }
 
 #[test]
-#[ignore]
 fn test_multiple_anagrams() {
     let word = "allergy";
 
@@ -86,7 +82,6 @@ fn test_multiple_anagrams() {
 }
 
 #[test]
-#[ignore]
 fn test_case_insensitive_anagrams() {
     let word = "Orchestra";
 
@@ -98,7 +93,6 @@ fn test_case_insensitive_anagrams() {
 }
 
 #[test]
-#[ignore]
 fn test_unicode_anagrams() {
     let word = "ΑΒΓ";
 
@@ -111,7 +105,6 @@ fn test_unicode_anagrams() {
 }
 
 #[test]
-#[ignore]
 fn test_misleading_unicode_anagrams() {
     // Despite what a human might think these words different letters, the input uses Greek A and B
     // while the list of potential anagrams uses Latin A and B.
@@ -125,7 +118,6 @@ fn test_misleading_unicode_anagrams() {
 }
 
 #[test]
-#[ignore]
 fn test_does_not_detect_a_word_as_its_own_anagram() {
     let word = "banana";
 
@@ -137,7 +129,6 @@ fn test_does_not_detect_a_word_as_its_own_anagram() {
 }
 
 #[test]
-#[ignore]
 fn test_does_not_detect_a_differently_cased_word_as_its_own_anagram() {
     let word = "banana";
 
@@ -149,7 +140,6 @@ fn test_does_not_detect_a_differently_cased_word_as_its_own_anagram() {
 }
 
 #[test]
-#[ignore]
 fn test_does_not_detect_a_differently_cased_unicode_word_as_its_own_anagram() {
     let word = "ΑΒΓ";
 
@@ -161,7 +151,6 @@ fn test_does_not_detect_a_differently_cased_unicode_word_as_its_own_anagram() {
 }
 
 #[test]
-#[ignore]
 fn test_same_bytes_different_chars() {
     let word = "a⬂"; // 61 E2 AC 82
 
