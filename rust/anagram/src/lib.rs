853a31bb9b8baf513617b0b71129dd85f840ff2d
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-05-08 10:56:50 +0300

    Combine candidate lowercase conversion and check with word

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,7 +1,6 @@
 use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-
     let lowercase_word = word.to_lowercase();
 
     let mut sorted_chars: Vec<char> = lowercase_word.chars().collect();
@@ -11,9 +10,13 @@ pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'
 
     possible_anagrams
         .iter()
-        .filter(|w| w.len() == word.len() && w.to_lowercase() != lowercase_word)
-        .map(|w| {
-            let mut sorted_w: Vec<char> = w.to_lowercase().chars().collect();
+        .filter(|w| w.len() == word.len())
+        .filter_map(|w| {
+            let w_lower = w.to_lowercase();
+            (w_lower != lowercase_word).then(|| (w, w_lower))
+        })
+        .map(|(w, w_lower)| {
+            let mut sorted_w: Vec<char> = w_lower.chars().collect();
             sorted_w.sort_unstable();
 
             (w, sorted_w)
