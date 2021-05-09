c95445a4858d63d767c3bd6f2b35c6c514529362
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-05-09 14:17:09 +0300

    Convert to imperative style

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -3,30 +3,36 @@ use std::collections::HashSet;
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
     let lowercase_word = word.to_lowercase();
 
-    let mut sorted_chars: Vec<char> = lowercase_word.chars().collect();
-    sorted_chars.sort_unstable();
+    let sorted_chars = {
+        let mut chars: Vec<char> = lowercase_word.chars().collect();
+        chars.sort_unstable();
+        chars
+    };
 
-    println!("{:?}", sorted_chars);
+    let mut anagrams = HashSet::new();
+    for candidate in possible_anagrams {
+        if candidate.len() != word.len() {
+            continue;
+        }
+        let lowercase_candidate = candidate.to_lowercase();
+        if lowercase_candidate == lowercase_word {
+            continue;
+        }
 
-    possible_anagrams
-        .iter()
-        .filter(|candidate| candidate.len() == word.len())
-        .filter_map(|candidate| {
-            let lowercase_candidate = candidate.to_lowercase();
-            (lowercase_candidate != lowercase_word).then(|| (candidate, lowercase_candidate))
-        })
-        .map(|(candidate, lowercase_candidate)| {
-            let mut sorted_candidate_chars: Vec<char> = lowercase_candidate.chars().collect();
-            sorted_candidate_chars.sort_unstable();
+        let sorted_candidate_chars = {
+            let mut chars: Vec<char> = lowercase_candidate.chars().collect();
+            chars.sort_unstable();
+            chars
+        };
 
-            (candidate, sorted_candidate_chars)
-        })
-        .filter(|(_candidate, sorted_candidate_chars)| {
-            sorted_chars
-                .iter()
-                .zip(sorted_candidate_chars)
-                .all(|(c1, c2)| c1 == c2)
-        })
-        .map(|(candidate, _sorted_candidate_chars)| *candidate)
-        .collect()
+        if sorted_chars
+            .iter()
+            .zip(sorted_candidate_chars)
+            .all(|(c1, c2)| *c1 == c2)
+        {
+            anagrams.insert(*candidate);
+        }
+    }
+
+    anagrams
 }
