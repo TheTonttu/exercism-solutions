503008a4880fb166af12333d87cb42b6169a34fa
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-05-10 18:56:19 +0300

    Add C# Grade School exercise solution

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -1,20 +1,39 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class GradeSchool
 {
+    private readonly Dictionary<int, List<string>> _grades = new Dictionary<int, List<string>>();
+
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (_grades.TryGetValue(grade, out var gradeStudentList))
+        {
+            gradeStudentList.Add(student);
+        }
+        else
+        {
+            _grades.Add(grade, new List<string>() { student });
+        }
     }
 
     public IEnumerable<string> Roster()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var roster = new List<string>();
+        foreach (var grade in _grades.OrderBy(kv => kv.Key).Select(kv => kv.Value))
+        {
+            roster.AddRange(grade.OrderBy(student => student));
+        }
+        return roster;
     }
 
     public IEnumerable<string> Grade(int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (!_grades.TryGetValue(grade, out var gradeStudentList))
+        {
+            return Enumerable.Empty<string>();
+        }
+        return gradeStudentList.OrderBy(student => student).ToArray();
     }
 }
--- csharp/grade-school/GradeSchoolTests.cs
+++ csharp/grade-school/GradeSchoolTests.cs
@@ -1,22 +1,22 @@
 // This file was auto-generated based on version 1.0.1 of the canonical data.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
-public class GradeSchoolTests
-{
+public class GradeSchoolTests {
+
     [Fact]
-    public void Adding_a_student_adds_them_to_the_sorted_roster()
-    {
+    public void Adding_a_student_adds_them_to_the_sorted_roster() {
         var sut = new GradeSchool();
         sut.Add("Aimee", 2);
         var expected = new[] { "Aimee" };
         Assert.Equal(expected, sut.Roster());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Adding_more_student_adds_them_to_the_sorted_roster()
-    {
+    [Fact]
+    public void Adding_more_student_adds_them_to_the_sorted_roster() {
         var sut = new GradeSchool();
         sut.Add("Blair", 2);
         sut.Add("James", 2);
@@ -25,9 +25,8 @@ public class GradeSchoolTests
         Assert.Equal(expected, sut.Roster());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Adding_students_to_different_grades_adds_them_to_the_same_sorted_roster()
-    {
+    [Fact]
+    public void Adding_students_to_different_grades_adds_them_to_the_same_sorted_roster() {
         var sut = new GradeSchool();
         sut.Add("Chelsea", 3);
         sut.Add("Logan", 7);
@@ -35,17 +34,15 @@ public class GradeSchoolTests
         Assert.Equal(expected, sut.Roster());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Roster_returns_an_empty_list_if_there_are_no_students_enrolled()
-    {
+    [Fact]
+    public void Roster_returns_an_empty_list_if_there_are_no_students_enrolled() {
         var sut = new GradeSchool();
         var expected = Array.Empty<string>();
         Assert.Empty(sut.Roster());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Student_names_with_grades_are_displayed_in_the_same_sorted_roster()
-    {
+    [Fact]
+    public void Student_names_with_grades_are_displayed_in_the_same_sorted_roster() {
         var sut = new GradeSchool();
         sut.Add("Peter", 2);
         sut.Add("Anna", 1);
@@ -58,9 +55,8 @@ public class GradeSchoolTests
         Assert.Equal(expected, sut.Roster());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Grade_returns_the_students_in_that_grade_in_alphabetical_order()
-    {
+    [Fact]
+    public void Grade_returns_the_students_in_that_grade_in_alphabetical_order() {
         var sut = new GradeSchool();
         sut.Add("Franklin", 5);
         sut.Add("Bradley", 5);
@@ -69,11 +65,43 @@ public class GradeSchoolTests
         Assert.Equal(expected, sut.Grade(5));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Grade_returns_an_empty_list_if_there_are_no_students_in_that_grade()
-    {
+    [Fact]
+    public void Grade_returns_an_empty_list_if_there_are_no_students_in_that_grade() {
         var sut = new GradeSchool();
         var expected = Array.Empty<string>();
         Assert.Empty(sut.Grade(1));
+    }
+
+    // Additional tests
+
+    [Theory]
+    // Separate the data generation so the generation duration is not included in the execution duration.
+    [MemberData(nameof(GenerateLoadsAData))]
+    public void Loads_a_students(IEnumerable<(int Grade, string StudentName)> data, IEnumerable<string> expected) {
+        var sut = new GradeSchool();
+
+        foreach ((int Grade, string StudentName) in data) {
+            sut.Add(StudentName, Grade);
+        }
+        Assert.Equal(expected, sut.Roster());
+    }
+
+    public static IEnumerable<object[]> GenerateLoadsAData() {
+        const int GradeCount = 100;
+        const int StudentsPerGrade = 100;
+
+        var students = new List<(int Grade, string StudentName)>();
+        for (int grade = GradeCount; grade >= 0; grade--) {
+            for (int studentIter = 0; studentIter < StudentsPerGrade; studentIter++) {
+                int studentNumber = studentIter + grade * 1;
+                students.Add((grade, $"Student {studentNumber}"));
+            }
+        }
+        var expected = students.OrderBy(t => t.Grade)
+                           .ThenBy(t => t.StudentName)
+                           .Select(t => t.StudentName)
+                           .ToArray();
+
+        return new[] { new object[] { students, expected } };
     }
 }
