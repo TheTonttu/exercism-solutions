a17d311213dd42758548c6e31cfa02d0a31df20a
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-05-12 18:39:14 +0300

    Preliminary Codons parse & name_for implementations

--- rust/protein-translation/src/lib.rs
+++ rust/protein-translation/src/lib.rs
@@ -1,19 +1,26 @@
-use std::marker::PhantomData;
+use std::collections::HashMap;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
 
 pub struct CodonsInfo<'a> {
-    // This field is here to make the template compile and not to
-    // complain about unused type lifetime parameter "'a". Once you start
-    // solving the exercise, delete this field and the 'std::marker::PhantomData'
-    // import.
-    phantom: PhantomData<&'a ()>,
+    codons_hash_map: HashMap<&'a str, u64>,
+    hash_name_map: HashMap<u64, &'a str>,
 }
 
 impl<'a> CodonsInfo<'a> {
+
+    pub fn new(codons: HashMap<&'a str, u64>, names: HashMap<u64, &'a str>) -> Self {
+        Self {
+            codons_hash_map: codons,
+            hash_name_map: names
+        }
+    }
+
     pub fn name_for(&self, codon: &str) -> Option<&'a str> {
-        unimplemented!(
-            "Return the protein name for a '{}' codon or None, if codon string is invalid",
-            codon
-        );
+        self.codons_hash_map
+            .get(codon)
+            .and_then(|h| self.hash_name_map.get(h))
+            .cloned()
     }
 
     pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
@@ -22,8 +29,17 @@ impl<'a> CodonsInfo<'a> {
 }
 
 pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
-    unimplemented!(
-        "Construct a new CodonsInfo struct from given pairs: {:?}",
-        pairs
-    );
+
+    let mut hasher = DefaultHasher::new();
+
+    let mut codons = HashMap::new();
+    let mut names = HashMap::new();
+    for (codon, name) in pairs {
+        name.hash(&mut hasher);
+        let name_hash = hasher.finish();
+        names.entry(name_hash).or_insert( name);
+        codons.entry(codon).or_insert( name_hash);
+    }
+
+    CodonsInfo::new(codons, names)
 }
