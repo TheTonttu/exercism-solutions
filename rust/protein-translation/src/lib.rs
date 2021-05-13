71c6ba6accc021d2206131e19a43a65f905f88a3
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-05-13 16:34:13 +0300

    Remove useless separate link between codon and name

--- rust/protein-translation/src/lib.rs
+++ rust/protein-translation/src/lib.rs
@@ -1,25 +1,16 @@
-use std::collections::hash_map::DefaultHasher;
 use std::collections::HashMap;
-use std::hash::{Hash, Hasher};
 
 pub struct CodonsInfo<'a> {
-    codons_hash_map: HashMap<&'a str, u64>,
-    hash_name_map: HashMap<u64, &'a str>,
+    codon_name_map: HashMap<&'a str, &'a str>,
 }
 
 impl<'a> CodonsInfo<'a> {
-    pub fn new(codons: HashMap<&'a str, u64>, names: HashMap<u64, &'a str>) -> Self {
-        Self {
-            codons_hash_map: codons,
-            hash_name_map: names,
-        }
+    pub fn new(codon_name_map: HashMap<&'a str, &'a str>) -> Self {
+        Self { codon_name_map }
     }
 
     pub fn name_for(&self, codon: &str) -> Option<&'a str> {
-        self.codons_hash_map
-            .get(codon)
-            .and_then(|h| self.hash_name_map.get(h))
-            .cloned()
+        self.codon_name_map.get(codon).cloned()
     }
 
     pub fn of_rna(&self, rna: &str) -> Option<Vec<&'a str>> {
@@ -40,16 +31,5 @@ impl<'a> CodonsInfo<'a> {
 }
 
 pub fn parse<'a>(pairs: Vec<(&'a str, &'a str)>) -> CodonsInfo<'a> {
-    let mut hasher = DefaultHasher::new();
-
-    let mut codons = HashMap::new();
-    let mut names = HashMap::new();
-    for (codon, name) in pairs {
-        name.hash(&mut hasher);
-        let name_hash = hasher.finish();
-        names.entry(name_hash).or_insert(name);
-        codons.entry(codon).or_insert(name_hash);
-    }
-
-    CodonsInfo::new(codons, names)
+    CodonsInfo::new(pairs.into_iter().collect())
 }
