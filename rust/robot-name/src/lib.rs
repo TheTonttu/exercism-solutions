a55be431056658e30710fd67b5617b56130876b2
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-05-14 14:47:34 +0300

    Implement Robot methods

--- rust/robot-name/src/lib.rs
+++ rust/robot-name/src/lib.rs
@@ -1,15 +1,31 @@
-pub struct Robot;
+use rand::Rng;
+
+#[derive(Default)]
+pub struct Robot {
+    name: String,
+}
 
 impl Robot {
     pub fn new() -> Self {
-        unimplemented!("Construct a new Robot struct.");
+        Self {
+            name: gen_random_name(),
+        }
     }
 
     pub fn name(&self) -> &str {
-        unimplemented!("Return the reference to the robot's name.");
+        self.name.as_str()
     }
 
     pub fn reset_name(&mut self) {
-        unimplemented!("Assign a new unique name to the robot.");
+        self.name = gen_random_name()
     }
+}
+
+fn gen_random_name() -> String {
+    let mut rng = rand::thread_rng();
+
+    let letters: String = (0..2).map(|_| rng.gen_range('A'..'Z')).collect();
+    let numbers: String = (0..3).map(|_| rng.gen_range('0'..='9')).collect();
+
+    [letters, numbers].concat()
 }
