e57f3a644a8a25b9559f2d90d64292908f7e2c55
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-06-07 19:02:34 +0300

    Some initial implementations

--- csharp/circular-buffer/CircularBuffer.cs
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -2,28 +2,73 @@
 
 public class CircularBuffer<T>
 {
+    private const int UninitializedIndex = int.MinValue;
+
+    private readonly T[] _innerArray;
+    private int _tailIndex = UninitializedIndex;
+    private int _headIndex = UninitializedIndex;
+
     public CircularBuffer(int capacity)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        _innerArray = new T[capacity];
+
     }
 
     public T Read()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (_headIndex == UninitializedIndex)
+        {
+            throw new InvalidOperationException("No data available.");
+        }
+        var firstBufferedItem = _innerArray[_tailIndex];
+        _tailIndex++;
+        bool wasLastItem = _tailIndex > _headIndex;
+        if (wasLastItem)
+        {
+            // No further data
+            _tailIndex = UninitializedIndex;
+            _headIndex = UninitializedIndex;
+        }
+        return firstBufferedItem;
     }
 
     public void Write(T value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (_headIndex == UninitializedIndex)
+        {
+            _tailIndex = 0;
+            _headIndex = 0;
+            _innerArray[_headIndex] = value;
+            return;
+        }
+        int bufferLength = GetBufferLength();
+        if (bufferLength >= _innerArray.Length)
+        {
+            throw new InvalidOperationException("Buffer is full.");
+        }
+        _headIndex++;
+        _innerArray[_headIndex] = value;
     }
 
     public void Overwrite(T value)
     {
+        int bufferLength = GetBufferLength();
+        if (bufferLength < _innerArray.Length)
+        {
+            Write(value);
+            return;
+        }
         throw new NotImplementedException("You need to implement this function.");
+    }
+
+    private int GetBufferLength()
+    {
+        return (_headIndex - _tailIndex) + 1;
     }
 
     public void Clear()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        _tailIndex = UninitializedIndex;
+        _headIndex = UninitializedIndex;
     }
 }
--- csharp/circular-buffer/CircularBufferTests.cs
+++ csharp/circular-buffer/CircularBufferTests.cs
@@ -3,35 +3,30 @@
 using System;
 using Xunit;
 
-public class CircularBufferTests
-{
+public class CircularBufferTests {
     [Fact]
-    public void Reading_empty_buffer_should_fail()
-    {
+    public void Reading_empty_buffer_should_fail() {
         var buffer = new CircularBuffer<int>(capacity: 1);
         Assert.Throws<InvalidOperationException>(() => buffer.Read());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Can_read_an_item_just_written()
-    {
+    [Fact]
+    public void Can_read_an_item_just_written() {
         var buffer = new CircularBuffer<int>(capacity: 1);
         buffer.Write(1);
         Assert.Equal(1, buffer.Read());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Each_item_may_only_be_read_once()
-    {
+    [Fact]
+    public void Each_item_may_only_be_read_once() {
         var buffer = new CircularBuffer<int>(capacity: 1);
         buffer.Write(1);
         Assert.Equal(1, buffer.Read());
         Assert.Throws<InvalidOperationException>(() => buffer.Read());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Items_are_read_in_the_order_they_are_written()
-    {
+    [Fact]
+    public void Items_are_read_in_the_order_they_are_written() {
         var buffer = new CircularBuffer<int>(capacity: 2);
         buffer.Write(1);
         buffer.Write(2);
@@ -39,17 +34,15 @@ public class CircularBufferTests
         Assert.Equal(2, buffer.Read());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Full_buffer_cant_be_written_to()
-    {
+    [Fact]
+    public void Full_buffer_cant_be_written_to() {
         var buffer = new CircularBuffer<int>(capacity: 1);
         buffer.Write(1);
         Assert.Throws<InvalidOperationException>(() => buffer.Write(2));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void A_read_frees_up_capacity_for_another_write()
-    {
+    [Fact]
+    public void A_read_frees_up_capacity_for_another_write() {
         var buffer = new CircularBuffer<int>(capacity: 1);
         buffer.Write(1);
         Assert.Equal(1, buffer.Read());
@@ -57,9 +50,8 @@ public class CircularBufferTests
         Assert.Equal(2, buffer.Read());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Read_position_is_maintained_even_across_multiple_writes()
-    {
+    [Fact]
+    public void Read_position_is_maintained_even_across_multiple_writes() {
         var buffer = new CircularBuffer<int>(capacity: 3);
         buffer.Write(1);
         buffer.Write(2);
@@ -69,18 +61,16 @@ public class CircularBufferTests
         Assert.Equal(3, buffer.Read());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Items_cleared_out_of_buffer_cant_be_read()
-    {
+    [Fact]
+    public void Items_cleared_out_of_buffer_cant_be_read() {
         var buffer = new CircularBuffer<int>(capacity: 1);
         buffer.Write(1);
         buffer.Clear();
         Assert.Throws<InvalidOperationException>(() => buffer.Read());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Clear_frees_up_capacity_for_another_write()
-    {
+    [Fact]
+    public void Clear_frees_up_capacity_for_another_write() {
         var buffer = new CircularBuffer<int>(capacity: 1);
         buffer.Write(1);
         buffer.Clear();
@@ -88,18 +78,16 @@ public class CircularBufferTests
         Assert.Equal(2, buffer.Read());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Clear_does_nothing_on_empty_buffer()
-    {
+    [Fact]
+    public void Clear_does_nothing_on_empty_buffer() {
         var buffer = new CircularBuffer<int>(capacity: 1);
         buffer.Clear();
         buffer.Write(1);
         Assert.Equal(1, buffer.Read());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Overwrite_acts_like_write_on_non_full_buffer()
-    {
+    [Fact]
+    public void Overwrite_acts_like_write_on_non_full_buffer() {
         var buffer = new CircularBuffer<int>(capacity: 2);
         buffer.Write(1);
         buffer.Overwrite(2);
@@ -107,9 +95,8 @@ public class CircularBufferTests
         Assert.Equal(2, buffer.Read());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Overwrite_replaces_the_oldest_item_on_full_buffer()
-    {
+    [Fact]
+    public void Overwrite_replaces_the_oldest_item_on_full_buffer() {
         var buffer = new CircularBuffer<int>(capacity: 2);
         buffer.Write(1);
         buffer.Write(2);
@@ -118,9 +105,8 @@ public class CircularBufferTests
         Assert.Equal(3, buffer.Read());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Overwrite_replaces_the_oldest_item_remaining_in_buffer_following_a_read()
-    {
+    [Fact]
+    public void Overwrite_replaces_the_oldest_item_remaining_in_buffer_following_a_read() {
         var buffer = new CircularBuffer<int>(capacity: 3);
         buffer.Write(1);
         buffer.Write(2);
@@ -133,9 +119,8 @@ public class CircularBufferTests
         Assert.Equal(5, buffer.Read());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
-    public void Initial_clear_does_not_affect_wrapping_around()
-    {
+    [Fact]
+    public void Initial_clear_does_not_affect_wrapping_around() {
         var buffer = new CircularBuffer<int>(capacity: 2);
         buffer.Clear();
         buffer.Write(1);
