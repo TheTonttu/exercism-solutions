7a400746f58a30e63d6aab08e29ba0d87594d600
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-10-01 20:18:27 +0300

    Add C# Annalyn's Infiltration exercise solution

--- csharp/annalyns-infiltration/AnnalynsInfiltration.cs
+++ csharp/annalyns-infiltration/AnnalynsInfiltration.cs
@@ -4,21 +4,33 @@ static class QuestLogic
 {
     public static bool CanFastAttack(bool knightIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFastAttack() method");
+        return !knightIsAwake;
     }
 
     public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSpy() method");
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
     }
 
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSignalPrisoner() method");
+        return !archerIsAwake && prisonerIsAwake;
     }
 
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFreePrisoner() method");
+        return !archerIsAwake
+            && IsPrisonerQuiet(prisonerIsAwake, petDogIsPresent)
+            && IsKnightOutOfAction(knightIsAwake, petDogIsPresent);
+    }
+
+    private static bool IsPrisonerQuiet(bool prisonerIsAwake, bool petDogIsPresent)
+    {
+        return (prisonerIsAwake || petDogIsPresent);
+    }
+
+    private static bool IsKnightOutOfAction(bool knightIsAwake, bool petDogIsPresent)
+    {
+        return !knightIsAwake || (knightIsAwake && petDogIsPresent);
     }
 }
