ddcd737785bb5905a580bcaf83c7c2c54474cdad
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-10-02 21:52:02 +0300

    Add C# "Booking up for Beauty" exercise solution

--- csharp/booking-up-for-beauty/BookingUpForBeauty.cs
+++ csharp/booking-up-for-beauty/BookingUpForBeauty.cs
@@ -4,26 +4,22 @@ static class Appointment
 {
     public static DateTime Schedule(string appointmentDateDescription)
     {
-        throw new NotImplementedException("Please implement the (static) Appointment.Schedule() method");
+        if (DateTime.TryParse(appointmentDateDescription, out DateTime scheduledTime))
+        {
+            return scheduledTime;
+        }
+        return DateTime.MinValue;
     }
 
-    public static bool HasPassed(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.HasPassed() method");
-    }
+    public static bool HasPassed(DateTime appointmentDate) =>
+        appointmentDate < DateTime.Now;
 
-    public static bool IsAfternoonAppointment(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.IsAfternoonAppointment() method");
-    }
+    public static bool IsAfternoonAppointment(DateTime appointmentDate) =>
+        appointmentDate.Hour is >= 12 and < 18;
 
-    public static string Description(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.Description() method");
-    }
+    public static string Description(DateTime appointmentDate) =>
+        $"You have an appointment on {appointmentDate}.";
 
-    public static DateTime AnniversaryDate()
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.AnniversaryDate() method");
-    }
+    public static DateTime AnniversaryDate() =>
+        new DateTime(DateTime.Now.Year, 9, 15);
 }
