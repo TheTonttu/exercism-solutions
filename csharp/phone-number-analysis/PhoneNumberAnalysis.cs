b6bfc9a509d6dd278b0164723bb5715362e237b7
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-10-02 11:24:20 +0300

    Add C# Phone Number Analysis exercise solution

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -4,11 +4,29 @@ public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        var parsedPhoneNumber = ParsePhoneNumber(phoneNumber);
+        return (IsNewYorkDialingCode(parsedPhoneNumber.DialingCode), IsFakePrefixCode(parsedPhoneNumber.PrefixCode), parsedPhoneNumber.LocalNumber);
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+        return phoneNumberInfo.IsFake;
+    }
+
+    private static (string DialingCode, string PrefixCode, string LocalNumber) ParsePhoneNumber(string phoneNumber)
+    {
+        const char PhoneNumberDelimiter = '-';
+        var phoneNumberParts = phoneNumber.Split(PhoneNumberDelimiter);
+        return (phoneNumberParts[0], phoneNumberParts[1], phoneNumberParts[2]);
+    }
+
+    private static bool IsNewYorkDialingCode(string dialingCode)
+    {
+        return dialingCode == "212";
+    }
+
+    private static bool IsFakePrefixCode(string prefixCode)
+    {
+        return prefixCode == "555";
     }
 }
