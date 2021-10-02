fa041affc051120b4895368b204822aae210b23c
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-10-02 08:13:48 +0300

    Add C# Log Analysis exercise solution

--- csharp/log-analysis/LogAnalysis.cs
+++ csharp/log-analysis/LogAnalysis.cs
@@ -1,12 +1,57 @@
 using System;
 
-public static class LogAnalysis 
+public static class LogAnalysis
 {
-    // TODO: define the 'SubstringAfter()' extension method on the `string` type
+    private const int NotFoundIndex = -1;
 
-    // TODO: define the 'SubstringBetween()' extension method on the `string` type
-    
-    // TODO: define the 'Message()' extension method on the `string` type
+    public static string SubstringAfter(this string str, string delimiter)
+    {
+        if (delimiter is null)
+        {
+            throw new ArgumentNullException(nameof(delimiter));
+        }
 
-    // TODO: define the 'LogLevel()' extension method on the `string` type
+        if (String.IsNullOrEmpty(str)) { return String.Empty; }
+
+        int delimiterIndex = str.IndexOf(delimiter);
+        if (delimiterIndex <= NotFoundIndex) { return String.Empty; }
+
+        int substringIndex = delimiterIndex + delimiter.Length;
+        return str[substringIndex..];
+    }
+
+    public static string SubstringBetween(this string str, string startDelimiter, string endDelimiter)
+    {
+        if (startDelimiter is null)
+        {
+            throw new ArgumentNullException(nameof(startDelimiter));
+        }
+
+        if (endDelimiter is null)
+        {
+            throw new ArgumentNullException(nameof(endDelimiter));
+        }
+
+        if (String.IsNullOrEmpty(str)) { return String.Empty; }
+
+        int startDelimiterIndex = str.IndexOf(startDelimiter);
+        if (startDelimiterIndex <= NotFoundIndex) { return String.Empty; }
+
+        int substringStartIndex = startDelimiterIndex + startDelimiter.Length;
+        int endDelimiterIndex = str.IndexOf(endDelimiter, substringStartIndex);
+        if (endDelimiterIndex <= NotFoundIndex) { return String.Empty; }
+
+        int substringEndIndex = endDelimiterIndex;
+        return str[substringStartIndex..substringEndIndex];
+    }
+
+    public static string Message(this string logLine)
+    {
+        return logLine.SubstringAfter(":").Trim();
+    }
+
+    public static string LogLevel(this string logLine)
+    {
+        return logLine.SubstringBetween("[", "]");
+    }
 }
--- csharp/log-analysis/LogAnalysisTests.cs
+++ csharp/log-analysis/LogAnalysisTests.cs
@@ -10,29 +10,107 @@ public class LogAnalysisTests
         Assert.Equal(" am the 1st test", "I am the 1st test".SubstringAfter("I"));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void SubstringAfter_WithDelimeterOfLengthLongerThan1()
     {
         Assert.Equal(" test", "I am the 2nd test".SubstringAfter("2nd"));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void SubstringBetween()
     {
         Assert.Equal("INFO", "[INFO]: File Deleted.".SubstringBetween("[", "]"));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Message()
     {
         var log = "[WARNING]: Library is deprecated.";
         Assert.Equal("Library is deprecated.", log.Message());
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void LogLevel()
     {
         var log = "[WARNING]: Library is deprecated.";
-        Assert.Equal("WARNING", log.LogLevel());;
+        Assert.Equal("WARNING", log.LogLevel()); ;
     }
+
+    #region Own unit tests
+
+    [Fact]
+    public void SubstringAfter_NoDelimiterMatch()
+    {
+        Assert.Equal(String.Empty, "I am not the 1st test".SubstringAfter("bonk"));
+    }
+
+    [Fact]
+    public void SubstringAfter_DelimiterIsNull()
+    {
+        Assert.Throws<ArgumentNullException>("delimiter", () => "I am not the 1st test".SubstringAfter(null));
+    }
+
+    [Fact]
+    public void SubstringAfter_EmptyDelimiter()
+    {
+        Assert.Equal("I am not the 1st test", "I am not the 1st test".SubstringAfter(String.Empty));
+    }
+
+    [Fact]
+    public void SubstringBetween_NoStartDelimiterMatch()
+    {
+        Assert.Equal(String.Empty, "{INFO]: File Deleted.".SubstringBetween("[", "]"));
+    }
+
+    [Fact]
+    public void SubstringBetween_StartDelimiterIsNull()
+    {
+        Assert.Throws<ArgumentNullException>("startDelimiter", () => "[INFO]: File Deleted.".SubstringBetween(null, "]"));
+    }
+
+    [Fact]
+    public void SubstringBetween_EmptyStartDelimiterMatch()
+    {
+        Assert.Equal("[INFO", "[INFO]: File Deleted.".SubstringBetween(String.Empty, "]"));
+    }
+
+    [Fact]
+    public void SubstringBetween_NoEndDelimiterMatch()
+    {
+        Assert.Equal(String.Empty, "[INFO}: File Deleted.".SubstringBetween("[", "]"));
+    }
+
+    [Fact]
+    public void SubstringBetween_EndDelimiterIsNull()
+    {
+        Assert.Throws<ArgumentNullException>("endDelimiter", () => "[INFO]: File Deleted.".SubstringBetween("[", null));
+    }
+
+    [Fact]
+    public void SubstringBetween_EmptyEndDelimiter()
+    {
+        Assert.Equal(String.Empty, "[INFO]: File Deleted.".SubstringBetween("[", String.Empty));
+    }
+
+    [Fact]
+    public void SubstringBetween_EmptyDelimiters()
+    {
+        Assert.Equal(String.Empty, "[INFO]: File Deleted.".SubstringBetween(String.Empty, String.Empty));
+    }
+
+    [Fact]
+    public void Message_LineIsNull()
+    {
+        string log = null;
+        Assert.Equal(String.Empty, log.Message());
+    }
+
+    [Fact]
+    public void LogLevel_LineIsNull()
+    {
+        string log = null;
+        Assert.Equal(String.Empty, log.LogLevel());
+    }
+
+    #endregion
 }
