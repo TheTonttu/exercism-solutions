fda4993ef2c05bb671b1f0f067f2715ad9ffb9e1
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-10-02 17:19:46 +0300

    Add C# "Elon's Toys" exercise solution

--- csharp/elons-toys/ElonsToys.cs
+++ csharp/elons-toys/ElonsToys.cs
@@ -2,23 +2,36 @@ using System;
 
 class RemoteControlCar
 {
+    private int _batteryPercentage = 100;
+    private int _driveDistanceInMeters = 0;
+
     public static RemoteControlCar Buy()
     {
-        throw new NotImplementedException("Please implement the (static) RemoteControlCar.Buy() method");
+        return new RemoteControlCar();
     }
 
     public string DistanceDisplay()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDisplay() method");
+        return $"Driven {_driveDistanceInMeters} meters";
     }
 
     public string BatteryDisplay()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDisplay() method");
+        if (IsBatteryEmpty()) { return "Battery empty"; }
+
+        return $"Battery at {_batteryPercentage}%";
     }
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+        if (IsBatteryEmpty()) { return; }
+
+        _batteryPercentage -= 1;
+        _driveDistanceInMeters += 20;
+    }
+
+    private bool IsBatteryEmpty()
+    {
+        return _batteryPercentage <= 0;
     }
 }
