abc1dad6d791c9352abdcc76d1572155b8032fa2
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-10-02 13:25:00 +0300

    Add C# "Interest is Interesting" exercise solution

--- csharp/interest-is-interesting/InterestIsInteresting.cs
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -4,21 +4,35 @@ static class SavingsAccount
 {
     public static float InterestRate(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
+        if (balance >= 5000) { return 2.475f; }
+        if (balance >= 1000) { return 1.621f; }
+        if (balance >= 0) { return 0.5f; }
+        return -3.213f;
     }
 
     public static decimal Interest(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
+        return balance * InterestPercentage(balance);
     }
 
     public static decimal AnnualBalanceUpdate(decimal balance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
+        return balance + Interest(balance);
     }
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        int years = 0;
+        while (balance < targetBalance)
+        {
+            balance = AnnualBalanceUpdate(balance);
+            years++;
+        }
+        return years;
+    }
+
+    private static decimal InterestPercentage(decimal balance)
+    {
+        return (decimal)Math.Abs(InterestRate(balance) / 100.0f);
     }
 }
