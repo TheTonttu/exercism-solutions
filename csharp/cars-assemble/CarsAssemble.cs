01037a7e0a2443b1c7c7213d1e8cbe2c9de90805
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-10-02 12:06:53 +0300

    Add C# "Cars, Assemble!" exercise solution

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -4,11 +4,65 @@ static class AssemblyLine
 {
     public static double ProductionRatePerHour(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+        const int BaseProductionRatePerHour = 221;
+        return speed * BaseProductionRatePerHour * SuccessRate(speed);
     }
 
     public static int WorkingItemsPerMinute(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+        const int MinutesInHour = 60;
+        return (int)(ProductionRatePerHour(speed) / MinutesInHour);
     }
+
+    private static double SuccessRate(int speed)
+    {
+        const double UnsupportedSuccessRate = 0.0;
+
+        if (1 <= speed && speed <= 4)
+        {
+            return 1.0;
+        }
+        else if (5 <= speed && speed <= 8)
+        {
+            return 0.9;
+        }
+        else if (speed == 9)
+        {
+            return 0.8;
+        }
+        else if (speed == 10)
+        {
+            return 0.77;
+        }
+        return UnsupportedSuccessRate;
+    }
+
+    #region Alternative Success Rate Table Lookup
+
+    private static readonly double[] SuccessRateLookupTable =
+    {
+        0.0,
+        // 1-4
+        1.0, 1.0, 1.0, 1.0,
+        // 5-8
+        0.9, 0.9, 0.9, 0.9,
+        // 9
+        0.8,
+        // 10
+        0.77
+    };
+
+    private static double SuccessRateFromLookupTable(int speed)
+    {
+        const double UnsupportedSuccessRate = 0.0;
+
+        int maxSpeed = SuccessRateLookupTable.Length - 1;
+        if (0 <= speed && speed <= maxSpeed)
+        {
+            return SuccessRateLookupTable[speed];
+        }
+        return UnsupportedSuccessRate;
+    }
+
+    #endregion 
 }
