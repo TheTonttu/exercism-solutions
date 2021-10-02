ec410005afadfa50dad58b83950da506a1f862a3
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-10-02 17:33:15 +0300

    Mess with the code structure for no reason

--- csharp/elons-toys/ElonsToys.cs
+++ csharp/elons-toys/ElonsToys.cs
@@ -5,22 +5,15 @@ class RemoteControlCar
     private int _batteryPercentage = 100;
     private int _driveDistanceInMeters = 0;
 
-    public static RemoteControlCar Buy()
-    {
-        return new RemoteControlCar();
-    }
+    public static RemoteControlCar Buy() => new();
 
-    public string DistanceDisplay()
-    {
-        return $"Driven {_driveDistanceInMeters} meters";
-    }
+    public string DistanceDisplay() =>
+        $"Driven {_driveDistanceInMeters} meters";
 
-    public string BatteryDisplay()
-    {
-        if (IsBatteryEmpty()) { return "Battery empty"; }
-
-        return $"Battery at {_batteryPercentage}%";
-    }
+    public string BatteryDisplay() =>
+        IsBatteryEmpty()
+        ? "Battery empty"
+        : $"Battery at {_batteryPercentage}%";
 
     public void Drive()
     {
@@ -30,8 +23,6 @@ class RemoteControlCar
         _driveDistanceInMeters += 20;
     }
 
-    private bool IsBatteryEmpty()
-    {
-        return _batteryPercentage <= 0;
-    }
+    private bool IsBatteryEmpty() =>
+        _batteryPercentage <= 0;
 }
