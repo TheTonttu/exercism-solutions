38f50e7faa0775d42f28c429f417b9db0db179da
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-10-03 18:05:11 +0300

    Add C# "Squeaky Clean" exercise solution

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -1,9 +1,52 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
 
 public static class Identifier
 {
+    private const string ControlCharReplacement = "CTRL";
+    private const string WhiteSpaceReplacement = "_";
+
+    private static readonly HashSet<char> GreekLowerCaseLetters =
+        Enumerable.Range('α', 'ω' - 'α' + 1)
+                  .Select(i => (char)i)
+                  .ToHashSet();
+
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        var cleanIdBuilder = new StringBuilder();
+        char? prevChar = null;
+        foreach (char c in identifier)
+        {
+            if (char.IsControl(c))
+            {
+                cleanIdBuilder.Append(ControlCharReplacement);
+            }
+            else if (char.IsWhiteSpace(c))
+            {
+                cleanIdBuilder.Append(WhiteSpaceReplacement);
+            }
+            else if (char.IsLetter(c))
+            {
+                char letter = c;
+
+                bool isKebabCase = prevChar == '-';
+                if (isKebabCase)
+                {
+                    letter = char.ToUpperInvariant(letter);
+                }
+
+                if (!GreekLowerCaseLetters.Contains(letter))
+                {
+                    cleanIdBuilder.Append(letter);
+                }
+
+            }
+
+            prevChar = c;
+        }
+        return cleanIdBuilder.ToString();
     }
 }
