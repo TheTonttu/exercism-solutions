ef16c46c6984ac51f9897da7ff92b81d37394e8b
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-10-05 20:03:59 +0300

    Pass ToBuffer test cases

--- csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
+++ csharp/hyper-optimized-telemetry/HyperOptimizedTelemetry.cs
@@ -4,7 +4,90 @@ public static class TelemetryBuffer
 {
     public static byte[] ToBuffer(long reading)
     {
-        throw new NotImplementedException("Please implement the static TelemetryBuffer.ToBuffer() method");
+        const int PrefixByteIndex = 0;
+        const short SignedPrefixIdentifier = 256;
+
+        var bytes = new byte[9];
+        var extractedBytes = BitConverter.GetBytes(reading);
+        extractedBytes.CopyTo(bytes, 1);
+        if (reading > 0)
+        {
+            if (FitsPositiveLong(reading))
+            {
+                bytes[PrefixByteIndex] = SignedPrefixIdentifier - 8;
+            }
+            else if (FitsUInt32(reading))
+            {
+                bytes[PrefixByteIndex] = 4;
+            }
+            else if (FitPositiveInt32(reading))
+            {
+                bytes[PrefixByteIndex] = SignedPrefixIdentifier - 4;
+            }
+            else if (FitsUInt16(reading))
+            {
+                bytes[PrefixByteIndex] = 2;
+            }
+            else
+            {
+                bytes[PrefixByteIndex] = SignedPrefixIdentifier - 2;
+            }
+        }
+        else
+        {
+            if (FitsNegativeLong(reading))
+            {
+                bytes[PrefixByteIndex] = SignedPrefixIdentifier - 8;
+            }
+            else if (FitsNegativeInt32(reading))
+            {
+                bytes[PrefixByteIndex] = SignedPrefixIdentifier - 4;
+                for (int i = 5; i < bytes.Length; i++)
+                {
+                    bytes[i] = 0;
+                }
+            }
+            else
+            {
+                bytes[PrefixByteIndex] = 256 - 2;
+                for (int i = 3; i < bytes.Length; i++)
+                {
+                    bytes[i] = 0;
+                }
+            }
+        }
+
+        return bytes;
+    }
+
+    private static bool FitsNegativeInt32(long reading)
+    {
+        return reading < Int16.MinValue;
+    }
+
+    private static bool FitsNegativeLong(long reading)
+    {
+        return reading < Int32.MinValue;
+    }
+
+    private static bool FitsUInt16(long reading)
+    {
+        return reading > Int16.MaxValue;
+    }
+
+    private static bool FitPositiveInt32(long reading)
+    {
+        return reading > UInt16.MaxValue;
+    }
+
+    private static bool FitsUInt32(long reading)
+    {
+        return reading > Int32.MaxValue;
+    }
+
+    private static bool FitsPositiveLong(long reading)
+    {
+        return reading > UInt32.MaxValue;
     }
 
     public static long FromBuffer(byte[] buffer)
