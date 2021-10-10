59877d350b027db89ffff8e3d877ac9cf073c6a0
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2021-10-10 12:54:52 +0300

    Add C# "International Calling Connoisseur" exercise solution

--- csharp/international-calling-connoisseur/InternationalCallingConnoisseur.cs
+++ csharp/international-calling-connoisseur/InternationalCallingConnoisseur.cs
@@ -1,52 +1,60 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class DialingCodes
 {
-    public static Dictionary<int, string> GetEmptyDictionary()
-    {
-        throw new NotImplementedException($"Please implement the (static) GetEmptyDictionary() method");
-    }
+    public static Dictionary<int, string> GetEmptyDictionary() => new();
 
-    public static Dictionary<int, string> GetExistingDictionary()
-    {
-        throw new NotImplementedException($"Please implement the (static) GetExistingDictionary() method");
-    }
+    public static Dictionary<int, string> GetExistingDictionary() =>
+        new()
+        {
+            { 1, "United States of America" },
+            { 55, "Brazil" },
+            { 91, "India" },
+        };
 
-    public static Dictionary<int, string> AddCountryToEmptyDictionary(int CountryCode, string CountryName)
-    {
-        throw new NotImplementedException($"Please implement the (static) AddCountryToEmptyDictionary() method");
-    }
+    public static Dictionary<int, string> AddCountryToEmptyDictionary(int CountryCode, string CountryName) =>
+        new()
+        {
+            { CountryCode, CountryName },
+        };
 
     public static Dictionary<int, string> AddCountryToExistingDictionary(
-        Dictionary<int, string> existingDictionary, int countryCode, string CountryName)
+        Dictionary<int, string> existingDictionary, int countryCode, string countryName)
     {
-        throw new NotImplementedException($"Please implement the (static) AddCountryToExistingDictionary() method");
+        existingDictionary.Add(countryCode, countryName);
+        return existingDictionary;
     }
 
     public static string GetCountryNameFromDictionary(
         Dictionary<int, string> existingDictionary, int countryCode)
     {
-        throw new NotImplementedException($"Please implement the (static) GetCountryNameFromDictionary() method");
+        if (existingDictionary.TryGetValue(countryCode, out string existingCountryCode))
+        {
+            return existingCountryCode;
+        }
+        return String.Empty;
     }
     public static Dictionary<int, string> UpdateDictionary(
         Dictionary<int, string> existingDictionary, int countryCode, string countryName)
     {
-        throw new NotImplementedException($"Please implement the (static) UpdateDictionary() method");
+        if (existingDictionary.ContainsKey(countryCode))
+        {
+            existingDictionary[countryCode] = countryName;
+        }
+        return existingDictionary;
     }
 
     public static Dictionary<int, string> RemoveCountryFromDictionary(
         Dictionary<int, string> existingDictionary, int countryCode)
     {
-        throw new NotImplementedException($"Please implement the (static) RemoveCountryFromDictionary() method");
+        existingDictionary.Remove(countryCode);
+        return existingDictionary;
     }
-    public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode)
-    {
-        throw new NotImplementedException($"Please implement the (static) CheckCodeExists() method");
-    }
+    public static bool CheckCodeExists(Dictionary<int, string> existingDictionary, int countryCode) =>
+        existingDictionary.ContainsKey(countryCode);
 
-    public static string FindLongestCountryName(Dictionary<int, string> existingDictionary)
-    {
-        throw new NotImplementedException($"Please implement the (static) FindLongestCountryName() method");
-    }
+    public static string FindLongestCountryName(Dictionary<int, string> existingDictionary) =>
+        existingDictionary.Values.Aggregate(String.Empty, (longest, current) => longest.Length > current.Length ? longest : current);
 }
