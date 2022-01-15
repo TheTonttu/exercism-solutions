735a8114216fa33c87e1d8b8adf77a3002472e8e
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-01-15 14:52:38 +0200

    Add Rust "Assembly Line" exercise solution

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,11 +1,19 @@
-// This stub file contains items which aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
+const PRODUCTION_RATE_PER_HOUR: f64 = 221.0;
+const MINUTES_IN_HOUR: f64 = 60.0;
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    (speed as f64) * PRODUCTION_RATE_PER_HOUR *  success_rate(speed)
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    (production_rate_per_hour(speed) / MINUTES_IN_HOUR) as u32
+}
+
+fn success_rate(speed: u8) -> f64 {
+    match speed {
+        1..=4 => 1.0,
+        5..=8 => 0.9,
+        9..=10 => 0.77,
+        _ => 0.0
+    }
 }
