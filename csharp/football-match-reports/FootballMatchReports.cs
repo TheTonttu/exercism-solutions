a349ae691a1479a9eee6c65a7f49f74a06a4a974
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-05-06 20:26:50 +0300

    Add C# "Football Match Reports" exercise solution

--- csharp/football-match-reports/FootballMatchReports.cs
+++ csharp/football-match-reports/FootballMatchReports.cs
@@ -2,13 +2,34 @@ using System;
 
 public static class PlayAnalyzer
 {
-    public static string AnalyzeOnField(int shirtNum)
+    public static string AnalyzeOnField(int shirtNum) => shirtNum switch
     {
-        throw new NotImplementedException($"Please implement the (static) PlayAnalyzer.AnalyzeOnField() method");
-    }
+        1 => "goalie",
+        2 => "left back",
+        3 or 4 => "center back",
+        5 => "right back",
+        >= 6 and <= 8 => "midfielder",
+        9 => "left wing",
+        10 => "striker",
+        11 => "right wing",
+        _ => throw new ArgumentOutOfRangeException(nameof(shirtNum)),
+    };
 
-    public static string AnalyzeOffField(object report)
+    public static string AnalyzeOffField(object report) => report switch
     {
-        throw new NotImplementedException($"Please implement the (static) PlayAnalyzer.AnalyzeOffField() method");
-    }
+        int supporterCount => $"There are {supporterCount} supporters at the match.",
+        string reportMessage => reportMessage,
+        Manager manager => GetManagerDescription(manager),
+        Injury injury => $"Oh no! {injury.GetDescription()} Medics are on the field.",
+        Incident incident => incident.GetDescription(),
+        null => throw new ArgumentNullException(nameof(report)),
+        _ => throw new ArgumentException("Unsupported type", nameof(report)),
+    };
+
+    private static string GetManagerDescription(Manager manager) => manager switch
+    {
+        { Club: var club } when String.IsNullOrWhiteSpace(club) => manager.Name,
+        { } => $"{manager.Name} ({manager.Club})",
+        _ => String.Empty,
+    };
 }
