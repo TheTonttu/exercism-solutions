8d903b37c2b1840daece38e3ed16affe7a044cae
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-05-07 12:47:48 +0300

    Add C# "Logs, Logs, Logs!" exercise solution

--- csharp/logs-logs-logs/LogsLogsLogs.cs
+++ csharp/logs-logs-logs/LogsLogsLogs.cs
@@ -1,16 +1,32 @@
 using System;
 
-// TODO: define the 'LogLevel' enum
+public enum LogLevel
+{
+    Unknown = 0,
+    Trace = 1,
+    Debug = 2,
+    Info = 4,
+    Warning = 5,
+    Error = 6,
+    Fatal = 42,
+}
 
 static class LogLine
 {
-    public static LogLevel ParseLogLevel(string logLine)
+    public static LogLevel ParseLogLevel(string logLine) => logLine switch
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.ParseLogLevel() method");
-    }
+        string trace when IsMatch("[TRC]", trace) => LogLevel.Trace,
+        string debug when IsMatch("[DBG]", debug) => LogLevel.Debug,
+        string info when IsMatch("[INF]", info) => LogLevel.Info,
+        string warning when IsMatch("[WRN]", warning) => LogLevel.Warning,
+        string error when IsMatch("[ERR]", error) => LogLevel.Error,
+        string fatal when IsMatch("[FTL]", fatal) => LogLevel.Fatal,
+        _ => LogLevel.Unknown,
+    };
 
-    public static string OutputForShortLog(LogLevel logLevel, string message)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.OutputForShortLog() method");
-    }
+    public static string OutputForShortLog(LogLevel logLevel, string message) =>
+        $"{(int)logLevel}:{message}";
+
+    private static bool IsMatch(string value, string logLine) =>
+        logLine.StartsWith(value, StringComparison.OrdinalIgnoreCase);
 }
