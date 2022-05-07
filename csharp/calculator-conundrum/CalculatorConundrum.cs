7e65263e526856c152fe5df230dae3e2f5c6154e
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-05-07 11:30:05 +0300

    Add C# "Calculator Conundrum" exercise solution

--- csharp/calculator-conundrum/CalculatorConundrum.cs
+++ csharp/calculator-conundrum/CalculatorConundrum.cs
@@ -2,8 +2,44 @@ using System;
 
 public static class SimpleCalculator
 {
+    private enum Operation
+    {
+        Addition,
+        Multiplication,
+        Division,
+    }
+
     public static string Calculate(int operand1, int operand2, string operation)
     {
-        throw new NotImplementedException("Please implement the SimpleCalculator.Calculate() method");
+        try
+        {
+            int calculationResult = PerformCalculation(operand1, operand2, ParseOperation(operation));
+            return FormatCalculation(operand1, operand2, operation, calculationResult);
+        }
+        catch (DivideByZeroException)
+        {
+            return "Division by zero is not allowed.";
+        }
     }
+
+    private static Operation ParseOperation(string operation) => operation switch
+    {
+        "+" => Operation.Addition,
+        "*" => Operation.Multiplication,
+        "/" => Operation.Division,
+        "" => throw new ArgumentException($"'{nameof(operation)}' cannot be empty.", nameof(operation)),
+        null => throw new ArgumentNullException(nameof(operation)),
+        _ => throw new ArgumentOutOfRangeException(nameof(operation)),
+    };
+
+    private static int PerformCalculation(int operand1, int operand2, Operation operation) => operation switch
+    {
+        Operation.Addition => SimpleOperation.Addition(operand1, operand2),
+        Operation.Multiplication => SimpleOperation.Multiplication(operand1, operand2),
+        Operation.Division => SimpleOperation.Division(operand1, operand2),
+        _ => throw new NotSupportedException(operation.ToString())
+    };
+
+    private static string FormatCalculation(int operand1, int operand2, string operation, int result) =>
+        $"{operand1} {operation} {operand2} = {result}";
 }
