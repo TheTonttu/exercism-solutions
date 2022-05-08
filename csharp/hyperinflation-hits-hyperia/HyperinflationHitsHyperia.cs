f6942acdef015c2c07aa48147eba5baec3b1742c
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-05-08 20:22:48 +0300

    Add C# "Hyperinflation Hits Hyperia" exercise solution

--- csharp/hyperinflation-hits-hyperia/HyperinflationHitsHyperia.cs
+++ csharp/hyperinflation-hits-hyperia/HyperinflationHitsHyperia.cs
@@ -2,18 +2,49 @@ using System;
 
 public static class CentralBank
 {
+    private const string OverflowMessage = "*** Too Big ***";
+
     public static string DisplayDenomination(long @base, long multiplier)
     {
-        throw new NotImplementedException($"Please implement the (static) CentralBank.DisplayDenomination() method");
+        try
+        {
+            long denomination = checked(@base * multiplier);
+            return denomination.ToString();
+        }
+        catch (OverflowException)
+        {
+            return OverflowMessage;
+        }
     }
 
     public static string DisplayGDP(float @base, float multiplier)
     {
-        throw new NotImplementedException($"Please implement the (static) CentralBank.DisplayGDP() method");
+        try
+        {
+            float gdp = checked(@base * multiplier);
+            if (float.IsInfinity(gdp))
+            {
+                return OverflowMessage;
+            }
+            return gdp.ToString();
+        }
+        catch (OverflowException)
+        {
+            return OverflowMessage;
+        }
     }
 
     public static string DisplayChiefEconomistSalary(decimal salaryBase, decimal multiplier)
     {
-        throw new NotImplementedException($"Please implement the (static) CentralBank.DisplayChiefEconomistSalary() method");
+        const string TooBigSalaryMessage = "*** Much Too Big ***";
+        try
+        {
+            decimal salary = checked(salaryBase * multiplier);
+            return salary.ToString();
+        }
+        catch (OverflowException)
+        {
+            return TooBigSalaryMessage;
+        }
     }
 }
