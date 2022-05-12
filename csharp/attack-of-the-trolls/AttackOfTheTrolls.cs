7408ef6ffcf7618aebdf423129af496c7f0739ef
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-05-12 20:05:44 +0300

    Add C# "Attack of the Trolls" exercise solution

--- csharp/attack-of-the-trolls/AttackOfTheTrolls.cs
+++ csharp/attack-of-the-trolls/AttackOfTheTrolls.cs
@@ -1,28 +1,35 @@
 using System;
 
-// TODO: define the 'AccountType' enum
+public enum AccountType
+{
+    Guest,
+    User,
+    Moderator
+}
 
-// TODO: define the 'Permission' enum
+[Flags]
+public enum Permission
+{
+    None   = 0,
+    Read   = 0b001,
+    Write  = 0b010,
+    Delete = 0b100,
+    All    = 0b111 // or ~(~0 << 3)
+}
 
 static class Permissions
 {
-    public static Permission Default(AccountType accountType)
+    public static Permission Default(AccountType accountType) => accountType switch
     {
-        throw new NotImplementedException("Please implement the (static) Permissions.Default() method");
-    }
+        AccountType.Guest => Permission.Read,
+        AccountType.User => Permission.Read | Permission.Write,
+        AccountType.Moderator => Permission.All,
+        _ => Permission.None
+    };
 
-    public static Permission Grant(Permission current, Permission grant)
-    {
-        throw new NotImplementedException("Please implement the (static) Permissions.Grant() method");
-    }
+    public static Permission Grant(Permission current, Permission grant) => current |= grant;
 
-    public static Permission Revoke(Permission current, Permission revoke)
-    {
-        throw new NotImplementedException("Please implement the (static) Permissions.Revoke() method");
-    }
+    public static Permission Revoke(Permission current, Permission revoke) => current &= ~revoke;
 
-    public static bool Check(Permission current, Permission check)
-    {
-        throw new NotImplementedException("Please implement the (static) Permissions.Check() method");
-    }
+    public static bool Check(Permission current, Permission check) => current.HasFlag(check);
 }
