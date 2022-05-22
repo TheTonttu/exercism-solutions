182bba88c904e613708ca03874f45aeb682f6f69
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-05-22 11:08:57 +0300

    Add C# "Parsing Log Files" exercise solution

--- csharp/parsing-log-files/ParsingLogFiles.cs
+++ csharp/parsing-log-files/ParsingLogFiles.cs
@@ -1,30 +1,48 @@
-using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 public class LogParser
 {
+    private static readonly Regex ValidLineRegex = new(@"^\[(TRC|DBG|INF|WRN|ERR|FTL)\].*$", RegexOptions.Compiled);
+    private static readonly Regex SplitRegex = new(@"<[\^\*=-]+>", RegexOptions.Compiled);
+    private static readonly Regex QuotedPasswordCountRegex = new("\"[^\"]*password[^\"]*\"", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+    private static readonly Regex EndOfLineRemoveRegex = new("end-of-line[0-9]+", RegexOptions.Compiled);
+    private static readonly Regex PasswordMatchRegex = new(@"password\S+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
     public bool IsValidLine(string text)
     {
-        throw new NotImplementedException($"Please implement the LogParser.IsValidLine() method");
+        return ValidLineRegex.IsMatch(text);
     }
 
     public string[] SplitLogLine(string text)
     {
-        throw new NotImplementedException($"Please implement the LogParser.SplitLogLine() method");
+        return SplitRegex.Split(text);
     }
 
     public int CountQuotedPasswords(string lines)
     {
-        throw new NotImplementedException($"Please implement the LogParser.CountQuotedPasswords() method");
+        var matches = QuotedPasswordCountRegex.Matches(lines);
+        return matches.Count;
     }
 
     public string RemoveEndOfLineText(string line)
     {
-        throw new NotImplementedException($"Please implement the LogParser.RemoveEndOfLineText() method");
+        return EndOfLineRemoveRegex.Replace(line, string.Empty);
     }
 
     public string[] ListLinesWithPasswords(string[] lines)
     {
-        throw new NotImplementedException($"Please implement the LogParser.ListLinesWithPasswords() method");
+        return lines
+            .Select(ProcessLine)
+            .ToArray();
+    }
+
+    private static string ProcessLine(string line)
+    {
+        var match = PasswordMatchRegex.Match(line);
+        string password = match.Value;
+        return match.Success
+            ? $"{password}: {line}"
+            : $"--------: {line}";
     }
 }
