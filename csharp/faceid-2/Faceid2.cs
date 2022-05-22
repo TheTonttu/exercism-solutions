d71d3dc657ea4c6aca4557f74eb517d23bb83ed5
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-05-22 12:01:58 +0300

    Add C# "Face ID 2.0" exercise solution

--- csharp/faceid-2/Faceid2.cs
+++ csharp/faceid-2/Faceid2.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 
-public class FacialFeatures
+// public record FacialFeatures(string EyeColor, decimal PhiltrumWidth);
+public class FacialFeatures : IEquatable<FacialFeatures>
 {
     public string EyeColor { get; }
     public decimal PhiltrumWidth { get; }
@@ -10,10 +12,43 @@ public class FacialFeatures
         EyeColor = eyeColor;
         PhiltrumWidth = philtrumWidth;
     }
-    // TODO: implement equality and GetHashCode() methods
+
+    public bool Equals(FacialFeatures? other)
+    {
+        return other != null &&
+            (
+                ReferenceEquals(this, other)
+                ||
+                this.EyeColor == other.EyeColor &&
+                this.PhiltrumWidth == other.PhiltrumWidth
+            );
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is FacialFeatures other
+            && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(EyeColor, PhiltrumWidth);
+    }
+
+    public static bool operator ==(FacialFeatures? lhs, FacialFeatures? rhs)
+    {
+        return lhs is null && rhs is null
+            || Equals(lhs, rhs);
+    }
+
+    public static bool operator !=(FacialFeatures? lhs, FacialFeatures? rhs)
+    {
+        return !(lhs == rhs);
+    }
 }
 
-public class Identity
+// public record Identity(string Email, FacialFeatures FacialFeatures);
+public class Identity : IEquatable<Identity>
 {
     public string Email { get; }
     public FacialFeatures FacialFeatures { get; }
@@ -23,33 +58,69 @@ public class Identity
         Email = email;
         FacialFeatures = facialFeatures;
     }
-    // TODO: implement equality and GetHashCode() methods
+
+    public bool Equals(Identity? other)
+    {
+        return other != null &&
+            (
+                ReferenceEquals(this, other)
+                ||
+                this.Email == other.Email &&
+                this.FacialFeatures == other.FacialFeatures
+            );
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is Identity other
+            && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Email, FacialFeatures);
+    }
+
+    public static bool operator ==(Identity? lhs, Identity? rhs)
+    {
+        return lhs is null && rhs is null
+            || Equals(lhs, rhs);
+    }
+
+    public static bool operator !=(Identity? lhs, Identity? rhs)
+    {
+        return !(lhs == rhs);
+    }
 }
 
 public class Authenticator
 {
+    private static readonly Identity Admin = new("admin@exerc.ism", new FacialFeatures("green", 0.9m));
+
+    private readonly HashSet<Identity> RegisteredIdentities = new();
+
     public static bool AreSameFace(FacialFeatures faceA, FacialFeatures faceB)
     {
-        throw new NotImplementedException("Please implement the (static) Authenticator.AreSameFace() method");
+        return faceA == faceB;
     }
 
     public bool IsAdmin(Identity identity)
     {
-        throw new NotImplementedException("Please implement the Authenticator.IsAdmin() method");
+        return identity == Admin;
     }
 
     public bool Register(Identity identity)
     {
-        throw new NotImplementedException("Please implement the Authenticator.Register() method");
+        return RegisteredIdentities.Add(identity);
     }
 
     public bool IsRegistered(Identity identity)
     {
-        throw new NotImplementedException("Please implement the Authenticator.IsRegistered() method");
+        return RegisteredIdentities.Contains(identity);
     }
 
     public static bool AreSameObject(Identity identityA, Identity identityB)
     {
-        throw new NotImplementedException("Please implement the Authenticator.AreSameObject() method");
+        return ReferenceEquals(identityA, identityB);
     }
 }
