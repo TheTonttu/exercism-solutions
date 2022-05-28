815bc3d85c2f9f6fa74b2dbd33c8cf146de45732
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-05-28 15:47:13 +0300

    Update C# "Cars, Assemble!" exercise to latest iteration

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -1,68 +1,18 @@
-using System;
-
 static class AssemblyLine
 {
-    public static double ProductionRatePerHour(int speed)
-    {
-        const int BaseProductionRatePerHour = 221;
-        return speed * BaseProductionRatePerHour * SuccessRate(speed);
-    }
+    private const int BaseProductionRatePerHour = 221;
+    private const int MinutesInHour = 60;
 
-    public static int WorkingItemsPerMinute(int speed)
-    {
-        const int MinutesInHour = 60;
-        return (int)(ProductionRatePerHour(speed) / MinutesInHour);
-    }
-
-    private static double SuccessRate(int speed)
-    {
-        const double UnsupportedSuccessRate = 0.0;
-
-        if (1 <= speed && speed <= 4)
+    public static double SuccessRate(int speed) =>
+        speed switch
         {
-            return 1.0;
-        }
-        else if (5 <= speed && speed <= 8)
-        {
-            return 0.9;
-        }
-        else if (speed == 9)
-        {
-            return 0.8;
-        }
-        else if (speed == 10)
-        {
-            return 0.77;
-        }
-        return UnsupportedSuccessRate;
-    }
+            >= 1 and <= 4 => 1.0,
+            >= 5 and <= 8 => 0.9,
+            9 => 0.8,
+            10 => 0.77,
+            _ => 0.0
+        };
 
-    #region Alternative Success Rate Table Lookup
-
-    private static readonly double[] SuccessRateLookupTable =
-    {
-        0.0,
-        // 1-4
-        1.0, 1.0, 1.0, 1.0,
-        // 5-8
-        0.9, 0.9, 0.9, 0.9,
-        // 9
-        0.8,
-        // 10
-        0.77
-    };
-
-    private static double SuccessRateFromLookupTable(int speed)
-    {
-        const double UnsupportedSuccessRate = 0.0;
-
-        int maxSpeed = SuccessRateLookupTable.Length - 1;
-        if (0 <= speed && speed <= maxSpeed)
-        {
-            return SuccessRateLookupTable[speed];
-        }
-        return UnsupportedSuccessRate;
-    }
-
-    #endregion 
+    public static double ProductionRatePerHour(int speed) => speed * BaseProductionRatePerHour * SuccessRate(speed);
+    public static int WorkingItemsPerMinute(int speed) => (int)(ProductionRatePerHour(speed) / MinutesInHour);
 }
