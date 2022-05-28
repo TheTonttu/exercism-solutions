89846f37ea6ab42fb9db0c29e2d53f3934d9092d
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-05-28 16:18:35 +0300

    Update Rust "Gigasecond" exercise to latest iteration

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,9 +1,8 @@
-use chrono::{DateTime, Duration, Utc};
-use std::ops::Add;
+use time::{Duration, PrimitiveDateTime as DateTime};
 
 const GIGASECOND: i64 = 1_000_000_000;
 
-// Returns a Utc DateTime one billion seconds after start.
-pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    start.add(Duration::seconds(GIGASECOND))
+// Returns a DateTime one billion seconds after start.
+pub fn after(start: DateTime) -> DateTime {
+    start + Duration::seconds(GIGASECOND)
 }
