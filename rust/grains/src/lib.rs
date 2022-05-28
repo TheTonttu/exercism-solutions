d118dec33b89319b7f251294b484aba52660e3ff
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-05-28 16:48:19 +0300

    Update Rust "Grains" exercise to latest iteration

--- rust/grains/src/lib.rs
+++ rust/grains/src/lib.rs
@@ -2,7 +2,7 @@ const MIN_SQUARES: u32 = 1;
 const MAX_SQUARES: u32 = 64;
 
 pub fn square(s: u32) -> u64 {
-    assert!(MIN_SQUARES <= s && s <= MAX_SQUARES, format!("Square must be between {min} and {max}", min = MIN_SQUARES, max = MAX_SQUARES));
+    assert!(MIN_SQUARES <= s && s <= MAX_SQUARES, "Square must be between {} and {}", MIN_SQUARES, MAX_SQUARES);
 
     2u64.pow(s - 1)
 }
