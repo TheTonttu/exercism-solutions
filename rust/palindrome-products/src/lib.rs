c43615bc3e6b780feabcb56a6c7cd697f4b84420
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-05-29 10:21:36 +0300

    Get rid of most of the casting in FactorPairGenerator

--- rust/palindrome-products/src/lib.rs
+++ rust/palindrome-products/src/lib.rs
@@ -1,7 +1,7 @@
 pub struct FactorPairGenerator {
     step: i128,
-    start: i128,
-    end: i128,
+    start: u64,
+    end: u64,
     current: Option<(u64, u64)>,
 }
 
@@ -11,8 +11,8 @@ impl FactorPairGenerator {
 
         Self {
             step,
-            start: start as i128,
-            end: end as i128,
+            start,
+            end,
             current: None,
         }
     }
@@ -23,9 +23,9 @@ impl Iterator for FactorPairGenerator {
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.current {
-            Some((a, b)) if a as i128 == self.end && b as i128 == self.end => None,
-            Some((a, b)) if b as i128 == self.end => {
-                self.current = Some(((a as i128 + self.step) as u64, self.start as u64));
+            Some((a, b)) if a == self.end && b == self.end => None,
+            Some((a, b)) if b == self.end => {
+                self.current = Some(((a as i128 + self.step) as u64, self.start));
                 self.current
             }
             Some((a, b)) => {
@@ -34,7 +34,7 @@ impl Iterator for FactorPairGenerator {
             }
             // Init
             None => {
-                self.current = Some((self.start as u64, self.start as u64));
+                self.current = Some((self.start, self.start));
                 self.current
             }
         }
@@ -82,19 +82,18 @@ pub fn palindrome_products(min: u64, max: u64) -> Option<(Palindrome, Palindrome
 
     let mut min_gen = FactorPairGenerator::new(min, max);
 
-    let min_pal =
-        min_gen.find_map(|(a, b)| Palindrome::new(a * b));
+    let min_palindrome = min_gen.find_map(|(a, b)| Palindrome::new(a * b));
 
     let max_gen = FactorPairGenerator::new(max, min);
 
-    let max_pal = max_gen
+    let max_palindrome = max_gen
         .filter_map(|(a, b)| Palindrome::new(a * b))
         // HACK: max -> min palindromic products are not generated in largest first order so collect couple values first and then take the largest from them.
         // Collecting 3 is enough to pass the unit tests but might need to increase the number for other scenarios.
         .take(3)
         .max();
 
-    match (min_pal, max_pal) {
+    match (min_palindrome, max_palindrome) {
         (Some(min), Some(max)) => Some((min, max)),
         _ => None,
     }
