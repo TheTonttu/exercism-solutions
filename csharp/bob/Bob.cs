1cff4786ac8f8fe507dca5a85f3da6780bc14bd7
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-06-03 22:33:20 +0300

    Add C# "Bob" exercise solution

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Linq;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (Question(statement))
+        {
+            return Yelling(statement)
+                ? "Calm down, I know what I'm doing!"
+                : "Sure.";
+        }
+
+        if (Yelling(statement))
+        {
+            return "Whoa, chill out!";
+        }
+
+        if (Blank(statement))
+        {
+            return "Fine. Be that way!";
+        }
+
+        return "Whatever.";
     }
+
+    private static bool Question(string statement) => statement.AsSpan().Trim().EndsWith("?");
+    private static bool Yelling(string statement) => statement.Any(c => char.IsUpper(c)) && statement.All(c => !char.IsLetter(c) || char.IsUpper(c));
+    private static bool Blank(string statement) => statement is not null && string.IsNullOrWhiteSpace(statement);
 }
