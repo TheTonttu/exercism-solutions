a0cefd794e8d4c9a5ac7639be05d9944c8541baa
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-06-04 13:58:07 +0300

    Clarify directions further and fix mixed up row and col order

--- csharp/spiral-matrix/SpiralMatrix.cs
+++ csharp/spiral-matrix/SpiralMatrix.cs
@@ -1,4 +1,5 @@
 using System;
+using static Direction;
 
 public class SpiralMatrix
 {
@@ -10,22 +11,19 @@ public class SpiralMatrix
 
         int width = size;
         int height = size;
-        var spiral = new int[width,height];
+        // [row, col]
+        var spiral = new int[height,width];
 
-        int valueLimit = size*size;
+        int maxValue = height*width;
         int value = 1;
-        (sbyte DX, sbyte DY) direction = (1, 0);
+        var direction = East;
         var position = (X: 0, Y: 0);
-        while (value < valueLimit)
+        while (value < maxValue)
         {
-            spiral[position.X, position.Y] = value++;
+            spiral[position.Y, position.X] = value++;
 
             var nextPos = position.Move(direction);
-            while (
-                // Overboard
-                (nextPos.X < 0 || nextPos.X >= width || nextPos.Y < 0 || nextPos.Y >= height)
-                // Already assigned
-                || spiral[nextPos.X, nextPos.Y] != 0)
+            while (IsMoveBlocked(nextPos))
             {
                 direction = direction.TurnRight();
                 nextPos = position.Move(direction);
@@ -34,25 +32,36 @@ public class SpiralMatrix
         }
         var lastPosition = position;
         int lastValue = value;
-        spiral[lastPosition.X, lastPosition.Y] = lastValue;
+        spiral[lastPosition.Y, lastPosition.X] = lastValue;
 
         return spiral;
+
+        bool IsMoveBlocked((int X, int Y) movePos) =>
+            // Overboard
+            (movePos.X < 0 || movePos.X >= width || movePos.Y < 0 || movePos.Y >= height)
+            // Already assigned
+            || spiral[movePos.Y, movePos.X] != 0;
     }
+}
+
+internal static class Direction
+{
+    public static readonly (sbyte DX, sbyte DY) North = (0, -1);
+    public static readonly (sbyte DX, sbyte DY) East = (1, 0);
+    public static readonly (sbyte DX, sbyte DY) South = (0, 1);
+    public static readonly (sbyte DX, sbyte DY) West = (-1, 0);
 }
 
 internal static class MovementTupleExtensions
 {
+
     public static (int X, int Y) Move(this (int, int) position, (sbyte, sbyte) direction)
     {
         var (x, y) = position;
         var (dX, dY) = direction;
-        return (x + dY, y + dX);
+        return (x + dX, y + dY);
     }
 
-    private static readonly (sbyte, sbyte) North = (0, -1);
-    private static readonly (sbyte, sbyte) East = (1, 0);
-    private static readonly (sbyte, sbyte) South = (0, 1);
-    private static readonly (sbyte, sbyte) West = (-1, 0);
 
     public static (sbyte DX, sbyte DY) TurnRight(this (sbyte, sbyte) direction) =>
         direction switch
