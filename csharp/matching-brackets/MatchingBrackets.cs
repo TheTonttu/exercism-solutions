d38b962493fc8bfb543aa2fbd9f66376cf0f368d
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-06-04 09:48:12 +0300

    Partial solution
    Wrong order does not work properly yet.

--- csharp/matching-brackets/MatchingBrackets.cs
+++ csharp/matching-brackets/MatchingBrackets.cs
@@ -4,6 +4,43 @@ public static class MatchingBrackets
 {
     public static bool IsPaired(string input)
     {
-        throw new NotImplementedException();
+        // {}
+        int braceBalance = 0;
+        // ()
+        int parenthesisBalance = 0;
+        // []
+        int bracketBalance = 0;
+
+        foreach (char c in input)
+        {
+            if (c == '{')
+            {
+                braceBalance++;
+            }
+            else if (c == '}')
+            {
+                braceBalance--;
+            }
+            else if (c == '(')
+            {
+                parenthesisBalance++;
+            }
+            else if (c == ')')
+            {
+                parenthesisBalance--;
+            }
+            else if (c == '[')
+            {
+                bracketBalance++;
+            }
+            else if (c == ']')
+            {
+                bracketBalance--;
+            }
+        }
+
+        return braceBalance == 0
+            && parenthesisBalance == 0
+            && bracketBalance == 0;
     }
 }
