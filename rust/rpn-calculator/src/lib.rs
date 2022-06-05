20bd4860ccda70727abea1eba69abfc8fad4d65f
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-06-05 19:44:56 +0300

    Encapsulate value-vector-double-pop mess

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -8,39 +8,39 @@ pub enum CalculatorInput {
 }
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    let mut pending_values = Vec::new();
+    let mut values = Vec::new();
 
     for input in inputs {
         if let CalculatorInput::Value(n) = input {
-            pending_values.push(*n)
-        } else {
-            let result = match input {
-                CalculatorInput::Add => pending_values
-                    .pop()
-                    .and_then(|a| pending_values.pop().map(|b| b + a)),
-                CalculatorInput::Subtract => pending_values
-                    .pop()
-                    .and_then(|a| pending_values.pop().map(|b| b - a)),
-                CalculatorInput::Multiply => pending_values
-                    .pop()
-                    .and_then(|a| pending_values.pop().map(|b| b * a)),
-                CalculatorInput::Divide => pending_values
-                    .pop()
-                    .and_then(|a| pending_values.pop().map(|b| b / a)),
-                _ => return None,
-            };
+            values.push(*n);
+            continue;
+        }
 
-            if let Some(n) = result {
-                pending_values.push(n);
-            } else {
-                return None;
-            }
+        let result = match input {
+            CalculatorInput::Add => calculate(&mut values, |a, b| b + a),
+            CalculatorInput::Subtract => calculate(&mut values, |a, b| b - a),
+            CalculatorInput::Multiply => calculate(&mut values, |a, b| b * a),
+            CalculatorInput::Divide => calculate(&mut values, |a, b| b / a),
+            _ => return None,
+        };
+
+        if let Some(n) = result {
+            values.push(n);
+        } else {
+            return None;
         }
     }
 
-    if pending_values.len() == 1 {
-        pending_values.pop()
+    if values.len() == 1 {
+        values.pop()
     } else {
         None
     }
+}
+
+fn calculate<T>(values: &mut Vec<i32>, op: T) -> Option<i32>
+where
+    T: Fn(i32, i32) -> i32,
+{
+    values.pop().and_then(|a| values.pop().map(|b| op(a, b)))
 }
