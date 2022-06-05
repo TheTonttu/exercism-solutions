b478b32e4db2c6187281690bfad6cc13560aae41
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-06-05 19:22:03 +0300

    Add Rust "RPN Calculator" exercise solution

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -8,8 +8,39 @@ pub enum CalculatorInput {
 }
 
 pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
-    unimplemented!(
-		"Given the inputs: {:?}, evaluate them as though they were a Reverse Polish notation expression",
-		inputs,
-	);
+    let mut pending_values = Vec::new();
+
+    for input in inputs {
+        if let CalculatorInput::Value(n) = input {
+            pending_values.push(*n)
+        } else {
+            let result = match input {
+                CalculatorInput::Add => pending_values
+                    .pop()
+                    .and_then(|a| pending_values.pop().map(|b| b + a)),
+                CalculatorInput::Subtract => pending_values
+                    .pop()
+                    .and_then(|a| pending_values.pop().map(|b| b - a)),
+                CalculatorInput::Multiply => pending_values
+                    .pop()
+                    .and_then(|a| pending_values.pop().map(|b| b * a)),
+                CalculatorInput::Divide => pending_values
+                    .pop()
+                    .and_then(|a| pending_values.pop().map(|b| b / a)),
+                _ => return None,
+            };
+
+            if let Some(n) = result {
+                pending_values.push(n);
+            } else {
+                return None;
+            }
+        }
+    }
+
+    if pending_values.len() == 1 {
+        pending_values.pop()
+    } else {
+        None
+    }
 }
