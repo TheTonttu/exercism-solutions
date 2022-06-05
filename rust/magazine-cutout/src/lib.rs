dc227f3b5cd747fcde58af84c802627254729d7a
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-06-05 18:17:20 +0300

    Add Rust "Magazine Cutout" exercise solution

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -1,9 +1,19 @@
-// This stub file contains items that aren't used yet; feel free to remove this module attribute
-// to enable stricter warnings.
-#![allow(unused)]
-
 use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
+    let mut remaining_words: HashMap<&str, usize> = HashMap::new();
+
+    for word in magazine {
+        *remaining_words.entry(word).or_default() += 1;
+    }
+
+    for word in note {
+        let entry = remaining_words.entry(word).or_default();
+        if *entry > 0 {
+            *entry -= 1;
+        } else {
+            return false;
+        }
+    }
+    true
 }
