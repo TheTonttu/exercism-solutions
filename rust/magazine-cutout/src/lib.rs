8c9efe1acc7a1bb1257711fb14d6f6185a7d399f
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-06-05 18:24:49 +0300

    Reduce nesting and improve readability

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -4,16 +4,16 @@ pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
     let mut remaining_words: HashMap<&str, usize> = HashMap::new();
 
     for word in magazine {
-        *remaining_words.entry(word).or_default() += 1;
+        let word_count = remaining_words.entry(word).or_default();
+        *word_count += 1;
     }
 
     for word in note {
-        let entry = remaining_words.entry(word).or_default();
-        if *entry > 0 {
-            *entry -= 1;
-        } else {
+        let word_count = remaining_words.entry(word).or_default();
+        if *word_count == 0 {
             return false;
         }
+        *word_count -= 1;
     }
     true
 }
