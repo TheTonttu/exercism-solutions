f4be594577ce7841a1fd7327dc9b29f022357537
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-06-08 21:51:27 +0300

    Add Rust "Sublist" exercise solution

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -7,5 +7,36 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    if _first_list.len() == _second_list.len() && _first_list == _second_list {
+        Comparison::Equal
+    } else if _first_list.len() < _second_list.len()
+        && (_first_list.is_empty() || is_sublist(_first_list, _second_list))
+    {
+        Comparison::Sublist
+    } else if _first_list.len() > _second_list.len()
+        && (_second_list.is_empty() || is_sublist(_second_list, _first_list))
+    {
+        Comparison::Superlist
+    } else {
+        Comparison::Unequal
+    }
+}
+
+fn is_sublist<T: PartialEq>(sublist: &[T], superlist: &[T]) -> bool {
+    if let Some(expected_start_value) = sublist.first() {
+        for (start_index, start_value) in superlist.iter().enumerate() {
+            let remaining_length = superlist.len() - start_index;
+            if remaining_length < sublist.len() {
+                return false;
+            }
+            if start_value == expected_start_value {
+                let stop_index = start_index + sublist.len();
+                let candidate_slice = &superlist[start_index..stop_index];
+                if sublist == candidate_slice {
+                    return true;
+                }
+            }
+        }
+    }
+    false
 }
