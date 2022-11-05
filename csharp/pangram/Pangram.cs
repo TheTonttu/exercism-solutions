8b7a76863b5a8b8180964dd485b6a10f43030710
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-11-05 12:59:21 +0200

    Add C# "Pangram" exercise solution

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -2,8 +2,28 @@ using System;
 
 public static class Pangram
 {
+    // First 26 bits set to 1.
+    // A-Z = 26 letters.
+    private const int PangramMask = 67108863;
+
     public static bool IsPangram(string input)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(input))
+        {
+            return false;
+        }
+
+        int sentenceMask = 0;
+        foreach (char letter in input)
+        {
+            if (letter is >= 'a' and <= 'z')
+            {
+                sentenceMask |= 1 << (letter - 'a');
+            } else if (letter is >= 'A' and <= 'Z')
+            {
+                sentenceMask |= 1 << (letter - 'A');
+            }
+        }
+        return sentenceMask == PangramMask;
     }
 }
