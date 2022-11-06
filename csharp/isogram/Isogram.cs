e3713e64482dcce1a8016a108ee0d3c26d41fe35
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-11-06 22:04:06 +0200

    Reduce repetition

--- csharp/isogram/Isogram.cs
+++ csharp/isogram/Isogram.cs
@@ -7,24 +7,24 @@ public static class Isogram
         var bitField = new BitField();
         foreach (char letter in word)
         {
+            int letterBitPosition;
             if (letter is >= 'a' and <= 'z')
             {
-                int letterBitPosition = letter - 'a';
-                if (bitField.IsSet(letterBitPosition))
-                {
-                    return false;
-                }
-                bitField.Set(letterBitPosition);
+                letterBitPosition = letter - 'a';
             }
             else if (letter is >= 'A' and <= 'Z')
             {
-                int letterBitPosition = letter - 'A';
-                if (bitField.IsSet(letterBitPosition))
-                {
-                    return false;
-                }
-                bitField.Set(letterBitPosition);
+                letterBitPosition = letter - 'A';
+            } else
+            {
+                continue;
             }
+
+            if (bitField.IsBitSet(letterBitPosition))
+            {
+                return false;
+            }
+            bitField.SetBit(letterBitPosition);
         }
         return true;
     }
@@ -38,12 +38,12 @@ public static class Isogram
             bits = 0;
         }
 
-        public bool IsSet(in int position)
+        public bool IsBitSet(in int position)
         {
             return (bits & 1 << position) != 0;
         }
 
-        public void Set(in int position)
+        public void SetBit(in int position)
         {
             bits |= 1 << position;
         }
