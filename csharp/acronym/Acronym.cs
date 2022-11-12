2a5fbeef86f385139061abca2f083b935247470a
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2022-11-12 09:52:16 +0200

    Add C# "Acronym" exercise solution

--- csharp/acronym/Acronym.cs
+++ csharp/acronym/Acronym.cs
@@ -1,9 +1,31 @@
-using System;
+using System.Text;
 
 public static class Acronym
 {
     public static string Abbreviate(string phrase)
     {
-        throw new NotImplementedException("Please implement this function");
+        var acronymBuilder = new StringBuilder();
+
+        bool pickNextLetter = true;
+        foreach (char character in phrase)
+        {
+            if (IsLetter(character))
+            {
+                if (pickNextLetter)
+                {
+                    acronymBuilder.Append(char.ToUpperInvariant(character));
+                    pickNextLetter = false;
+                }
+            }
+            else if (!IsDiacriticalMark(character))
+            {
+                pickNextLetter = true;
+            }
+        }
+
+        return acronymBuilder.ToString();
     }
+
+    private static bool IsLetter(char character) => character is (>= 'a' and <= 'z') or (>= 'A' and <= 'Z');
+    private static bool IsDiacriticalMark(char character) => character is '\'';
 }
