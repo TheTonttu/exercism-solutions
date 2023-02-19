73e8bd16e6a32b356b2333b32ed7b17e9dfd0b6f
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-02-19 11:15:33 +0200

    Add C++ "Leap" exercise solution

--- cpp/leap/leap.h
+++ cpp/leap/leap.h
@@ -2,7 +2,12 @@
 #define LEAP_H
 
 namespace leap {
-
+	/// <summary>
+	/// Checks if the given year is a leap year.
+	/// </summary>
+	/// <param name="year"></param>
+	/// <returns>True if year is leap year; otherwise false.</returns>
+	bool is_leap_year(int year);
 }  // namespace leap
 
 #endif // LEAP_H
