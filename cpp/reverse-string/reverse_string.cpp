1d4544c262a61d19849b2cc80fbfcc95a50a0c91
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-02-19 18:13:48 +0200

    Add C++ "Reverse String" exercise solution

--- cpp/reverse-string/reverse_string.cpp
+++ cpp/reverse-string/reverse_string.cpp
@@ -1,5 +1,10 @@
+#include <algorithm>
 #include "reverse_string.h"
 
 namespace reverse_string {
-
+	string reverse_string(string str) {
+		string reverse_copy = str;
+		std::reverse(reverse_copy.begin(), reverse_copy.end());
+		return reverse_copy;
+	}
 }  // namespace reverse_string
--- cpp/reverse-string/reverse_string.h
+++ cpp/reverse-string/reverse_string.h
@@ -1,8 +1,17 @@
 #if !defined(REVERSE_STRING_H)
 #define REVERSE_STRING_H
 
-namespace reverse_string {
+#include <string>
 
+using namespace std;
+
+namespace reverse_string {
+	/// <summary>
+	/// Creates reversed copy of the given string.
+	/// </summary>
+	/// <param name="str"></param>
+	/// <returns>Reversed string copy.</returns>
+	string reverse_string(string str);
 }  // namespace reverse_string
 
 #endif // REVERSE_STRING_H
