a1ceda01672cddefc60f0c05cad4251279788b60
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-02-22 20:05:18 +0200

    Add C++ "Space Age" exercise solution

--- cpp/space-age/space_age.cpp
+++ cpp/space-age/space_age.cpp
@@ -1,5 +1,70 @@
 #include "space_age.h"
 
 namespace space_age {
+	constexpr int64_t seconds_in_earth_year{ 31'557'600 };
 
+	space_age::space_age(int64_t seconds)
+	{
+		m_age_in_seconds = seconds;
+	}
+
+	int64_t space_age::seconds() const
+	{
+		return m_age_in_seconds;
+	}
+
+	double space_age::on_earth() const
+	{
+		constexpr long double seconds_in_year{ seconds_in_earth_year };
+		return m_age_in_seconds / seconds_in_year;
+	}
+
+	double space_age::on_jupiter() const
+	{
+		constexpr double orbital_period{ 11.862615 };
+		constexpr double seconds_in_year { seconds_in_earth_year * orbital_period };
+		return m_age_in_seconds / seconds_in_year;
+	}
+
+	double space_age::on_mars() const
+	{
+		constexpr double orbital_period{ 1.8808158 };
+		constexpr double seconds_in_year{ seconds_in_earth_year * orbital_period };
+		return m_age_in_seconds / seconds_in_year;
+	}
+
+	double space_age::on_mercury() const
+	{
+		constexpr double orbital_period{ 0.2408467 };
+		constexpr double seconds_in_year{ seconds_in_earth_year * orbital_period };
+		return m_age_in_seconds / seconds_in_year;
+	}
+
+	double space_age::on_neptune() const
+	{
+		constexpr double orbital_period{ 164.79132 };
+		constexpr double seconds_in_year{ seconds_in_earth_year * orbital_period };
+		return m_age_in_seconds / seconds_in_year;
+	}
+
+	double space_age::on_saturn() const
+	{
+		constexpr double orbital_period{ 29.447498 };
+		constexpr double seconds_in_year{ seconds_in_earth_year * orbital_period };
+		return m_age_in_seconds / seconds_in_year;
+	}
+
+	double space_age::on_uranus() const
+	{
+		constexpr double orbital_period{ 84.016846 };
+		constexpr double seconds_in_year{ seconds_in_earth_year * orbital_period };
+		return m_age_in_seconds / seconds_in_year;
+	}
+
+	double space_age::on_venus() const
+	{
+		constexpr double orbital_period{ 0.61519726 };
+		constexpr double seconds_in_year{ seconds_in_earth_year * orbital_period };
+		return m_age_in_seconds / seconds_in_year;
+	}
 }  // namespace space_age
--- cpp/space-age/space_age.h
+++ cpp/space-age/space_age.h
@@ -1,8 +1,70 @@
 #if !defined(SPACE_AGE_H)
 #define SPACE_AGE_H
 
-namespace space_age {
+#include <cstdint>
 
+namespace space_age {
+	struct space_age {
+	public:
+		space_age(int64_t seconds);
+
+		/// <summary>
+		/// Gets the age in seconds.
+		/// </summary>
+		/// <returns>Age in seconds.</returns>
+		int64_t seconds() const;
+
+		/// <summary>
+		/// Calculates age in Earth years.
+		/// </summary>
+		/// <returns>Age in earth years.</returns>
+		double on_earth() const;
+
+		/// <summary>
+		/// Calculates age in Earth years.
+		/// </summary>
+		/// <returns>Age in Jupiter years.</returns>
+		double on_jupiter() const;
+
+		/// <summary>
+		/// Calculates age in Mars years.
+		/// </summary>
+		/// <returns>Age in Mars years.</returns>
+		double on_mars() const;
+
+		/// <summary>
+		/// Calculates age in Mercury years.
+		/// </summary>
+		/// <returns>Age in Mercury years.</returns>
+		double on_mercury() const;
+
+		/// <summary>
+		/// Calculates age in Neptune years.
+		/// </summary>
+		/// <returns>Age in Neptune years.</returns>
+		double on_neptune() const;
+
+		/// <summary>
+		/// Calculates age in Saturn years.
+		/// </summary>
+		/// <returns>Age in Saturn years.</returns>
+		double on_saturn() const;
+
+		/// <summary>
+		/// Calculates age in Uranus years.
+		/// </summary>
+		/// <returns>Age in Uranus years.</returns>
+		double on_uranus() const;
+
+		/// <summary>
+		/// Calculates age in Venus years.
+		/// </summary>
+		/// <returns>Age in Venus years.</returns>
+		double on_venus() const;
+
+	private:
+		int64_t m_age_in_seconds;
+	};
 }  // namespace space_age
 
 #endif // SPACE_AGE_H
