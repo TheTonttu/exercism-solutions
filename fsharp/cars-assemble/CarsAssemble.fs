0cf578d1e46425b4dd9378cd825e5195c380738b
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-02-23 19:22:47 +0200

    Add F# "Cars, Assemble!" exercise solution

--- fsharp/cars-assemble/CarsAssemble.fs
+++ fsharp/cars-assemble/CarsAssemble.fs
@@ -1,10 +1,17 @@
 module CarsAssemble
 
 let successRate (speed: int): float =
-    failwith "Please implement the 'successRate' function"
+    match speed with
+    | 0 -> 0.0
+    | s when s >= 1 && s <= 4 -> 1.0
+    | s when s >= 5 && s <= 8 -> 0.9
+    | 9 -> 0.8
+    | 10 -> 0.77
+    | _ -> failwith $"Speed of {speed} is out of the question"
 
 let productionRatePerHour (speed: int): float =
-    failwith "Please implement the 'productionRatePerHour' function"
+    let baseProductionPerHour = 221
+    float (speed * baseProductionPerHour) * successRate(speed)
 
 let workingItemsPerMinute (speed: int): int =
-    failwith "Please implement the 'workingItemsPerMinute' function"
+    int (productionRatePerHour speed / 60.0)
