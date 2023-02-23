77f238258e7ee93e2aee63f9e13c9d4bbe641323
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-02-23 18:56:33 +0200

    Remove useless fluff

--- fsharp/valentines-day/ValentinesDay.fs
+++ fsharp/valentines-day/ValentinesDay.fs
@@ -18,7 +18,7 @@ type Genre =
 type Activity =
     | BoardGame
     | Chill
-    | Movie of genre: Genre
+    | Movie of Genre
     | Restaurant of Cuisine
     | Walk of kilometers: int
 
@@ -26,10 +26,10 @@ let rateActivity (activity: Activity): Approval =
     match activity with
     | BoardGame -> No
     | Chill -> No
-    | Movie(Romance) -> Yes
-    | Movie(_) -> No
-    | Restaurant(Korean) -> Yes
-    | Restaurant(Turkish) -> Maybe
-    | Walk(km) when km < 3 -> Yes
-    | Walk(km) when km < 5 -> Maybe
-    | Walk(_) -> No
+    | Movie Romance -> Yes
+    | Movie _ -> No
+    | Restaurant Korean -> Yes
+    | Restaurant Turkish -> Maybe
+    | Walk km when km < 3 -> Yes
+    | Walk km when km < 5 -> Maybe
+    | Walk _ -> No
