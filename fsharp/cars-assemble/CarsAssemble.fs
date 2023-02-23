75798d67a0b7f745ee011622cb188858e28cfcf9
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-02-23 19:43:59 +0200

    Active pattern for speed range

--- fsharp/cars-assemble/CarsAssemble.fs
+++ fsharp/cars-assemble/CarsAssemble.fs
@@ -1,10 +1,13 @@
 module CarsAssemble
 
+let (|InRange|_|) low high x =
+  if low <= x && x <= high then Some () else None
+
 let successRate (speed: int): float =
     match speed with
     | 0 -> 0.0
-    | s when s >= 1 && s <= 4 -> 1.0
-    | s when s >= 5 && s <= 8 -> 0.9
+    | InRange 1 4 -> 1.0
+    | InRange 5 8 -> 0.9
     | 9 -> 0.8
     | 10 -> 0.77
     | _ -> failwith $"Speed of {speed} is out of the question"
