96995e7706bd4ed133e4ea410517fa7670f56195
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-02-24 17:58:10 +0200

    Add F# "Interest is Interesting" exercise solution

--- fsharp/interest-is-interesting/InterestIsInteresting.fs
+++ fsharp/interest-is-interesting/InterestIsInteresting.fs
@@ -1,13 +1,24 @@
 module InterestIsInteresting
 
 let interestRate (balance: decimal): single =
-    failwith "Please implement the 'interestRate' function"
+    match balance with
+    | b when b < 0m -> 3.213f
+    | b when b < 1000m -> 0.5f
+    | b when b < 5000m -> 1.621f
+    | _ -> 2.475f
+
+let interestPercentage (balance: decimal): single =
+    interestRate balance / 100.0f
 
 let interest (balance: decimal): decimal =
-   failwith "Please implement the 'interest' function"
+   balance * decimal(interestPercentage balance)
 
 let annualBalanceUpdate(balance: decimal): decimal =
-   failwith "Please implement the 'annualBalanceUpdate' function"
+   balance + interest balance
 
 let amountToDonate(balance: decimal) (taxFreePercentage: float): int =
-   failwith "Please implement the 'amountToDonate' function"
+    let donationPercentage = 
+        if balance > 0m
+        then taxFreePercentage * 2.0 / 100.0 
+        else 0.0
+    int(balance * decimal(donationPercentage))
