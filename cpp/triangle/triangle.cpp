111bf63b0fa7a3666cfbb486dc086467b414e61f
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-02-24 18:21:16 +0200

    Add C++ "Triangle" exercise solution

--- cpp/triangle/triangle.cpp
+++ cpp/triangle/triangle.cpp
@@ -1,5 +1,31 @@
 #include "triangle.h"
+#include <stdexcept>
+
+using namespace std;
 
 namespace triangle {
+	flavor kind(float a, float b, float c)
+	{
+		// All sides have to be of length > 0.
+		if (a <= 0 || b <= 0 || c <= 0) {
+			throw domain_error("All sides have to be greater than 0.");
+		}
 
+		// Sum of the lengths of any two sides must be greater than or equal to the length of the third side.
+		if (!(a + b >= c) ||
+			!(a + c >= b) ||
+			!(b + c >= a)) {
+			throw domain_error("Irregular length of sides.");
+		}
+
+		if (a == b && a == c && b == c) {
+			return flavor::equilateral;
+		}
+
+		if (a == b || a == c || b == c) {
+			return flavor::isosceles;
+		}
+
+		return flavor::scalene;
+	}
 }  // namespace triangle
--- cpp/triangle/triangle.h
+++ cpp/triangle/triangle.h
@@ -2,7 +2,27 @@
 #define TRIANGLE_H
 
 namespace triangle {
+	/// <summary>
+	/// Triangle flavors.
+	/// </summary>
+	enum flavor {
+		/// <summary>
+		/// Triangle has all three sides the same length.
+		/// </summary>
+		equilateral,
 
+		/// <summary>
+		/// Triangle has at least two sides the same length.
+		/// </summary>
+		isosceles,
+
+		/// <summary>
+		/// Triangle has all sides of different lengths.
+		/// </summary>
+		scalene
+	};
+
+	flavor kind(float, float, float);
 }  // namespace triangle
 
 #endif // TRIANGLE_H
