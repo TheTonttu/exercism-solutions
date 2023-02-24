00353bf0ad670e7a4409e3c4bf15e1ce4ed7935b
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-02-24 19:53:58 +0200

    Add C++ "Two Fer" exercise solution

--- cpp/two-fer/two_fer.cpp
+++ cpp/two-fer/two_fer.cpp
@@ -1,7 +1,15 @@
 #include "two_fer.h"
+#include <string>
 
 namespace two_fer
 {
+	std::string two_fer(std::string your_name)
+	{
+		if (your_name.empty()) {
+			your_name = "you";
+		}
 
+		return "One for " + your_name + ", one for me.";
+	}
 } // namespace two_fer
 
