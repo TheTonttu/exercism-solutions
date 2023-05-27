b9b6b658608ba76de18fcff80faef5038e1a0afa
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-05-27 16:05:41 +0300

    Add C# "Run-Length Encoding" exercise solution

--- csharp/run-length-encoding/RunLengthEncoding.cs
+++ csharp/run-length-encoding/RunLengthEncoding.cs
@@ -1,14 +1,63 @@
-using System;
+using System.Text;
 
 public static class RunLengthEncoding
 {
     public static string Encode(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int consecutives = 0;
+        char character = default;
+        var stringBuilder = new StringBuilder();
+        foreach (char c in input)
+        {
+            if (character == c)
+            {
+                consecutives++;
+            }
+            else
+            {
+                EncodeRun(stringBuilder, consecutives, character);
+                character = c;
+                consecutives = 1;
+            }
+        }
+
+        EncodeRun(stringBuilder, consecutives, character);
+
+        return stringBuilder.ToString();
+
+        static void EncodeRun(StringBuilder stringBuilder, int consecutives, char character)
+        {
+            if (consecutives > 1)
+            {
+                stringBuilder.Append(consecutives);
+            }
+
+            if (consecutives > 0)
+            {
+                stringBuilder.Append(character);
+            }
+        }
     }
 
     public static string Decode(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        int consecutives = 0;
+        var stringBuilder = new StringBuilder();
+        foreach (char c in input)
+        {
+            if (char.IsDigit(c))
+            {
+                int digit = (int)char.GetNumericValue(c);
+                consecutives = (consecutives * 10) + digit;
+            }
+            else
+            {
+                int repetition = consecutives > 0 ? consecutives : 1;
+                stringBuilder.Append(c, repetition);
+                consecutives = 0;
+            }
+        }
+
+        return stringBuilder.ToString();
     }
 }
