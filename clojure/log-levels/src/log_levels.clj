d6579da28bb030ac59bc5c742e1a2361e8ec0a44
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-06-04 15:28:16 +0300

    Add Clojure "Log Levels" exercise solution

--- clojure/log-levels/src/log_levels.clj
+++ clojure/log-levels/src/log_levels.clj
@@ -1,20 +1,32 @@
-(ns log-levels
-  (:require [clojure.string :as str]))
+(ns log-levels)
+(use '[clojure.string :only (index-of, lower-case, trim)])
 
 (defn message
   "Takes a string representing a log line
    and returns its message with whitespace trimmed."
   [s]
+  (let [separator (index-of s ":")]
+    (if (<= 0 separator)
+      (trim (subs s (+ 1 separator)))
+      (trim s)))
   )
 
 (defn log-level
   "Takes a string representing a log line
    and returns its level in lower-case."
   [s]
+  (lower-case
+    (let [start (index-of s "[")
+          end (index-of s "]")]
+      (if (and (<= 0 start) (<= 0 end))
+        (subs s (+ 1 start) end)
+        (s)
+        )))
   )
 
 (defn reformat
   "Takes a string representing a log line and formats it
    with the message first and the log level in parentheses."
   [s]
+  (format "%s (%s)" (message s) (log-level s))
   )
