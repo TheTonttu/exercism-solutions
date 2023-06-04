f21058fa22c4baeaad690204de2bb72160822afe
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2023-06-04 14:17:52 +0300

    Add Clojure "Cars, Assemble!" exercise solution

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -1,12 +1,27 @@
 (ns cars-assemble)
 
+(def base-production-rate 221.0)
+
+(defn success-rate [speed]
+  (cond
+    (< speed 1) 0.0
+    (<= speed 4) 1.0
+    (<= speed 8) 0.9
+    (= speed 9) 0.8
+    (= speed 10) 0.77
+    :else 0.0)
+  )
+
 (defn production-rate
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
+  (* (success-rate speed)
+     (* speed base-production-rate))
   )
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
   [speed]
+  (int (/ (production-rate speed) 60))
   )
