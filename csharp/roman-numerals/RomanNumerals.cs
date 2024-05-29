9304df5a0738a23f4e7c7173af25a6183b5f0f25
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-05-29 23:11:23 +0300

    Add C# "Roman Numerals" exercise solution

--- csharp/roman-numerals/RomanNumerals.cs
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -1,9 +1,38 @@
-using System;
+using System.Text;
 
 public static class RomanNumeralExtension
 {
     public static string ToRoman(this int value)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var stringBuilder = new StringBuilder(capacity: value);
+
+        stringBuilder
+            .Append('I', value)
+            // 5
+            .Replace("IIIII", "V")
+            // 10
+            .Replace("VV", "X")
+            // 50
+            .Replace("XXXXX", "L")
+            // 100
+            .Replace("LL", "C")
+            // 500
+            .Replace("CCCCC", "D")
+            // 1000
+            .Replace("DD", "M")
+            // 4
+            .Replace("IIII", "IV")
+            // 5 + 4 = 9
+            .Replace("VIV", "IX")
+            // 40
+            .Replace("XXXX", "XL")
+            // 50 + 40 = 90
+            .Replace("LXL", "XC")
+            // 400
+            .Replace("CCCC", "CD")
+            // 500 + 400 = 900
+            .Replace("DCD", "CM");
+
+        return stringBuilder.ToString();
     }
 }
