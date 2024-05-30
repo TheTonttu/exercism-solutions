dde52b9b2f7cfa5fc6f56b05e5ed4d34ee669cf4
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-05-30 21:02:16 +0300

    More efficient implementation + benchmarks

--- csharp/roman-numerals/RomanNumerals.cs
+++ csharp/roman-numerals/RomanNumerals.cs
@@ -1,37 +1,35 @@
+using System;
 using System.Text;
 
 public static class RomanNumeralExtension
 {
     public static string ToRoman(this int value)
     {
-        var stringBuilder = new StringBuilder(capacity: value);
+        var stringBuilder = new StringBuilder();
 
-        stringBuilder
-            .Append('I', value)
-            // 5
-            .Replace("IIIII", "V")
-            // 10
-            .Replace("VV", "X")
-            // 50
-            .Replace("XXXXX", "L")
-            // 100
-            .Replace("LL", "C")
-            // 500
-            .Replace("CCCCC", "D")
-            // 1000
-            .Replace("DD", "M")
-            // 4
-            .Replace("IIII", "IV")
-            // 5 + 4 = 9
-            .Replace("VIV", "IX")
-            // 40
-            .Replace("XXXX", "XL")
-            // 50 + 40 = 90
-            .Replace("LXL", "XC")
-            // 400
-            .Replace("CCCC", "CD")
-            // 500 + 400 = 900
-            .Replace("DCD", "CM");
+        int remainder = value;
+        while (remainder > 0)
+        {
+            (int subtraction, string romanNumerals) = remainder switch
+            {
+                >= 1000 => (1000, "M"),
+                >= 900 => (900, "CM"),
+                >= 500 => (500, "D"),
+                >= 400 => (400, "CD"),
+                >= 100 => (100, "C"),
+                >= 90 => (90, "XC"),
+                >= 50 => (50, "L"),
+                >= 40 => (40, "XL"),
+                >= 10 => (10, "X"),
+                >= 9 => (9, "IX"),
+                >= 5 => (5, "V"),
+                >= 4 => (4, "IV"),
+                >= 1 => (1, "I"),
+                _ => throw new InvalidOperationException("Should be out of loop by now")
+            };
+            stringBuilder.Append(romanNumerals);
+            remainder -= subtraction;
+        }
 
         return stringBuilder.ToString();
     }
---
+++ csharp/roman-numerals/RomanNumeralsBenchmarks.cs
@@ -0,0 +1,121 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomanNumerals
+{
+    [MemoryDiagnoser]
+    public class RomanNumeralsBenchmarks
+    {
+        [Benchmark(Baseline = true)]
+        [ArgumentsSource(nameof(Numbers))]
+        public string StringBuilderReplace(int number) {
+            return StringBuilderReplaceImplementation.ToRoman(number);
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Numbers))]
+        public string StringBuilderReplaceWithoutInitialCapacity(int number)
+        {
+            return StringBuilderReplaceImplementation.ToRomanWithoutInitialCapacity(number);
+        }
+
+        [Benchmark]
+        [ArgumentsSource(nameof(Numbers))]
+        public string WhileLoopSwitchPattern(int number) {
+            return number.ToRoman();
+        }
+
+
+        public static IEnumerable<int> Numbers()
+        {
+            yield return 1;
+            yield return 911;
+            yield return 3999;
+        }
+    }
+
+    /// <summary>
+    /// Oof
+    /// </summary>
+    internal static class StringBuilderReplaceImplementation
+    {
+        public static string ToRoman(int value)
+        {
+            var stringBuilder = new StringBuilder(capacity: value);
+
+            stringBuilder
+                .Append('I', value)
+                // 5
+                .Replace("IIIII", "V")
+                // 10
+                .Replace("VV", "X")
+                // 50
+                .Replace("XXXXX", "L")
+                // 100
+                .Replace("LL", "C")
+                // 500
+                .Replace("CCCCC", "D")
+                // 1000
+                .Replace("DD", "M")
+                // 4
+                .Replace("IIII", "IV")
+                // 5 + 4 = 9
+                .Replace("VIV", "IX")
+                // 40
+                .Replace("XXXX", "XL")
+                // 50 + 40 = 90
+                .Replace("LXL", "XC")
+                // 400
+                .Replace("CCCC", "CD")
+                // 500 + 400 = 900
+                .Replace("DCD", "CM");
+
+            return stringBuilder.ToString();
+        }
+
+        public static string ToRomanWithoutInitialCapacity(int value)
+        {
+            var stringBuilder = new StringBuilder();
+
+            stringBuilder
+                .Append('I', value)
+                // 5
+                .Replace("IIIII", "V")
+                // 10
+                .Replace("VV", "X")
+                // 50
+                .Replace("XXXXX", "L")
+                // 100
+                .Replace("LL", "C")
+                // 500
+                .Replace("CCCCC", "D")
+                // 1000
+                .Replace("DD", "M")
+                // 4
+                .Replace("IIII", "IV")
+                // 5 + 4 = 9
+                .Replace("VIV", "IX")
+                // 40
+                .Replace("XXXX", "XL")
+                // 50 + 40 = 90
+                .Replace("LXL", "XC")
+                // 400
+                .Replace("CCCC", "CD")
+                // 500 + 400 = 900
+                .Replace("DCD", "CM");
+
+            return stringBuilder.ToString();
+        }
+    }
+
+
+    public class Program
+    {
+        public static void Main(string[] _)
+        {
+            BenchmarkRunner.Run<RomanNumeralsBenchmarks>();
+        }
+    }
+}
