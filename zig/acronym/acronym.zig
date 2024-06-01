820b205dff2024b268b029fa941c902dd6e206d7
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-06-01 22:55:05 +0300

    More convoluted implementation 🤡

--- zig/acronym/acronym.zig
+++ zig/acronym/acronym.zig
@@ -4,14 +4,22 @@ const mem = std.mem;
 pub fn abbreviate(allocator: mem.Allocator, words: []const u8) mem.Allocator.Error![]u8 {
     var list = std.ArrayList(u8).init(allocator);
     defer list.deinit();
-    var prevChar: u8 = std.ascii.control_code.nul;
-    for (words) |char| {
-        if (std.ascii.isAlphabetic(char) and
-            !(std.ascii.isAlphabetic(prevChar) or isDiacriticalMark(prevChar)))
-        {
+
+    var remainder = words[0..];
+    while (remainder.len > 0) {
+        const char = remainder[0];
+        if (std.ascii.isAlphabetic(char)) {
             try list.append(std.ascii.toUpper(char));
+            // Skip until the word ends
+            while (remainder.len > 0 and isPartOfWord(remainder[0])) {
+                remainder = remainder[1..];
+            }
+        } else {
+            // Skip until next word begins
+            while (remainder.len > 0 and !isPartOfWord(remainder[0])) {
+                remainder = remainder[1..];
+            }
         }
-        prevChar = char;
     }
     return list.toOwnedSlice();
 }
@@ -21,4 +29,8 @@ fn isDiacriticalMark(c: u8) bool {
         '\'' => true,
         else => false,
     };
+}
+
+fn isPartOfWord(c: u8) bool {
+    return std.ascii.isAlphabetic(c) or isDiacriticalMark(c);
 }
