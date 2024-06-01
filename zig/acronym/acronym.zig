8ea84cfb7ccd5006f6dcf357aec1b8e3114e7f9b
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-06-01 21:57:15 +0300

    Add Zig "Acronym" exercise solution

--- zig/acronym/acronym.zig
+++ zig/acronym/acronym.zig
@@ -2,7 +2,23 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn abbreviate(allocator: mem.Allocator, words: []const u8) mem.Allocator.Error![]u8 {
-    _ = allocator;
-    _ = words;
-    @compileError("please implement the abbreviate function");
+    var list = std.ArrayList(u8).init(allocator);
+    defer list.deinit();
+    var prevChar: u8 = std.ascii.control_code.nul;
+    for (words) |char| {
+        if (std.ascii.isAlphabetic(char) and
+            !(std.ascii.isAlphabetic(prevChar) or isDiacriticalMark(prevChar)))
+        {
+            try list.append(std.ascii.toUpper(char));
+        }
+        prevChar = char;
+    }
+    return list.toOwnedSlice();
+}
+
+fn isDiacriticalMark(c: u8) bool {
+    return switch (c) {
+        '\'' => true,
+        else => false,
+    };
 }
