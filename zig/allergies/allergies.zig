16ebc8b9782e2e0005cef183bec1abe1967677bd
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-06-08 13:30:05 +0300

    Add Zig "Allergies" exercise solution

--- zig/allergies/allergies.zig
+++ zig/allergies/allergies.zig
@@ -1,24 +1,34 @@
 const std = @import("std");
 const EnumSet = std.EnumSet;
 
-pub const Allergen = enum {
-    eggs,
-    peanuts,
-    shellfish,
-    strawberries,
-    tomatoes,
-    chocolate,
-    pollen,
-    cats,
+pub const Allergen = enum(u8) {
+    eggs = 1,
+    peanuts = 2,
+    shellfish = 4,
+    strawberries = 8,
+    tomatoes = 16,
+    chocolate = 32,
+    pollen = 64,
+    cats = 128,
 };
 
 pub fn isAllergicTo(score: u8, allergen: Allergen) bool {
-    _ = score;
-    _ = allergen;
-    @compileError("please implement the isAllergicTo function");
+    const allergen_set = initAllergenSet(score);
+    return allergen_set.contains(allergen);
 }
 
 pub fn initAllergenSet(score: usize) EnumSet(Allergen) {
-    _ = score;
-    @compileError("please implement the initAllergenSet function");
+    var allergen_set = EnumSet(Allergen).initEmpty();
+
+    inline for (std.meta.fields(Allergen)) |allergen| {
+        if (is_set(score, allergen.value)) {
+            allergen_set.insert(@enumFromInt(allergen.value));
+        }
+    }
+
+    return allergen_set;
+}
+
+inline fn is_set(value: usize, bit_mask: usize) bool {
+    return value & bit_mask != 0;
 }
