23ddad07a4927738c07a3c262f1a9cb374711e52
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-06-10 23:25:45 +0300

    Remove largest prime + extra loop

--- zig/sieve/sieve.zig
+++ zig/sieve/sieve.zig
@@ -12,30 +12,22 @@ pub fn primes(buffer: []u32, comptime limit: u32) []u32 {
     // sieve_mask.unset(0);
     // sieve_mask.unset(1);
 
-    const largest_prime: u32 = sqrt(limit);
-    for (FIRST_PRIME..largest_prime + 1) |n| {
+    var unused_buffer = buffer[0..];
+    for (FIRST_PRIME..limit + 1) |n| {
         if (!sieve_mask.isSet(n)) {
             continue;
         }
-        // mark prime composites
-        const p = n;
-        var i = p * p;
-        while (i <= limit) : (i += p) {
-            sieve_mask.unset(i);
+
+        const prime: u32 = @intCast(n);
+        unused_buffer[0] = prime;
+        unused_buffer = unused_buffer[1..];
+
+        var composite = prime * prime;
+        while (composite <= limit) : (composite += prime) {
+            sieve_mask.unset(composite);
         }
     }
 
-    var unused_buffer = buffer[0..];
-    for (FIRST_PRIME..limit + 1) |n| {
-        if (sieve_mask.isSet(n)) {
-            unused_buffer[0] = @intCast(n);
-            unused_buffer = unused_buffer[1..];
-        }
-    }
     const used_buffer_len = buffer.len - unused_buffer.len;
     return buffer[0..used_buffer_len];
-}
-
-fn sqrt(number: u32) u32 {
-    return @intFromFloat(@ceil(@sqrt(@as(f64, @floatFromInt(number)))));
 }
