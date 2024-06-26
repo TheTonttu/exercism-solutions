997940819d1ae3b928aef5faae8b4100853b82f3
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-06-26 21:30:39 +0300

    Add C# "Luhn" exercise solution

--- csharp/luhn/Luhn.cs
+++ csharp/luhn/Luhn.cs
@@ -4,6 +4,36 @@ public static class Luhn
 {
     public static bool IsValid(string number)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        ReadOnlySpan<char> trimmedNumber = number.AsSpan().Trim();
+        if (trimmedNumber.Length < 2)
+        {
+            return false;
+        }
+
+        bool doubleDigit = false;
+        int sum = 0;
+        for (int i = trimmedNumber.Length - 1; i >= 0; i--)
+        {
+            char c = trimmedNumber[i];
+            if (c is >= '0' and <= '9')
+            {
+                int digit = c - '0';
+                if (doubleDigit)
+                {
+                    digit *= 2;
+                    if (digit > 9)
+                    {
+                        digit -= 9;
+                    }
+                }
+                sum += digit;
+                doubleDigit = !doubleDigit;
+            }
+            else if (c != ' ')
+            {
+                return false;
+            }
+        }
+        return sum % 10 == 0;
     }
 }
