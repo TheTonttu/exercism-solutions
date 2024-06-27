22a8b3bbeb3eb492291ad1d0e9e57fe936093147
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-06-27 21:01:35 +0300

    Add C# "Scrabble Score" exercise solution

--- csharp/scrabble-score/ScrabbleScore.cs
+++ csharp/scrabble-score/ScrabbleScore.cs
@@ -1,9 +1,24 @@
-using System;
-
 public static class ScrabbleScore
 {
     public static int Score(string input)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        int score = 0;
+
+        foreach (char letter in input)
+        {
+            score += char.ToUpperInvariant(letter) switch
+            {
+                'A' or 'E' or 'I' or 'O' or 'U' or 'L' or 'N' or 'R' or 'S' or 'T' => 1,
+                'D' or 'G' => 2,
+                'B' or 'C' or 'M' or 'P' => 3,
+                'F' or 'H' or 'V' or 'W' or 'Y' => 4,
+                'K' => 5,
+                'J' or 'X' => 8,
+                'Q' or 'Z' => 10,
+                _ => 0,
+            };
+        }
+
+        return score;
     }
 }
