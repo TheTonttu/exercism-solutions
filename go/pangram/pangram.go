98b666147a965a7d5825f36c2b059881dda75252
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-06-27 22:10:26 +0300

    Add Go "Pangram" exercise solution

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,5 +1,27 @@
 package pangram
 
+import "unicode"
+
 func IsPangram(input string) bool {
-	panic("Please implement the IsPangram function")
+	const Alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	matches := make(map[rune]bool)
+	for _, alphabet := range Alphabets {
+		matches[alphabet] = false
+	}
+
+	for _, char := range input {
+		upper := unicode.ToUpper(char)
+		match, exists := matches[upper]
+		if exists && !match {
+			matches[upper] = true
+		}
+	}
+
+	for _, match := range matches {
+		if !match {
+			return false
+		}
+	}
+	return true
 }
