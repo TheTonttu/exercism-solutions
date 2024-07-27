367fa86b5c61d2118537e702a1a5db70f041060b
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-07-27 13:34:50 +0300

    Add C# "Zebra Puzzle" exercise solution
    Manually solved... 🙃

---
+++ csharp/zebra-puzzle/StreetTests.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+using static ZebraPuzzle;
+using static Color;
+using static Drink;
+using static Nationality;
+using static Pet;
+using static Smoke;
+
+public class StreetTests
+{
+    // There are five houses.
+    [Fact]
+    public void ThereAreFiveHouses()
+    {
+        var street = GenerateStreet();
+        Assert.Equal(5, street.Length);
+    }
+
+    // The Englishman lives in the red house.
+    [Fact]
+    public void EnglishManLivesInRedHouse()
+    {
+        var street = GenerateStreet();
+        var redHouse = street.First(h => h.Color == Red);
+        Assert.Equal(Englishman, redHouse.Nationality);
+    }
+
+    // The Spaniard owns the dog.
+    [Fact]
+    public void SpaniardOwnsDog()
+    {
+        var street = GenerateStreet();
+        var dogOwner = street.First(h => h.Pet == Dog);
+        Assert.Equal(Spaniard, dogOwner.Nationality);
+    }
+
+    // Coffee is drunk in the green house.
+    [Fact]
+    public void CoffeeIsDrunkInGreenHouse()
+    {
+        var street = GenerateStreet();
+        var greenHouse = street.First(h => h.Color == Green);
+        Assert.Equal(Coffee, greenHouse.Drink);
+    }
+
+    // The green house is immediately to the right of the ivory house.
+    [Fact]
+    public void GreenHouseIsRightOfIvoryHouse()
+    {
+        var street = GenerateStreet();
+        int ivoryHouseNumber = IndexOf(street, h => h.Color == Ivory);
+        var houseRightOfIvoryHouse = street[ivoryHouseNumber+1];
+        Assert.Equal(Green, houseRightOfIvoryHouse.Color);
+    }
+
+    // The Ukrainian drinks tea.
+    [Fact]
+    public void UkrainianDrinksTea()
+    {
+        var street = GenerateStreet();
+        var teaDrinker = street.First(h => h.Drink == Tea);
+        Assert.Equal(Ukranian, teaDrinker.Nationality);
+    }
+
+    // The Old Gold smoker owns snails.
+    [Fact]
+    public void OldGoldSmokerOwnsSnails()
+    {
+        var street = GenerateStreet();
+        var snailOwner = street.First(h => h.Pet == Snails);
+        Assert.Equal(OldGold, snailOwner.Smoke);
+    }
+
+    // Kools are smoked in the yellow house.
+    [Fact]
+    public void KoolsAreSmokedInYellowHouse()
+    {
+        var street = GenerateStreet();
+        var yellowHouse = street.First(h => h.Color == Yellow);
+        Assert.Equal(Kools, yellowHouse.Smoke);
+    }
+
+    // Milk is drunk in the middle house.
+    [Fact]
+    public void MilkIsDrunkInMiddleHouse()
+    {
+        var street = GenerateStreet();
+        var middleHouse = street[street.Length/2];
+        Assert.Equal(Milk, middleHouse.Drink);
+    }
+
+    // The Norwegian lives in the first house.
+    [Fact]
+    public void NorwegianLivesInFirstHouse()
+    {
+        var street= GenerateStreet();
+        var firstHouse = street[0];
+        Assert.Equal(Norwegian, firstHouse.Nationality);
+    }
+
+    // The man who smokes Chesterfields lives in the house next to the man with the fox.
+    [Fact]
+    public void ChesterfieldsSmokerLivesNextToFoxOwner()
+    {
+        var street = GenerateStreet();
+        int chesterfieldsSmokerHouseNumber = IndexOf(street, h => h.Smoke == Chesterfields);
+        int foxOwnerHouseNumber = IndexOf(street, h => h.Pet == Fox);
+
+        Assert.True(LiveNextToEachOther(chesterfieldsSmokerHouseNumber, foxOwnerHouseNumber),
+            $"""
+            Chesterfields smoker should live next to fow owner.
+            Chesterfields h#={chesterfieldsSmokerHouseNumber}, Fox h#={foxOwnerHouseNumber}
+            """);
+
+    }
+
+    // Kools are smoked in the house next to the house where the horse is kept.
+    [Fact]
+    public void KoolsSmokerLivesNextToHorseOwner()
+    {
+        var street = GenerateStreet();
+        int koolsSmokerHouseNumber = IndexOf(street, h => h.Smoke == Kools);
+        int horseOwnerHouseNumber = IndexOf(street, h => h.Pet == Horse);
+
+        Assert.True(LiveNextToEachOther(koolsSmokerHouseNumber, horseOwnerHouseNumber),
+            $"""
+            Kools smoker should live next to horse owner.
+            Chesterfields h#={koolsSmokerHouseNumber}, Fox h#={horseOwnerHouseNumber}
+            """);
+    }
+
+    // The Lucky Strike smoker drinks orange juice.
+    [Fact]
+    public void LuckyStrikeSmokerDrinksOrangeJuice()
+    {
+        var street = GenerateStreet();
+        var orangeJuiceDrinker = street.First(h => h.Drink == OrangeJuice);
+        Assert.Equal(LuckyStrike, orangeJuiceDrinker.Smoke);
+    }
+
+    // The Japanese smokes Parliaments.
+    [Fact]
+    public void JapaneseSmokesParliaments()
+    {
+        var street = GenerateStreet();
+        var parliamentsSmoker = street.First(h => h.Smoke == Parliaments);
+        Assert.Equal(Japanese, parliamentsSmoker.Nationality);
+    }
+
+    // The Norwegian lives next to the blue house.
+    [Fact]
+    public void NorwegianLivesNextToBlueHouse()
+    {
+        var street = GenerateStreet();
+        var norwegianHouseNumber = IndexOf(street, h => h.Nationality == Norwegian);
+        var blueHouseNumber = IndexOf(street, h => h.Color == Blue);
+        Assert.Equal(blueHouseNumber, norwegianHouseNumber + 1);
+    }
+
+    [Fact]
+    public void HousesAreUnique()
+    {
+        var street = GenerateStreet();
+        AssertHouseUniqueness(street);
+    }
+
+    private bool LiveNextToEachOther(int houseNumber1, int houseNumber2) =>
+        (houseNumber2 == houseNumber1 - 1) ||
+        (houseNumber2 == houseNumber1 + 1);
+
+    private static void AssertHouseUniqueness(House[] street)
+    {
+        for (int i = 0; i < street.Length; i++)
+        {
+            House house = street[i];
+            for (int nhi = i + 1; nhi < street.Length; nhi++)
+            {
+                var otherHouse = street[nhi];
+                Assert.False(house.HasSameCharacteristics(otherHouse), $"House {house} (h#{i}) and {otherHouse} (h#{nhi}) share characteristics.");
+            }
+        }
+    }
+
+    private static int IndexOf(House[] street, Predicate<House> predicate)
+    {
+        for (int i = 0; i <= street.Length; i++)
+        {
+            if (predicate(street[i]))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
--- csharp/zebra-puzzle/ZebraPuzzle.cs
+++ csharp/zebra-puzzle/ZebraPuzzle.cs
@@ -2,21 +2,241 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-public enum Color { Red , Green , Ivory , Yellow , Blue }
-public enum Nationality { Englishman , Spaniard , Ukranian , Japanese , Norwegian }
-public enum Pet { Dog , Snails , Fox , Horse , Zebra }
-public enum Drink { Coffee , Tea , Milk , OrangeJuice , Water }
-public enum Smoke { OldGold , Kools , Chesterfields , LuckyStrike , Parliaments }
+using static Color;
+using static Nationality;
+using static Pet;
+using static Drink;
+using static Smoke;
+
+public enum Color { Red, Green, Ivory, Yellow, Blue }
+public enum Nationality { Englishman, Spaniard, Ukranian, Japanese, Norwegian }
+public enum Pet { Dog, Snails, Fox, Horse, Zebra }
+public enum Drink { Coffee, Tea, Milk, OrangeJuice, Water }
+public enum Smoke { OldGold, Kools, Chesterfields, LuckyStrike, Parliaments }
 
 public static class ZebraPuzzle
 {
+    private static readonly House[] Street = GenerateStreet();
+
     public static Nationality DrinksWater()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var waterDrinker = Street.First(h => h.Drink == Water);
+        return waterDrinker.Nationality;
     }
 
     public static Nationality OwnsZebra()
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var zebraOwner = Street.First(h => h.Pet == Zebra);
+        return zebraOwner.Nationality;
+    }
+
+    public static House[] GenerateStreet()
+    {
+        // Meh, let's do this manually.
+
+        // There are five houses.
+        var builders = new HouseBuilder[5];
+
+        // The Englishman lives in the red house.
+        var english = new HouseBuilder()
+        {
+            Color = Red,
+            Nationality = Englishman
+        };
+        // The Spaniard owns the dog.
+        var spanish = new HouseBuilder()
+        {
+            Nationality = Spaniard,
+            Pet = Dog
+        };
+        // The Ukrainian drinks tea.
+        var ukrainian = new HouseBuilder()
+        {
+            Nationality = Ukranian,
+            Drink = Tea
+        };
+        // The Japanese smokes Parliaments.
+        var japanese = new HouseBuilder()
+        {
+            Nationality = Japanese,
+            Smoke = Parliaments
+        };
+        var norwegian = new HouseBuilder()
+        {
+            Nationality = Norwegian,
+        };
+
+        // The Norwegian lives in the first house.
+        builders[0] = norwegian;
+        if (builders[0] is var h0)
+        {
+            // Kools are smoked in the yellow house.
+            h0.Color = Yellow;
+            h0.Drink = Water;
+            h0.Pet = Fox;
+            h0.Smoke = Kools;
+        }
+        builders[1] = ukrainian;
+        if (builders[1] is var h1)
+        {
+            // The Norwegian lives next to the blue house.
+            h1.Color = Blue;
+            // Kools are smoked in the house next to the house where the horse is kept.
+            h1.Pet = Horse;
+            // The man who smokes Chesterfields lives in the house next to the man with the fox.
+            h1.Smoke = Chesterfields;
+        }
+        builders[2] = english;
+        if (builders[2] is var h2)
+        {
+            // Milk is drunk in the middle house.
+            h2.Drink = Milk;
+            // The Old Gold smoker owns snails.
+            h2.Pet = Snails;
+            h2.Smoke = OldGold;
+        }
+        builders[3] = spanish;
+        if (builders[3] is var h3)
+        {
+            // The green house is immediately to the right of the ivory house.
+            h3.Color = Ivory;
+            // The Lucky Strike smoker drinks orange juice.
+            h3.Drink = OrangeJuice;
+            h3.Smoke = LuckyStrike;
+        }
+        builders[4] = japanese;
+        if (builders[4] is var h4)
+        {
+            // Coffee is drunk in the green house.
+            h4.Color = Green;
+            h4.Drink = Coffee;
+            h4.Pet = Zebra;
+        }
+
+        var street = BuildStreet(builders);
+
+        return street;
+    }
+
+    private static House[] BuildStreet(HouseBuilder[] builders)
+    {
+        House[] houses = new House[builders.Length];
+
+        var failures = new List<Exception>();
+        for (int i = 0; i < builders.Length; i++)
+        {
+            try
+            {
+                houses[i] = builders[i].Build();
+            }
+            catch (Exception e)
+            {
+                failures.Add(new InvalidOperationException($"Building house #{i} failed.", e));
+            }
+        }
+        if (failures.Count > 0)
+        {
+            throw new AggregateException(failures);
+        }
+        return houses;
+    }
+
+    public class HouseBuilder()
+    {
+        private Color? _color;
+        private Nationality? _nationality;
+        private Pet? _pet;
+        private Drink? _drink;
+        private Smoke? _smoke;
+
+        public Color? Color
+        {
+            get => _color;
+            set
+            {
+                if (_color != null)
+                {
+                    throw new InvalidOperationException("Color already set");
+                }
+                _color = value;
+            }
+        }
+        public Nationality? Nationality
+        {
+            get => _nationality;
+            set
+            {
+                if (_nationality != null)
+                {
+                    throw new InvalidOperationException("Nationality already set");
+                }
+                _nationality = value;
+            }
+        }
+        public Pet? Pet
+        {
+            get => _pet;
+            set
+            {
+                if (_pet != null)
+                {
+                    throw new InvalidOperationException("Pet already set");
+                }
+                _pet = value;
+            }
+        }
+        public Drink? Drink
+        {
+            get => _drink;
+            set
+            {
+                if (_drink != null)
+                {
+                    throw new InvalidOperationException("Drink already set");
+                }
+                _drink = value;
+            }
+        }
+        public Smoke? Smoke
+        {
+            get => _smoke;
+            set
+            {
+                if (_smoke != null)
+                {
+                    throw new InvalidOperationException("Smoke already set");
+                }
+                _smoke = value;
+            }
+        }
+
+        public House Build()
+        {
+            return new House(
+                Color ?? throw new InvalidOperationException("Color missing"),
+                Nationality ?? throw new InvalidOperationException("Nationality missing"),
+                Pet ?? throw new InvalidOperationException("Pet missing"),
+                Drink ?? throw new InvalidOperationException("Drink missing"),
+                Smoke ?? throw new InvalidOperationException("Smoke missing")
+            );
+        }
+    }
+
+    public record House(Color Color, Nationality Nationality, Pet Pet, Drink Drink, Smoke Smoke)
+    {
+
+        public bool HasSameCharacteristics(House? other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Color == other.Color ||
+                Nationality == other.Nationality ||
+                Pet == other.Pet ||
+                Drink == other.Drink ||
+                Smoke == other.Smoke;
+        }
     }
 }
