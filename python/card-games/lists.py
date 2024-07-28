a725bb28263450ea1bfd1d36f5cbfad4c184b734
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-07-28 17:46:30 +0300

    Add Python "Card Games" exercise solution

--- python/card-games/lists.py
+++ python/card-games/lists.py
@@ -3,74 +3,102 @@
 Python list documentation: https://docs.python.org/3/tutorial/datastructures.html
 """
 
+from __future__ import annotations
 
-def get_rounds(number):
+
+def get_rounds(number: int) -> list[int]:
     """Create a list containing the current and next two round numbers.
 
     :param number: int - current round number.
     :return: list - current round and the two that follow.
     """
+    return [number, number + 1, number + 2]
 
-    pass
 
-
-def concatenate_rounds(rounds_1, rounds_2):
+def concatenate_rounds(rounds_1: list[int], rounds_2: list[int]) -> list[int]:
     """Concatenate two lists of round numbers.
 
     :param rounds_1: list - first rounds played.
     :param rounds_2: list - second set of rounds played.
     :return: list - all rounds played.
     """
+    return rounds_1 + rounds_2
 
-    pass
 
-
-def list_contains_round(rounds, number):
+def list_contains_round(rounds: list[int], number: int) -> bool:
     """Check if the list of rounds contains the specified number.
 
     :param rounds: list - rounds played.
     :param number: int - round number.
     :return: bool - was the round played?
     """
+    return number in rounds
 
-    pass
 
-
-def card_average(hand):
+def card_average(hand: list[int]) -> float:
     """Calculate and returns the average card value from the list.
 
     :param hand: list - cards in hand.
     :return: float - average value of the cards in the hand.
     """
+    card_count = len(hand)
+    if card_count == 0:
+        return 0
 
-    pass
+    hand_sum = 0
+    for card in hand:
+        hand_sum += card
+
+    return hand_sum / card_count
 
 
-def approx_average_is_average(hand):
+def approx_average_is_average(hand: list[int]) -> bool:
     """Return if the (average of first and last card values) OR ('middle' card) == calculated average.
 
     :param hand: list - cards in hand.
     :return: bool - does one of the approximate averages equal the `true average`?
     """
+    if len(hand) == 0:
+        # This technically could also return true
+        # if missing values would be handled as zero.
+        return False
 
-    pass
+    avg = card_average(hand)
+
+    first = hand[0]
+    middle = hand[len(hand) // 2]
+    last = hand[-1]
+
+    return (first + last) / 2 == avg or middle == avg
 
 
-def average_even_is_average_odd(hand):
+def average_even_is_average_odd(hand: list[int]) -> bool:
     """Return if the (average of even indexed card values) == (average of odd indexed card values).
 
     :param hand: list - cards in hand.
     :return: bool - are even and odd averages equal?
     """
+    even = hand[::2]
+    odd = hand[1::2]
 
-    pass
+    even_avg = card_average(even)
+    odd_avg = card_average(odd)
+
+    return even_avg == odd_avg
 
 
-def maybe_double_last(hand):
+JACK_CARD = 11
+
+
+def maybe_double_last(hand: list[int]) -> list[int]:
     """Multiply a Jack card value in the last index position by 2.
 
     :param hand: list - cards in hand.
     :return: list - hand with Jacks (if present) value doubled.
     """
+    new_hand = hand.copy()
 
-    pass
+    if new_hand[-1] == JACK_CARD:
+        new_hand[-1] *= 2
+
+    return new_hand
