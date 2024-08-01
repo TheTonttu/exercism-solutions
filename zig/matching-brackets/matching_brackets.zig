bfb6a71917bb659690586722b15e2f13c718b022
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-08-01 22:58:53 +0300

    Inline closed pair check method

--- zig/matching-brackets/matching_brackets.zig
+++ zig/matching-brackets/matching_brackets.zig
@@ -4,20 +4,17 @@ const mem = std.mem;
 pub fn isBalanced(allocator: mem.Allocator, s: []const u8) !bool {
     var expected_closed_pairs = std.ArrayList(u8).init(allocator);
     defer expected_closed_pairs.deinit();
+
     for (s) |c| {
         switch (c) {
             '{' => try expected_closed_pairs.append('}'),
             '(' => try expected_closed_pairs.append(')'),
             '[' => try expected_closed_pairs.append(']'),
-            '}', ')', ']' => if (!expectedClosedPair(c, &expected_closed_pairs)) {
+            '}', ')', ']' => if (expected_closed_pairs.popOrNull() != c) {
                 return false;
             },
             else => {},
         }
     }
     return expected_closed_pairs.items.len == 0;
-}
-
-fn expectedClosedPair(closed_pair: u8, closed_pair_queue: *std.ArrayList(u8)) bool {
-    return closed_pair_queue.popOrNull() == closed_pair;
 }
