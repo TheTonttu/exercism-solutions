a15ab341e2b9ee6fd367dbd81c97b21c99313ac6
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-08-01 23:09:27 +0300

    Combine close pair switch cases

--- zig/matching-brackets/matching_brackets.zig
+++ zig/matching-brackets/matching_brackets.zig
@@ -2,19 +2,26 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn isBalanced(allocator: mem.Allocator, s: []const u8) !bool {
-    var expected_closed_pairs = std.ArrayList(u8).init(allocator);
-    defer expected_closed_pairs.deinit();
+    var expected_close_pairs = std.ArrayList(u8).init(allocator);
+    defer expected_close_pairs.deinit();
 
     for (s) |c| {
         switch (c) {
-            '{' => try expected_closed_pairs.append('}'),
-            '(' => try expected_closed_pairs.append(')'),
-            '[' => try expected_closed_pairs.append(']'),
-            '}', ')', ']' => if (expected_closed_pairs.popOrNull() != c) {
+            '{', '(', '[' => try expected_close_pairs.append(closePair(c)),
+            '}', ')', ']' => if (expected_close_pairs.popOrNull() != c) {
                 return false;
             },
             else => {},
         }
     }
-    return expected_closed_pairs.items.len == 0;
+    return expected_close_pairs.items.len == 0;
+}
+
+fn closePair(open_pair: u8) u8 {
+    return switch (open_pair) {
+        '{' => '}',
+        '(' => ')',
+        '[' => ']',
+        else => unreachable,
+    };
 }
