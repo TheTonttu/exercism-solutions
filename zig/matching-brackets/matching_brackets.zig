19a15b692bead17a109e6ac073b8b9bfb6ff9f80
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-08-01 22:55:48 +0300

    Add Zig "Matching Brackets" exercise solution

--- zig/matching-brackets/matching_brackets.zig
+++ zig/matching-brackets/matching_brackets.zig
@@ -2,7 +2,26 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn isBalanced(allocator: mem.Allocator, s: []const u8) !bool {
-    _ = allocator;
-    _ = s;
-    @compileError("please implement the isBalanced function");
+    var expected_closed_pairs = std.ArrayList(u8).init(allocator);
+    defer expected_closed_pairs.deinit();
+    for (s) |c| {
+        switch (c) {
+            '{' => try expected_closed_pairs.append('}'),
+            '(' => try expected_closed_pairs.append(')'),
+            '[' => try expected_closed_pairs.append(']'),
+            '}', ')', ']' => if (!expectedClosedPair(c, &expected_closed_pairs)) {
+                return false;
+            },
+            else => {},
+        }
+    }
+    return expected_closed_pairs.items.len == 0;
+}
+
+fn expectedClosedPair(closed_pair: u8, closed_pair_queue: *std.ArrayList(u8)) bool {
+    const expected_closed_pair = closed_pair_queue.popOrNull();
+    if (expected_closed_pair == null) {
+        return false;
+    }
+    return expected_closed_pair == closed_pair;
 }
