d3891ba79a12f5703d450e773ca6a1e73b58fc3c
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-08-03 10:50:47 +0300

    Add C# "RNA Transcription" exercise solution

--- csharp/rna-transcription/RnaTranscription.cs
+++ csharp/rna-transcription/RnaTranscription.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Text;
 
 public static class RnaTranscription
 {
-    public static string ToRna(string nucleotide)
+    public static string ToRna(string dnaStrand)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        var rnaStrandBuilder = new StringBuilder();
+        foreach (char dnaNucleotide in dnaStrand)
+        {
+            char rnaNucleotide = dnaNucleotide switch
+            {
+                'G' => 'C',
+                'C' => 'G',
+                'T' => 'A',
+                'A' => 'U',
+                _ => throw new InvalidOperationException($"{dnaNucleotide} is invalid DNA nucleotide.")
+            };
+            rnaStrandBuilder.Append(rnaNucleotide);
+        }
+        return rnaStrandBuilder.ToString();
     }
 }
