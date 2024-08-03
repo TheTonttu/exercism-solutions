359c7670cbc08417af9dfc321b6728f223fe733d
Tonttu <15074459+TheTonttu@users.noreply.github.com>
2024-08-03 11:17:47 +0300

    Add C# "Binary Search" exercise solution

--- csharp/binary-search/BinarySearch.cs
+++ csharp/binary-search/BinarySearch.cs
@@ -4,6 +4,37 @@ public static class BinarySearch
 {
     public static int Find(int[] input, int value)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        return Find(input.AsSpan(), value);
+    }
+
+    private static int Find(ReadOnlySpan<int> input, int value)
+    {
+        if (input.IsEmpty)
+        {
+            return -1;
+        }
+
+        int middlePointIndex = input.Length / 2;
+
+        int middlePointValue = input[middlePointIndex];
+        if (middlePointValue == value)
+        {
+            return middlePointIndex;
+        }
+
+        if (middlePointValue > value)
+        {
+            var left = input[..middlePointIndex];
+            return Find(left, value);
+        }
+
+        var right = input[(middlePointIndex + 1)..];
+        int rightIndex = Find(right, value);
+        if (rightIndex > -1)
+        {
+            return middlePointIndex + 1 + rightIndex;
+        }
+
+        return -1;
     }
 }
